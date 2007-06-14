d6766b246b55940cd18808adae2bb2aa334c9df5
Michael Hanke <michael.hanke@gmail.com>
2007-06-14 21:00:51 +0200

    Added p-map to the searchlight output.
    Also added more documentation to this file.

--- mvpa/searchlight.py
+++ mvpa/searchlight.py
@@ -27,8 +27,46 @@ import algorithms
 import numpy
 import crossval
 import sys
+import scipy.stats as stats
 
 class Searchlight( object ):
+    """
+    The object provides a number of properties that can be used to access
+    input and output data of the searchlight algorithm.
+
+    The following properties provide access to array that match the size of
+    the mask shape. Each element contains information about the sphere that
+    is centered on the respective location in the mask:
+
+        perfmean   - mean classification performance
+        perfvar    - variance of classification performance
+        perfmin    - minimal classification performance of all CV-folds
+        perfmax    - maximal classification performance of all CV-folds
+        notchance  - two-tailed probability of the peformance being different
+                     from chance level ( 1 / <number of classes> ).
+        spheresize - number of features in the sphere
+
+    These properties give access to the input data and other status variables:
+
+        pattern     - the MVPAPattern object that holds the data set
+        mask        - the mask that is used to generate the spheres
+        radius      - the sphere radius in an arbitrary unit (see elementsize
+                      property)
+        elementsize - a vector specifying the extent of each data element
+                      along all dimensions in the dataset. This information is
+                      used to translate the radius into element units
+        cvtype      - type of cross-validation that is used. 1 means N-1 CV,
+                      2 means N-2 ...
+        clf         - a class providing a classifier
+        clfargs     - dictionary with keyword arguments for the classifiers
+                      constructor
+        forcesphere - if True a full sphere is used regardless of the status
+                      of the status of the sphere elements in the mask. If
+                      False only elements are considered as sphere elements
+                      that have a non-zero value in the mask.
+        ncvfolds    - number of cross-validation folds that are computed by
+                      the searchlight algorithm for each sphere
+    """
     def __init__( self, pattern, mask,
                   radius = 1.0,
                   elementsize = None,
@@ -63,10 +101,21 @@ class Searchlight( object ):
         self.__perfmin = numpy.zeros(self.pattern.origshape)
         self.__perfmax = numpy.zeros(self.pattern.origshape)
         self.__perfvar = numpy.zeros(self.pattern.origshape)
+        self.__notchance = numpy.zeros(self.pattern.origshape)
         self.__spheresize = numpy.zeros(self.pattern.origshape, dtype='uint')
 
 
     def run( self, verbose=False, classifier = None, **kwargs ):
+        """ Perform the spheresearch for all possible spheres in the
+        mask.
+
+        By setting 'verbose' to True one can enable some status messages that
+        inform about the progress while processing the spheres.
+
+        The 'classifier' argument overrides can be used to select a new
+        classification algorithm. Additional keyword are passed to the
+        classifier's contructor.
+        """
         # accept new classifier if any
         if classifier:
             self.__clf = classifier
@@ -111,12 +160,15 @@ class Searchlight( object ):
             self.__perfmin[center_index] = perf.min()
             # performance maximum
             self.__perfmax[center_index] = perf.max()
+            # significantly different from chance?
+            self.__notchance[center_index] = \
+                stats.ttest_1samp(perf, 
+                                  1.0 / len( self.pattern.reglabels ) )[1]
             # spheresize / number of features
             self.__spheresize[center_index] = sphere.shape[1]
 
-            sphere_count += 1
-
             if verbose:
+                sphere_count += 1
                 print "\rDoing %i spheres: %i%%" \
                     % (nspheres, float(sphere_count)/nspheres*100,),
                 sys.stdout.flush()
@@ -125,6 +177,9 @@ class Searchlight( object ):
             print ''
 
     def getNCVFolds( self ):
+        """ Returns the number of cross-validation folds that is used by
+        the searchlight algorithm.
+        """
         return len( crossval.getUniqueLengthNCombinations(
                         self.pattern.originlabels,
                         self.cvtype ) )
@@ -134,6 +189,7 @@ class Searchlight( object ):
     perfvar = property( fget=lambda self: self.__perfvar )
     perfmin = property( fget=lambda self: self.__perfmin )
     perfmax = property( fget=lambda self: self.__perfmax )
+    notchance = property( fget=lambda self: self.__notchance )
     spheresize = property( fget=lambda self: self.__spheresize )
 
     # other data access
