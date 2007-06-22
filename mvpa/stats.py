8a755dc250dc2a8b3e16ecdfe122b4d4755cdf96
Michael Hanke <michael.hanke@gmail.com>
2007-06-22 21:45:29 +0200

    Added stats helper file (+ unittest).
    ATM it contains a function for computing chisquare values from contingency tables.

---
+++ mvpa/stats.py
@@ -0,0 +1,50 @@
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#    PyMVPA: Little statistics helper
+#
+#    Copyright (C) 2007 by
+#    Michael Hanke <michael.hanke@gmail.com>
+#
+#    This package is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2 of the License, or (at your option) any later version.
+#
+#    This package is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+
+import numpy as np
+import scipy.stats as stats
+
+def chisquare( obs, exp = None ):
+    """ Compute the chisquare value of a contingency table with arbitrary
+    dimensions.
+
+    If no expected frequencies are supplied, the total N is assumed to be
+    equally distributed across all cells.
+
+    Returns: chisquare-stats, associated p-value (upper tail)
+    """
+    obs = np.array( obs )
+
+    # get total number of observations
+    N = np.sum( obs )
+
+    # if no expected value are supplied assume equal distribution
+    if exp == None:
+        exp = np.ones(obs.shape) * N / np.prod(obs.shape)
+
+    # make sure to have floating point data
+    exp = exp.astype( float )
+
+    # compute chisquare value
+    chisq = np.sum( ( obs - exp )**2 / exp )
+
+    # return chisq and probability (upper tail)
+    return chisq, stats.chisqprob( chisq, np.prod( obs.shape ) - 1 )
+
+
---
+++ tests/test_stats.py
@@ -0,0 +1,45 @@
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+#
+#    Unit tests for PyMVPA stats helpers
+#
+#    Copyright (C) 2007 by
+#    Michael Hanke <michael.hanke@gmail.com>
+#
+#    This package is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    version 2 of the License, or (at your option) any later version.
+#
+#    This package is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+
+import mvpa.stats as ms
+import unittest
+import numpy as np
+
+class StatsTests(unittest.TestCase):
+
+    def testChiSquare(self):
+        # test equal distribution
+        tbl = np.array([[5,5],[5,5]])
+        chi, p = ms.chisquare( tbl )
+        self.failUnless( chi == 0.0 )
+        self.failUnless( p == 1.0 )
+
+        # test non-equal distribution
+        tbl = np.array([[4,0],[0,4]])
+        chi, p = ms.chisquare( tbl )
+        self.failUnless( chi == 8.0 )
+        self.failUnless( p < 0.05 )
+
+
+def suite():
+    return unittest.makeSuite(StatsTests)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
