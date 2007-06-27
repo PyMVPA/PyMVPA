2d78df70f5aafba976ced6beffd71197381f2bb9
Michael Hanke <michael.hanke@gmail.com>
2007-06-27 14:14:54 +0200

    Completed SerialFeatureInclusion.
    Finished selectROIs() and refactored selectFeatures() to share a common backend
    with selectROIs().

--- mvpa/serialinclusion.py
+++ mvpa/serialinclusion.py
@@ -74,9 +74,132 @@ class SerialFeatureInclusion( object ):
         self.__cv_perf = None
 
 
-    def selectROIs( self, classifier, verbose=False, **kwargs ):
+    def __doCrossValidation( self, features, clf, clfargs ):
+        """ Internal method to perform a cross-validation on a subset of
+        features.
+
+        Call CrossValidation.run() and returns the CrossValidation object.
         """
-        NOT IMPLEMENTED YET!
+        # select the feature subset from the dataset
+        temp_pat = self.pattern.selectFeatures( features )
+
+        # now do cross-validation with the current feature set
+        cv = crossval.CrossValidation( temp_pat, **(self.__cvargs) )
+        cv.run( clf, cvtype=self.__cvtype, **(clfargs) )
+
+        return cv
+
+
+    def __selectByFeatureId( self, selected, candidate ):
+        """ Internal method: ignore! """
+        return selected + [candidate]
+
+
+    def __selectByMaskValue( self, selected, candidate ):
+        """ Internal method: ignore! """
+        return np.logical_or( selected, self.mask == candidate )
+
+
+    def __doSelection( self, clf, clfargs, featureselector,
+                       selected, candidates, verbose ):
+        """ Backend method for selectFeatures() and selectROIs.
+        """
+        # yes, it is exactly that
+        best_performance_ever = 0.0
+
+        # contains the number of selected candidates
+        selection_counter = 0
+
+        # list of best canidate ids
+        # in case of a simple feature selection, this is redundant with
+        # the 'selected' list
+        best_ids = []
+
+        # as long as there are candidates left
+        # the loop might get broken earlier if the generalization
+        # error does not go down when a new ROI is selected
+        while len( candidates ):
+            # holds the performance value of each candidate
+            candidate_rating = []
+
+            # for all possible candidates
+            for candidate in candidates:
+                # display some status output about the progress if requested
+                if verbose:
+                    print "\rTested %i; nselected %i; mean performance: %.3f" \
+                        % ( len(candidate_rating),
+                            selection_counter,
+                            best_performance_ever ),
+                    sys.stdout.flush()
+
+                # take the new candidate and all already selected features
+                features = featureselector( selected, candidate )
+
+                # now do cross-validation with the current feature set
+                cv = self.__doCrossValidation( features,
+                                               clf,
+                                               clfargs )
+
+                # store the generalization performance for this feature set
+                candidate_rating.append( np.mean(cv.perf) )
+
+            # I like arrays!
+            rating_array = np.array( candidate_rating )
+
+            # determine the best performing additonal candidate (get its id)
+            best_id = candidates[ rating_array.argmax() ]
+
+            # check if the new candidate brings no value
+            # if this is the case we are done.
+            if rating_array.max() < best_performance_ever:
+                break
+
+            # the new candidate adds value, because the generalization error
+            # went down, therefore add it to the list of selected features
+            selected = featureselector( selected, best_id )
+            best_ids.append( best_id )
+
+            # and remove it from the candidate features
+            # otherwise the while loop could run forever
+            candidates.remove( best_id )
+
+            # update the latest best performance
+            best_performance_ever = rating_array.max()
+
+            # and look for the next best thing (TM)
+            selection_counter += 1
+
+        # if new candidates are left or the generalization performance
+        # went down
+        if verbose:
+            # end pending line
+            print ''
+
+        # now do cross-validation again with the current best
+        # feature set to have access to the full CV results
+        cv = self.__doCrossValidation( selected,
+                                       clf,
+                                       clfargs )
+
+        # and leave the loop as there is nothing more to do
+        return selected, best_ids, cv
+
+
+
+    def selectROIs( self, classifier, verbose=False, **kwargs ):
+        """ Select the best set of ROIs from a mask that maximizes the
+        generalization performance.
+
+        The method works exactly like selectFeatures(), but instead of choosing
+        single features from a mask it works on ROIs (sets of features).
+
+        An ROI (region of interest) are all features sharing a common value in
+        the mask.
+
+        The method returns the list of selected ROI ids (mask values). A
+        boolean mask with all selected feature from all selected ROIs is
+        available via the selectionmask property.
+
         By setting 'verbose' to True one can enable some status messages that
         inform about the progress.
 
@@ -84,13 +207,32 @@ class SerialFeatureInclusion( object ):
         the classification. Additional keyword are passed to the classifier's
         contructor.
         """
-        raise RuntimeError, 'Not implemented yet.'
-
         # cleanup prior runs first
         self.__clearResults()
 
         # get all different mask values
-        mask_values = np.unique( self.mask )
+        mask_values = np.unique( self.mask ).tolist()
+
+        # selected features are stored in a mask array with the same shape
+        # as the original feature mask
+        selected_features = np.zeros( self.mask.shape, dtype='bool' )
+
+        # call the backend method that returns a list of the selected
+        # features and the cross-validation object with the best feature set
+        # that can be used to query more detailed results
+        feature_mask, selected_rois, best_cv = \
+             self.__doSelection( classifier, kwargs,
+                                 self.__selectByMaskValue,
+                                 selected_features,
+                                 mask_values,
+                                 verbose )
+
+        # store results
+        self.__contingency_tbl = best_cv.contingencytbl
+        self.__cv_perf = np.array( best_cv.perf )
+        self.__selection_mask = feature_mask
+
+        return selected_rois
 
 
     def selectFeatures( self, classifier, verbose=False, **kwargs ):
@@ -102,7 +244,7 @@ class SerialFeatureInclusion( object ):
         respect to their individiual classification performance. Only the best
         performing feature is selected. Next, each remaining feature is tested
         again whether it provides the most additional information with respect
-        to classification performance. This procedure continues until a 
+        to classification performance. This procedure continues until a
         to-be-selected feature does not reduce the generalization error.
 
         By setting 'verbose' to True one can enable some status messages that
@@ -120,98 +262,32 @@ class SerialFeatureInclusion( object ):
 
         # initially empty list of selected features
         selected_features = []
+
         # transform coordinates of nonzero mask elements into feature ids
         cand_features = [ self.pattern.getFeatureId(coord)
                           for coord in np.transpose( self.mask.nonzero() ) ]
 
-        # yes, it is exactly that
-        best_performance_ever = 0.0
+        # call the backend method that returns a list of the selected
+        # features and the cross-validation object with the best feature set
+        # that can be used to query more detailed results
+        selected_features, dummy, best_cv = \
+             self.__doSelection( classifier, kwargs,
+                                 self.__selectByFeatureId,
+                                 selected_features,
+                                 cand_features,
+                                 verbose )
 
-        # as long as there are candidate features left
-        # the loop might get broken earlier if the generalization
-        # error does not go down when a new feature is selected
-        while len( cand_features ):
-            # holds the performance value of each candidate feature
-            feature_rating = []
-
-            # for all possible candidate features
-            for cand_feature in cand_features:
-                # display some status output about the progress if requested
-                if verbose:
-                    print "\rTested %i; nselected %i; mean performance: %.3f" \
-                        % ( len(feature_rating),
-                            len(selected_features),
-                            best_performance_ever ),
-                    sys.stdout.flush()
-
-                # take the new candidate and all already selected features
-                features = selected_features + [cand_feature]
-                # to select the feature from the dataset
-                temp_pat = self.pattern.selectFeatures( features )
-
-                # now do cross-validation with the current feature set
-                cv = crossval.CrossValidation( temp_pat, **(self.__cvargs) )
-                cv.run( classifier, cvtype=self.__cvtype, **(kwargs) )
-
-                # store the generalization performance for this feature
-                feature_rating.append( np.mean(cv.perf) )
-
-            # I like arrays!
-            rating_array = np.array( feature_rating )
-
-            # determine the best performing additonal feature (get its id)
-            best_id = cand_features[ rating_array.argmax() ]
-
-            # check if the new feature brings no value
-            # if this is the case we are done.
-            if rating_array.max() < best_performance_ever:
-                if verbose:
-                    # end pending line
-                    print ''
-
-                # now do cross-validation again with the current best
-                # feature set to have access to the full CV results
-                temp_pat = self.pattern.selectFeatures( selected_features )
-                cv = crossval.CrossValidation( temp_pat, **(self.__cvargs) )
-                cv.run( classifier, cvtype=self.__cvtype, **(kwargs) )
-
-                # store results
-                self.__selected_features = selected_features
-                self.__contingency_tbl = cv.contingencytbl
-                self.__cv_perf = np.array( cv.perf )
-
-                # and leave the loop as there is nothing more to do
-                break;
-
-            # the new feature adds value, because the generalization error
-            # went down, therefore add it to the list of selected features
-            selected_features.append( best_id )
-            # and remove it from the candidate features
-            # otherwise the while loop could run forever
-            cand_features.remove( best_id )
-
-            # update the latest best performance
-            best_performance_ever = rating_array.max()
-
-            # and look for the next best thing (TM)
+        # store results
+        self.__contingency_tbl = best_cv.contingencytbl
+        self.__cv_perf = np.array( best_cv.perf )
+        self.__selection_mask = \
+            self.pattern.features2origmask( selected_features )
 
         return selected_features
 
 
-    def getSelectionMask( self ):
-        """ Returns a mask of the selected feature in the original dataspace.
-
-        If no features were selected 'None' is returned.
-        """
-        if self.selectedfeatures == None:
-            return None
-
-        return self.pattern.features2origmask( self.selectedfeatures )
-
-
     # access to the results
-    selectedfeatures = property( fget=lambda self: self.__selected_features )
-    selectionmask = property( fget=getSelectionMask )
+    selectionmask = property( fget=lambda self: self.__selection_mask )
     cvperf = property( fget=lambda self: self.__cv_perf )
     contingencytbl = property( fget=lambda self: self.__contingency_tbl )
 
--- tests/test_serinclusion.py
+++ tests/test_serinclusion.py
@@ -1,4 +1,4 @@
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 #
 #    Unit tests for PyMVPA serial feature inclusion algorithm
 #
@@ -14,7 +14,7 @@
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 #    Lesser General Public License for more details.
 #
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 
 import mvpa.serialinclusion as si
 import mvpa
@@ -26,32 +26,51 @@ import scipy.stats as stats
 
 class SerialInclusionTests(unittest.TestCase):
 
-    def testSerialInclusion(self):
+    def setUp(self):
         # prepare demo dataset and mask
-        mask = np.ones((3,3,3))
-        data = np.random.uniform(0,1,(100,) + (mask.shape))
+        self.mask = np.ones((3,3,3))
+        data = np.random.uniform(0,1,(100,) + (self.mask.shape))
         reg = np.arange(100) / 50
         orig = range(10) * 10
-        pattern = mvpa.MVPAPattern(data, reg, orig)
+        self.pattern = mvpa.MVPAPattern(data, reg, orig)
 
+
+
+    def testSerialFeatureInclusion(self):
         # init algorithm
-        sinc = si.SerialFeatureInclusion( pattern,
-                                          mask,
+        sinc = si.SerialFeatureInclusion( self.pattern,
+                                          self.mask,
                                           ncvfoldsamples=1 )
 
         # run selection of single features
-        sinc.selectFeatures( svm_wrap.SVM, verbose=True)
+        selected_features = sinc.selectFeatures( knn.kNN, verbose=True)
 
         # no real check yet, simply checking something happened
         # one must always be selected
-        self.failUnless( len(sinc.selectedfeatures) > 0 )
+        self.failUnless( len(selected_features) > 0 )
         # mask have origshape
-        self.failUnless( sinc.selectionmask.shape ==\
-                         mask.shape )
+        self.failUnless( sinc.selectionmask.shape == \
+                         self.mask.shape )
 
-        print sinc.cvperf
-        print sinc.cvperf.mean()
-        print stats.ttest_1samp(sinc.cvperf, 0.5)
+
+    def testSerialROIInclusion(self):
+        # make 3 slice roi mask
+        self.mask[0] = 1
+        self.mask[1] = 2
+        self.mask[2] = 3
+
+        # init algorithm
+        sinc = si.SerialFeatureInclusion( self.pattern,
+                                          self.mask,
+                                          ncvfoldsamples=1 )
+
+        # run selection of single features
+        selected_rois = sinc.selectROIs( knn.kNN, verbose=True)
+
+        # only three ROIs max
+        self.failUnless( len( selected_rois ) <= 3 )
+        # each ROI has 9 so total selection number is dividable by 9
+        self.failUnless( np.sum(sinc.selectionmask) % 9 == 0 )
 
 
 def suite():
