28835ed8e31559e33a4bf84391233a95800a63e7
Michael Hanke <michael.hanke@gmail.com>
2007-08-15 15:48:57 +0200

    Implemented meta CV in incr feature search; unittest still incomplete.

--- mvpa/incrsearch.py
+++ mvpa/incrsearch.py
@@ -21,6 +21,7 @@ import numpy as np
 import crossval
 import sys
 import stats
+import copy
 
 class IncrementalFeatureSearch( object ):
     """ Tell me what this is!
@@ -30,6 +31,7 @@ class IncrementalFeatureSearch( object ):
     def __init__( self, pattern,
                         mask,
                         cvtype = 1,
+                        metacvtype = 1,
                         **kwargs ):
 
         """
@@ -54,6 +56,7 @@ class IncrementalFeatureSearch( object ):
         self.__pattern = pattern
         self.__mask = mask
         self.__cvtype = cvtype
+        self.__metacvtype = metacvtype
         self.__cvargs = kwargs
 
         if not mask.shape == pattern.origshape:
@@ -67,24 +70,18 @@ class IncrementalFeatureSearch( object ):
         next run.
         """
         # init the result maps
-        self.__perf = []
-        self.__contingencytbl = None
-        self.__selected_features = None
-        self.__contingency_tbl = None
-        self.__cv_perf = None
+        self.__perfs = []
+        self.__selection_masks = []
+        self.__metacv_origins = []
 
 
-    def __doCrossValidation( self, features, clf, clfargs ):
-        """ Internal method to perform a cross-validation on a subset of
-        features.
+    def __doCrossValidation( self, pattern, clf, clfargs ):
+        """ Internal method to perform a cross-validation on a pattern set.
 
-        Call CrossValidation.run() and returns the CrossValidation object.
+        Calls CrossValidation.run() and returns the CrossValidation object.
         """
-        # select the feature subset from the dataset
-        temp_pat = self.pattern.selectFeatures( features )
-
         # now do cross-validation with the current feature set
-        cv = crossval.CrossValidation( temp_pat, **(self.__cvargs) )
+        cv = crossval.CrossValidation( pattern, **(self.__cvargs) )
         cv.run( clf, cvtype=self.__cvtype, **(clfargs) )
 
         return cv
@@ -100,10 +97,61 @@ class IncrementalFeatureSearch( object ):
         return np.logical_or( selected, self.mask == candidate )
 
 
-    def __doSelection( self, clf, clfargs, featureselector,
+    def __doCVSelection( self, clf, clfargs, featureselector,
+                         selected, candidates, verbose ):
+        # get the list of all combinations of to be excluded folds
+        self.__metacv_origins = crossval.getUniqueLengthNCombinations(
+                                      self.__pattern.originlabels,
+                                      self.__metacvtype )
+
+        # list for the results of all CV folds
+        cv_features = []
+        cv_rois = []
+
+        # meta crossvalidation
+        for exclude in self.__metacv_origins:
+            # split data for meta cross-validation
+            meta_train, meta_test = \
+                crossval.CrossValidation.splitTrainTestData( self.pattern, 
+                                                             exclude )
+
+            # call the backend method that returns a list of the selected
+            # features and the cross-validation object with the best feature set
+            # that can be used to query more detailed results
+            selected_features, selected_rois = \
+                 self.__doSelection( meta_train,
+                                     clf, clfargs,
+                                     featureselector,
+                                     selected,
+                                     candidates,
+                                     verbose )
+
+            # now take the selected features of this meta CV fold and test
+            # their performance on the meta test patterns
+            bestfeature_pat = self.pattern.selectFeatures( selected_features )
+            classifier, predictions = \
+                crossval.CrossValidation.getClassification( 
+                        clf,
+                        bestfeature_pat,
+                        meta_test.selectFeatures( selected_features ),
+                        **(clfargs) )
+
+            # store results
+            self.perfs.append ( ( predictions == meta_test.reg ).mean() )
+            cv_features.append( selected_features )
+            cv_rois.append( selected_rois )
+
+        return cv_features, cv_rois
+
+
+    def __doSelection( self, pattern, clf, clfargs, featureselector,
                        selected, candidates, verbose ):
         """ Backend method for selectFeatures() and selectROIs.
         """
+        # make a copy of the candidates otherwise repeated calls will cause
+        # trouble
+        feat_cand = copy.copy( candidates )
+        
         # yes, it is exactly that
         best_performance_ever = 0.0
 
@@ -118,12 +166,12 @@ class IncrementalFeatureSearch( object ):
         # as long as there are candidates left
         # the loop might get broken earlier if the generalization
         # error does not go down when a new ROI is selected
-        while len( candidates ):
+        while len( feat_cand ):
             # holds the performance value of each candidate
             candidate_rating = []
 
             # for all possible candidates
-            for candidate in candidates:
+            for candidate in feat_cand:
                 # display some status output about the progress if requested
                 if verbose:
                     print "\rTested %i; nselected %i; mean performance: %.3f" \
@@ -135,8 +183,11 @@ class IncrementalFeatureSearch( object ):
                 # take the new candidate and all already selected features
                 features = featureselector( selected, candidate )
 
+                # select the feature subset from the dataset
+                temp_pat = pattern.selectFeatures( features )
+
                 # now do cross-validation with the current feature set
-                cv = self.__doCrossValidation( features,
+                cv = self.__doCrossValidation( temp_pat,
                                                clf,
                                                clfargs )
 
@@ -147,7 +198,7 @@ class IncrementalFeatureSearch( object ):
             rating_array = np.array( candidate_rating )
 
             # determine the best performing additonal candidate (get its id)
-            best_id = candidates[ rating_array.argmax() ]
+            best_id = feat_cand[ rating_array.argmax() ]
 
             # check if the new candidate brings no value
             # if this is the case we are done.
@@ -161,7 +212,7 @@ class IncrementalFeatureSearch( object ):
 
             # and remove it from the candidate features
             # otherwise the while loop could run forever
-            candidates.remove( best_id )
+            feat_cand.remove( best_id )
 
             # update the latest best performance
             best_performance_ever = rating_array.max()
@@ -169,20 +220,13 @@ class IncrementalFeatureSearch( object ):
             # and look for the next best thing (TM)
             selection_counter += 1
 
-        # if new candidates are left or the generalization performance
+        # if no candidates are left or the generalization performance
         # went down
         if verbose:
             # end pending line
             print ''
 
-        # now do cross-validation again with the current best
-        # feature set to have access to the full CV results
-        cv = self.__doCrossValidation( selected,
-                                       clf,
-                                       clfargs )
-
-        # and leave the loop as there is nothing more to do
-        return selected, best_ids, cv
+        return selected, best_ids
 
 
 
@@ -220,17 +264,15 @@ class IncrementalFeatureSearch( object ):
         # call the backend method that returns a list of the selected
         # features and the cross-validation object with the best feature set
         # that can be used to query more detailed results
-        feature_mask, selected_rois, best_cv = \
-             self.__doSelection( classifier, kwargs,
-                                 self.__selectByMaskValue,
-                                 selected_features,
-                                 mask_values,
-                                 verbose )
+        feature_mask, selected_rois = \
+             self.__doCVSelection( classifier, kwargs,
+                                   self.__selectByMaskValue,
+                                   selected_features,
+                                   mask_values,
+                                   verbose )
 
         # store results
-        self.__contingency_tbl = best_cv.contingencytbl
-        self.__cv_perf = np.array( best_cv.perf )
-        self.__selection_mask = feature_mask
+        self.__selection_masks = feature_mask
 
         return selected_rois
 
@@ -270,26 +312,23 @@ class IncrementalFeatureSearch( object ):
         # call the backend method that returns a list of the selected
         # features and the cross-validation object with the best feature set
         # that can be used to query more detailed results
-        selected_features, dummy, best_cv = \
-             self.__doSelection( classifier, kwargs,
-                                 self.__selectByFeatureId,
-                                 selected_features,
-                                 cand_features,
-                                 verbose )
+        selected_features, dummy = \
+             self.__doCVSelection( classifier, kwargs,
+                                   self.__selectByFeatureId,
+                                   selected_features,
+                                   cand_features,
+                                   verbose )
 
         # store results
-        self.__contingency_tbl = best_cv.contingencytbl
-        self.__cv_perf = np.array( best_cv.perf )
-        self.__selection_mask = \
-            self.pattern.features2origmask( selected_features )
+        self.__selection_masks = \
+            [ self.pattern.features2origmask( f ) for f in selected_features ]
 
         return selected_features
 
 
     # access to the results
-    selectionmask = property( fget=lambda self: self.__selection_mask )
-    cvperf = property( fget=lambda self: self.__cv_perf )
-    contingencytbl = property( fget=lambda self: self.__contingency_tbl )
+    selectionmasks = property( fget=lambda self: self.__selection_masks )
+    perfs = property( fget=lambda self: self.__perfs )
 
     # other data access
     pattern = property( fget=lambda self: self.__pattern )
