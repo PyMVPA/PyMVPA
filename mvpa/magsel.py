19803eefdd73328d515b6aa6c6da06b6183b07c4
Michael Hanke <michael.hanke@gmail.com>
2007-10-13 16:06:30 +0200

    Added initial magsel.

---
+++ mvpa/magsel.py
@@ -0,0 +1,77 @@
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#    PyMVPA: Feature selcetion by weight magnitude
+#
+#    Copyright (C) 2007 by
+#    Michael Hanke <michael.hanke@gmail.com>
+#
+#    This package is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2 of the License, or (at your option) any later version.
+#
+#    This package is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+
+import numpy as N
+import support
+
+
+class MagnitudeFeatureSelection( object ):
+    """
+    """
+    def __init__( self,
+                  ntoselect,
+                  select_by = 'fraction',
+                  verbose=False ):
+        """
+        """
+        self.__ntoselect = ntoselect
+        self.__select_by = select_by
+        self.__verbose = verbose
+
+
+    def selectFeatures( self, pattern, classifier ):
+        """
+        """
+        # train clf on given dataset and store performance on training set
+        classifier.train( pattern )
+
+        # feature rating: large values == important
+        fr = classifier.getFeatureBenchmark()
+
+        # determine the number of features to be selected
+        if self.__select_by == 'fraction':
+            nselect = int( len(featrank) * self.__ntoselect )
+        elif self.__select_by == 'number':
+            nselect = self.__ntoselect
+        else:
+            raise ValueError, 'Unknown elimination method: ' \
+                              + str(self.__select_by)
+
+        if self.__verbose:
+            print "Selecting", nselect, "features."
+
+        # map the weights into the orig feature space
+        weight_map = N.zeros( pattern.origshape, dtype='float32' )
+        weight_map[pattern.getFeatureMask(copy=False) > 0] = fr
+
+        # return the first 'nselect' features (with the highest ranking)
+        # and the map of all feature weights
+        return pattern.selectFeaturesById( fr.argsort()[:nselect] ), weight_map
+
+
+    def setVerbosity( self, value ):
+        """ If verbosity is set to True, some status messages will be printed
+        during recursive feature selection.
+        """
+        self.__verbose = value
+
+
+    # properties
+    verbose = property( fget=lambda self: self.__verbose,
+                        fset=setVerbosity )
---
+++ tests/test_magsel.py
@@ -0,0 +1,82 @@
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#    Unit tests for PyMVPA incremental feature search algorithm
+#
+#    Copyright (C) 2007 by
+#    Michael Hanke <michael.hanke@gmail.com>
+#
+#    This package is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    version 2 of the License, or (at your option) any later version.
+#
+#    This package is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+
+import mvpa, mvpa.magsel, mvpa.svm, mvpa.featsel
+import unittest
+import numpy as N
+
+class MagSelTests(unittest.TestCase):
+
+    def setUp(self):
+        # prepare demo dataset and mask
+        self.mask = N.ones((3,3,3))
+        data = N.random.uniform(0,1,(100,) + (self.mask.shape))
+        reg = N.arange(100) / 50
+        orig = range(10) * 10
+        self.pattern = mvpa.MVPAPattern(data, reg, orig)
+
+
+
+    def testMagSel(self):
+        # init algorithm
+        fselect = \
+            mvpa.magsel.MagnitudeFeatureSelection( 10,
+                                                   select_by = 'number',
+                                                   verbose=True )
+
+        # run selection of single features
+        selected, frating = fselect.selectFeatures( self.pattern,
+                                                    mvpa.svm.SVM() )
+
+        self.failUnless( selected.nfeatures == 10 )
+
+        self.failUnless( frating.shape == self.pattern.origshape )
+        self.failIf( (frating == 0.0).any() )
+
+        # mask have origshape
+        self.failUnless( selected.origshape == \
+                         self.mask.shape )
+
+
+    def testCVMagSel(self):
+        clf = mvpa.svm.SVM( kernel_type=mvpa.svm.libsvm.LINEAR,
+                            svm_type = mvpa.svm.libsvm.NU_SVC,
+                            nu = 0.7,
+                            eps = 0.1 )
+        fselect = \
+            mvpa.magsel.MagnitudeFeatureSelection( 10,
+                                                   select_by = 'number',
+                                                   verbose=True )
+        fsv = mvpa.featsel.FeatSelValidation( self.pattern, cvtype=1 )
+        fsv( fselect, clf )
+
+        print fsv.perfs
+        print N.mean(fsv.perfs)
+        print fsv.getMeanRatingMap()
+
+        clf.train(self.pattern)
+        print clf.getFeatureBenchmark()
+
+
+def suite():
+    return unittest.makeSuite(MagSelTests)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
