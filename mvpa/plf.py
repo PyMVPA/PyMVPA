0c3c0d71c4fbc2deac26eff9883a2c8417615726
Per B. Sederberg <per@sync.(none)>
2007-10-18 17:31:15 -0400

    Starting to clean up logistic regression

--- mvpa/plf.py
+++ mvpa/plf.py
@@ -23,13 +23,13 @@ class IterationError(Exception):
     pass
 
 class PLF:
-    def __init__(self,data,lm=1,criterion=1,reduce=False,maxiter=20,verbose=False):
+    def __init__(self,lm=1,criterion=1,reduce=False,maxiter=20,verbose=False):
         """
         Initialize a penalized logistic regression analysis
 
         Input:
         =====
-        data   is a MVPApattern object containing the data
+
         lm     the penalty term lambda
         criterion is the criterion applied to judge convergence
         if reduce is not False, the rank of the data is reduced before
@@ -41,7 +41,6 @@ class PLF:
                this number of iterations, an exception is raised
         verbose switches on/off logging information to stderr
         """
-        self.__data = data
         self.__lm   = lm
         self.__criterion = criterion
         self.__reduce = reduce
@@ -50,10 +49,13 @@ class PLF:
 
         self.__train()
 
-    def __train(self):
+    def __train(self,data):
+        """
+        data   is a MVPApattern object containing the data
+        """
         # Set up the environment for fitting the data
-        X = self.__data.pattern.T
-        d = self.__data.reg
+        X = data.pattern.T
+        d = data.reg
         if not list(set(d))==[0,1]:
             raise ValueError, "Regressors for logistic regression should be [0,1]"
 
