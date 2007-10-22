cbc2f1a584c05126d25cd7672046a0a40d96f284
Michael Hanke <michael.hanke@gmail.com>
2007-10-22 05:42:45 +0200

    Switched license from LGPL to MIT (in code where I hold the copyright ;-)

--- mvpa/plf.py
+++ mvpa/plf.py
@@ -6,14 +6,12 @@
 #    Ingo Fruend <ingo.fruend@gmail.com>
 #
 #    This package is free software; you can redistribute it and/or
-#    modify it under the terms of the GNU Lesser General Public
-#    License as published by the Free Software Foundation; either
-#    version 2 of the License, or (at your option) any later version.
+#    modify it under the terms of the MIT License.
 #
 #    This package is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#    Lesser General Public License for more details.
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the COPYING
+#    file that comes with this package for more details.
 #
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 
--- tests/maskeddataset.py
+++ tests/maskeddataset.py
@@ -6,13 +6,12 @@
 #    Michael Hanke <michael.hanke@gmail.com>
 #
 #    This package is free software; you can redistribute it and/or
-#    modify it under the terms of the GNU Lesser General Public
-#    version 2 of the License, or (at your option) any later version.
+#    modify it under the terms of the MIT License.
 #
 #    This package is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#    Lesser General Public License for more details.
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the COPYING
+#    file that comes with this package for more details.
 #
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 
