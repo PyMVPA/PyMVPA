60a9dae12065ec988a168c8e231b699956d1c440
Yaroslav Halchenko <git@onerussian.com>
2007-10-24 23:56:43 -0400

    initial standartization, efficiency adjustment of maskmapper
    * Silent some pylint complaints (see pylintrc)
    * In all impacted files
      - added editor settings header
      - "docstring" moved from #-ed comment into a first line of the module
        so now it makes sense to call smth like "?mapper" from ipython ;)
    * In the process to tune up maskmapper to trade some space for
      efficiency

--- mvpa/mapper.py
+++ mvpa/mapper.py
@@ -1,4 +1,4 @@
-#emacs: -*- mode: python-mode; py-indent-offset: 4; tab-width: 4; indent-tabs-mode: nil -*-
+#emacs: -*- mode: python-mode; py-indent-offset: 4; indent-tabs-mode: nil -*-
 #ex: set sts=4 ts=4 sw=4 et:
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 #
@@ -16,6 +16,7 @@
 #    file that comes with this package for more details.
 #
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+"""PyMVPA: Module that contains abstract base class of all data mappers"""
 
 class Mapper(object):
     """
@@ -38,40 +39,33 @@ class Mapper(object):
     def __init__(self):
         pass
 
-
     def forward(self, data):
         """ Map data from the original dataspace into featurespace.
         """
         raise NotImplementedError
-
 
     def __getitem__(self, data):
         """ Calls the mappers forward() method.
         """
         return self.reverse(data)
 
-
     def reverse(self, data):
         """ Reverse map data from featurespace into the original dataspace.
         """
         raise NotImplementedError
-
 
     def __call__(self, data):
         """ Calls the mappers reverse() method.
         """
         return self.reverse(data)
 
-    # XXX -- should be deprecated and  might be substituted
-    # with functions like  getEmptyFrom / getEmptyTo
-    #
     def getInShape(self):
-        """
-        Returns the shape (or other dimensionality speicification)
-        of the original dataspace.
+        """ Returns the dimensionality speicification of the original dataspace.
+
+        XXX -- should be deprecated and  might be substituted
+        with functions like  getEmptyFrom / getEmptyTo
         """
         raise NotImplementedError
-
 
     def getOutShape(self):
         """
@@ -85,21 +79,18 @@ class Mapper(object):
         """ Returns the size of the entity in input space """
         raise NotImplementedError
 
-
     def getOutSize(self):
         """ Returns the size of the entity in output space """
         raise NotImplementedError
 
-
-    def getInEmpty(self):
-        """ Returns empty instance of input object """
-        raise NotImplementedError
-
-
-    def getOutEmpty(self):
-        """ Returns empty instance of output object """
-        raise NotImplementedError
-
+# comment out for now... introduce when needed
+#    def getInEmpty(self):
+#        """ Returns empty instance of input object """
+#        raise NotImplementedError
+#
+#    def getOutEmpty(self):
+#        """ Returns empty instance of output object """
+#        raise NotImplementedError
 
     def getInId(self, outId):
         """For a given Id in "out" returns corresponding "in" Id """
@@ -114,10 +105,9 @@ class Mapper(object):
         raise NotImplementedError
 
 
-
-
-
 ### yoh: To think about generalization
 ##
-## getMask... it might be more generic... so far seems to be specific for featsel and rfe
+## getMask... it might be more generic... so far seems to be
+##            specific for featsel and rfe
 ## buildMaskFromFeatureIds ... used in ifs
+
