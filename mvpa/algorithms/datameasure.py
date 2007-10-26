6730d344a1892ddb6dfafa85ebe7dc2eb3fa2eed
Michael Hanke <michael.hanke@gmail.com>
2007-10-26 22:47:27 +0200

    Added DataMeasure class that shall serve as a base-class for the SensitivityAnalysers.
    It is put in new submodule 'algorithms'. Old 'crossval' and 'searchlight'
    are moved into that submodule as well.
    
    Some tests are currently broken.

---
+++ mvpa/algorithms/datameasure.py
@@ -0,0 +1,28 @@
+#emacs: -*- mode: python-mode; py-indent-offset: 4; indent-tabs-mode: nil -*-
+#ex: set sts=4 ts=4 sw=4 et:
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the PyMVPA package for the
+#   copyright and license terms.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+"""PyMVPA: base class for data measures, algorithms that quantify properties of
+           datasets."""
+
+
+class DataMeasure(object):
+    """
+    All subclasses shall get all necessary parameters via their constructor,
+    so it is possible to get the same type of measure for multiple datasets
+    by passing them to the __call__() method successively.
+    """
+    def __call(self, dataset, callbacks=[]):
+        """
+        Each implementation has to handle two arguments. The first is the
+        source dataset and the second is a list of callables which have to be
+        called with the result of the computation.
+
+        Returns the computed measure in some iterable (list-like) container.
+        """
+        raise NotImplementedError
+
