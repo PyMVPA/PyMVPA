c66e3cadc1063e0a90e2527048bd08447848e4c9
Yaroslav Halchenko <debian@onerussian.com>
2007-10-26 18:35:13 -0400

    rewrite the headers of all .py using the script replace_header.sh
    To redo effect of this commit (if modification is needed), it is
    sufficient to
    git reset --hard dd96897; sh  doc/misc/replace_header.sh
    
    Commit changed 51 file! ;-) enjoy the merge

--- mvpa/misc/fsl/flobs.py
+++ mvpa/misc/fsl/flobs.py
@@ -1,19 +1,12 @@
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+#emacs: -*- mode: python-mode; py-indent-offset: 4; indent-tabs-mode: nil -*-
+#ex: set sts=4 ts=4 sw=4 et:
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 #
-#    Wrapper around FSLs halfcosbasis to generate HRF kernels
+#   See COPYING file distributed along with the PyMVPA package for the
+#   copyright and license terms.
 #
-#    Copyright (C) 2007 by
-#    Michael Hanke <michael.hanke@gmail.com>
-#
-#    This is free software; you can redistribute it and/or
-#    modify it under the terms of the MIT License.
-#
-#    This package is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the COPYING
-#    file that comes with this package for more details.
-#
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+"""PyMVPA: Wrapper around FSLs halfcosbasis to generate HRF kernels"""
 
 import os
 import tempfile
--- mvpa/mmatchprocessor.py
+++ mvpa/mmatchprocessor.py
@@ -1,20 +1,12 @@
+#emacs: -*- mode: python-mode; py-indent-offset: 4; indent-tabs-mode: nil -*-
+#ex: set sts=4 ts=4 sw=4 et:
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 #
-#    PyMVPA: Compute the percentage of correct classifications of
-#            cross-validation folds
-#
-#    Copyright (C) 2007 by
-#    Michael Hanke <michael.hanke@gmail.com>
-#
-#    This package is free software; you can redistribute it and/or
-#    modify it under the terms of the MIT License.
-#
-#    This package is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the COPYING
-#    file that comes with this package for more details.
+#   See COPYING file distributed along with the PyMVPA package for the
+#   copyright and license terms.
 #
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+"""PyMVPA: Compute the percentage of correct classifications of"""
 
 from mvpa.misc.errorfx import MeanMatchErrorFx
 from mvpa.splitprocessor import *
