9dff3b92642879b3c87f649808c237f71c36e125
Yaroslav Halchenko <debian@onerussian.com>
2007-11-05 22:58:06 -0500

    RF: more rfe refactoring

--- mvpa/clf/rfe.py
+++ mvpa/clf/rfe.py
@@ -10,6 +10,8 @@
 
 import numpy as N
 
+from math import floor, ceil
+
 from mvpa.misc.support import buildConfusionMatrix
 from mvpa.misc.exceptions import UnknownStateError
 from mvpa.misc.vproperty import VProperty
@@ -76,7 +78,8 @@ class StopNBackHistoryCriterion(StoppingCriterion):
         `steps`: int, for how many steps to check after optimal value
         `fun`: functor, to select the best results. Defaults to min
         """
-        if steps<0:
+        StoppingCriterion.__init__(self)
+        if steps < 0:
             raise ValueError, \
                   "Number of steps (got %d) should be non-negative" % steps
         self.__steps = steps
@@ -99,12 +102,13 @@ class StopNBackHistoryCriterion(StoppingCriterion):
             isbest = True
 
         # if number of elements after the min >= len -- stop
-        if len(errors) - minindex >= self.__steps:
+        if len(errors) - minindex > self.__steps:
             stop = True
 
         return (stop, isbest)
 
     steps = property(fget=lambda x:x.__steps)
+
 
 class FeatureSelector(object):
     """Base class to implement functors to select the set of properties
@@ -124,11 +128,8 @@ class XPercentFeatureSelector(FeatureSelector):
     def __init__(self, perc_discard=5.0, removeminimal=True):
         """XXX???
         """
-        if perc_discard>100 or perc_discard<0:
-            raise ValueError, \
-                  "Percentage (%f) cannot be outside of [0,100]" \
-                  % perc_discard
-        self.__perc_discard = perc_discard
+        self.__perc_discard = None      # pylint should smile
+        self.perc_discard = perc_discard
         self.__removeminimal = removeminimal
         self.__ndiscarded = None
         """Store number of discarded since for a given value we might
@@ -148,7 +149,7 @@ class XPercentFeatureSelector(FeatureSelector):
     def __call__(self, sensitivity):
         nfeatures = len(sensitivity)
         # how many to discard
-        nremove = self.__perc * nfeatures * 1.0 / 100.0
+        nremove = int(floor(self.__perc_discard * nfeatures * 1.0 / 100.0))
         sensmap = N.array(sensitivity)  # assure that it is ndarray
         sensmap2 = sensmap.copy()       # make a copy to sort
         sensmap2.sort()                 # sort inplace
@@ -162,7 +163,7 @@ class XPercentFeatureSelector(FeatureSelector):
         return good_ids
 
 
-    def getNDiscarded(self):
+    def _getNDiscarded(self):
         """Return number of discarded elements
 
         Raises an UnknownStateError exception if the instance wasn't
@@ -172,7 +173,19 @@ class XPercentFeatureSelector(FeatureSelector):
             raise UnknownStateError
         return self.__ndiscarded
 
-    ndiscarded = property(fget=getNDiscarded)
+
+    def _setPercDiscard(self, perc_discard):
+        """How many percent to discard"""
+        if perc_discard>100 or perc_discard<0:
+            raise ValueError, \
+                  "Percentage (%f) cannot be outside of [0,100]" \
+                  % perc_discard
+        self.__perc_discard = perc_discard
+
+
+    ndiscarded = property(fget=_getNDiscarded)
+    perc_discard = property(fget=lambda x:x.__perc_discard,
+                            fset=_setPercDiscard)
 
 # TODO: Abs value of sensitivity should be able to rule RFE
 # Often it is what abs value of the sensitivity is what matters.
@@ -185,18 +198,18 @@ class RFE(FeatureSelection):
     """
 
     def __init__(self,
-                 sensana,
-                 feature_selector=XPercentFeatureSelector,
-                 stopping_criterion=StopNBackHistoryCriterion,
+                 sensitivity_analyzer,
+                 feature_selector=XPercentFeatureSelector(),
+                 stopping_criterion=StopNBackHistoryCriterion(),
                  error_oracle=None
                  ):
         """ Initialize recurse feature elimination
-        `sensana`: `SensitivityAnalyzer`
+        `sensitivity_analyzer`: `SensitivityAnalyzer`
         `feature_selector`: functor
         `stopping_criterion`: functor
         """
 
-        self.__sensana = sensana
+        self.__sensitivity_analyzer = sensitivity_analyzer
         """Sensitivity analyzer used to call at each step"""
 
         self.__feature_selector = feature_selector
@@ -217,7 +230,7 @@ class RFE(FeatureSelection):
 
         while dataset.nfeatures>0:
             # Compute
-            sensitivity = self.__sensana(dataset)
+            sensitivity = self.__sensitivity_analyzer(dataset)
             # Record the error
             errors.append(error_oracle(dataset))
             # Check if it is time to stop and if we got
@@ -247,170 +260,3 @@ class RFE(FeatureSelection):
 
         return result
 
-    def __trainClassifier(self):
-        self.__clf.train( self.__pattern )
-        self.__testClassifierOnTrainingSet()
-
-        # feature rating: large values == important
-        fr = self.__clf.getFeatureBenchmark()
-
-
-    def __testClassifierOnTrainingSet(self):
-        predictions = N.array(self.__clf.predict( self.pattern.samples ))
-
-        self.__training_confusion_mat = \
-            buildConfusionMatrix( self.pattern.reglabels,
-                                  self.pattern.regs,
-                                  predictions )
-        self.__training_perf = \
-            float(self.trainconfmat.diagonal().sum()) / self.trainconfmat.sum()
-
-
-    def selectFeatures( self, n, eliminate_by = 'fraction',
-                        kill_per_iter = 0.1 ):
-        """ Select n predictive features from the dataset.
-
-        This method performs as many iterations as necessary until only 'n'
-        feature are remaining. The number of iteration depend on the elimination
-        method that is used.
-
-        'eliminate_by' and 'kill_per_iter' are used to determine how many
-        features are removed in each iteration.
-
-        Supported values for 'eliminate_by' and the associated meaning of
-        'kill_per_iter' are listed below:
-
-        eliminate_by: 'fraction'
-            in this case 100*'kill_per_iter' represents the percentage of
-            features that are removed at each iteration
-
-        eliminate_by: 'number'
-            each iteration removed 'number' features
-        """
-        # are we talking integers?
-        n = int( n )
-
-        # elimination log mask
-        elim_mask = N.zeros( self.pattern.mapper.dsshape, dtype='uint' )
-
-        # do it until there are more feature than there should be
-        while n < self.pattern.nfeatures:
-            # get importance of each remaining feature
-            featrank = self.__clf.getFeatureBenchmark()
-
-            # determine the number of features to be eliminated
-            if eliminate_by == 'fraction':
-                nkill = int( len(featrank) * kill_per_iter )
-            elif eliminate_by == 'number':
-                nkill = kill_per_iter
-            else:
-                raise ValueError, 'Unknown elimination method: ' \
-                                  + str(eliminate_by)
-
-            # limit number of eliminations if necessary (would have less than
-            # n features after elimination)
-            if nkill > self.pattern.nfeatures - n:
-                nkill = self.pattern.nfeatures - n
-
-            # make sure to remove one feature at least to prevent endless
-            # loops
-            if nkill < 1:
-                nkill = 1
-
-            if self.__verbose:
-                print "Removing", nkill, "features.",
-
-            # eliminate the first 'nkill' features (with the lowest ranking)
-            self.__pattern = \
-                self.pattern.selectFeatures( featrank.argsort()[nkill:] )
-
-            # increment all features still present in the elim mask
-            elim_mask[ self.pattern.mapper.getMask(copy=False) ] += 1
-
-            # retrain the classifier with the new feature set
-            self.__trainClassifier()
-
-            if self.__verbose:
-                print "New performance:", self.trainperf
-
-        return elim_mask
-
-
-    def killNFeatures( self, n,
-                       eliminate_by = 'number',
-                       kill_per_iter = 1 ):
-        """ Eliminates the 'n' least predictive features.
-
-        Please see the documentation of the selectFeatures() methods for
-        information about the remaining arguments.
-        """
-        self.selectFeatures( self.pattern.nfeatures - n,
-                             eliminate_by = eliminate_by,
-                             kill_per_iter = kill_per_iter )
-
-
-    def killFeatureFraction( self, frac,
-                             eliminate_by = 'fraction',
-                             kill_per_iter = 0.1 ):
-        """ Eliminates the 100*frac percent least predictive features.
-
-        Please see the documentation of the selectFeatures() methods for
-        information about the remaining arguments.
-        """
-        nkill = int(frac * self.pattern.nfeatures)
-        self.selectFeatures( self.pattern.nfeatures - nkill,
-                             eliminate_by = eliminate_by,
-                             kill_per_iter = kill_per_iter )
-
-
-    def testSelection( self, pattern ):
-        """ Test the performance of the current feature set on a given dataset.
-
-        The datasets in 'pattern' (MVPAPattern) are passed to the internal
-        classifier object. No retraining is performed. The classifiers
-        predictions are compared to the regressor values in 'pattern'.
-
-        'pattern' either has to provide the same number of features as the
-        current feature set or has to match the original data shape. In case of
-        the latter an automatic feature selection is performed and only those
-        features from 'pattern' are used for the prediction that are also
-        present in the current internal feature set.
-
-        The method returns a 3-tuple:
-            ( list of classifier prediction (one per dataset),
-              mean classifier performance (0 < x < 1),
-              confusion matrix of classifications )
-        """
-        if not pattern.nfeatures == self.pattern.nfeatures:
-            # select necessary features from the provided patterns
-            masked = pattern.selectFeaturesByMask( 
-                            self.pattern.mapper.getMask() )
-        else:
-            masked = pattern
-
-        # get the predictions from the classifier
-        predicted = self.clf.predict( masked.samples )
-
-        confmat = buildConfusionMatrix( self.pattern.reglabels,
-                                        masked.regs,
-                                        predicted )
-
-        perf = N.mean( predicted == masked.regs )
-
-        return predicted, perf, confmat
-
-
-    def setVerbosity( self, value ):
-        """ If verbosity is set to True, some status messages will be printed
-        during recursive feature selection.
-        """
-        self.__verbose = value
-
-
-    # properties
-    pattern = property( fget=lambda self: self.__pattern )
-    verbosity = property( fget=lambda self: self.__verbose,
-                          fset=setVerbosity )
-    clf = property( fget=lambda self: self.__clf )
-    trainconfmat = property( fget=lambda self: self.__training_confusion_mat )
-    trainperf = property( fget=lambda self: self.__training_perf )
--- tests/test_rfe.py
+++ tests/test_rfe.py
@@ -12,13 +12,16 @@ import unittest
 import numpy as N
 
 from mvpa.datasets.maskeddataset import MaskedDataset
-from mvpa.clf.rfe import RFE, StopNBackHistoryCriterion
+from mvpa.clf.rfe import RFE, \
+     StopNBackHistoryCriterion, XPercentFeatureSelector
 #from mvpa.clf.svm import SVM
 
+from mvpa.misc.exceptions import UnknownStateError
 
 class RFETests(unittest.TestCase):
 
     def testStopCriterion(self):
+        """Test stopping criterions"""
         stopcrit = StopNBackHistoryCriterion()
         # for empty history -- no best but just go
         self.failUnless(stopcrit([]) == (False, False))
@@ -39,7 +42,23 @@ class RFETests(unittest.TestCase):
         self.failUnless(stopcrit([0.8, 0.9, 1.0]+[0.9]*9) == (False, False))
         self.failUnless(stopcrit([0.8, 0.9, 1.0]+[0.9]*10) == (True, False))
 
-        
+
+    def testFeatureSelector(self):
+        """Test feature selector"""
+        # remove 10% weekest
+        selector = XPercentFeatureSelector(10)
+        dataset = N.array([3.5, 10, 7, 5, -0.4, 0, 0, 2, 10, 9])
+        target10 = N.array([3.5, 10, 7, 5, 0, 0, 2, 10, 9])
+        target20 = N.array([3.5, 10, 7, 5, 2, 10, 9])
+
+        self.failUnlessRaises(UnknownStateError, selector._getNDiscarded)
+        self.failUnless((selector(dataset) == target10).all())
+        selector.perc_discard = 20      # discard 20%
+                                        # but since there are 2 0s
+        self.failUnless((selector(dataset) == target20).all())
+        self.failUnless(selector.ndiscarded == 3) # se 3 were discarded
+
+        # XXX more needed
 
 def suite():
     return unittest.makeSuite(RFETests)
