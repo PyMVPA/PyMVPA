edaee9dfe4111e5de8047911f1de29ab9631c1eb
Yaroslav Halchenko <debian@onerussian.com>
2007-11-15 00:14:00 -0500

    NF+RF: added FixedNumberFeatureSelector
    now both FixedNumberFeatureSelector and XPercentFeatureSelector are
    children of the TailFeatureSelector. I hate all the names, Michael
    help! ;-)
    It might be that both FixedNumberFeatureSelector and
    XPercentFeatureSelector should be even merged into a single selector
    which would be doing selection by percent until it reaches
    prespectified level and then doing by a fixed amount (like I did it before)...

--- mvpa/misc/state.py
+++ mvpa/misc/state.py
@@ -13,23 +13,33 @@ __docformat__ = 'restructuredtext'
 from mvpa.misc.exceptions import UnknownStateError
 
 class State(dict):
+
     def __init__(self, *args, **kwargs):
+
         self.__registered = []
         dict.__init__(self, *args, **kwargs)
 
-    def __setitem__(self, *args, **kwargs):
-        raise NotImplementedError
-
-    def __getitem__(self, index):
+    def __checkIndex(self, index):
         if not index in self.__registered:
             raise KeyError, \
                   "State of %s has no key %s registered" \
                   % (self.__class__.__name__, index)
 
+    # actually we have to provide some simple way to set the State
+    def __setitem__(self, index, *args, **kwargs):
+        self.__checkIndex(index)
+        dict.__setitem__(self, index, *args, **kwargs)
+
+    def __getitem__(self, index):
+        self.__checkIndex(index)
         if not self.has_key(index):
-            raise UnknownStateError
+            raise UnknownStateError("Unknown yet value for '%s'" % index)
+        else:
+            return dict.__getitem__(self, index)
 
         return dict.__getitem__(self, index)
-
+    # XXX think about it -- may be it is worth making whole State
+    # handling via static methods to remove any possible overhead of
+    # registering the same keys in each constructor
     def _register(self, key):
         self.__registered.append(key)
