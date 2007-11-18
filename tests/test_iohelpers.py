8edd4b118240270da896ff64c96813e96402f08d
Michael Hanke <michael.hanke@gmail.com>
2007-11-18 22:16:00 +0100

    NF: Added iohelper for reading FSL's EV3 format.

--- tests/test_iohelpers.py
+++ tests/test_iohelpers.py
@@ -13,7 +13,7 @@ import unittest
 from tempfile import mkstemp
 import numpy as N
 
-from mvpa.misc.iohelpers import ColumnDataFromFile
+from mvpa.misc.iohelpers import ColumnDataFromFile, FslEV3
 
 
 class IOHelperTests(unittest.TestCase):
@@ -40,6 +40,34 @@ class IOHelperTests(unittest.TestCase):
 
         # cleanup
         os.remove(fpath)
+
+
+    def testFslEV(self):
+        ex1 = """0.0 2.0 1
+        13.89 2 1
+        16 2.0 0.5
+        """
+        file, fpath = mkstemp('mvpa', 'test')
+        file = open(fpath, 'w')
+        file.write(ex1)
+        file.close()
+
+        # intentionally rely on defaults
+        d = FslEV3(fpath)
+
+        # check header (sort because order in dict is unpredictable)
+        self.failUnless(sorted(d.keys()) == ['duration','intensity','onset'])
+
+        self.failUnless(d['onset'] == [0.0, 13.89, 16.0])
+        self.failUnless(d['duration'] == [2.0, 2.0, 2.0])
+        self.failUnless(d['intensity'] == [1.0, 1.0, 0.5])
+
+        self.failUnless(d.getNEVs() == 3)
+        self.failUnless(d.getEV(1) == (13.89, 2.0, 1.0))
+        # cleanup
+        os.remove(fpath)
+
+
 
 
 def suite():
