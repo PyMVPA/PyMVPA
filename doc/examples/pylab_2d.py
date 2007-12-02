954a55a55f68952872e667470cd9174296ba6530
Per B. Sederberg <psederberg@gmail.com>
2007-12-02 11:21:05 -0500

    BF: Added in license info to top of example.

--- doc/examples/pylab_2d.py
+++ doc/examples/pylab_2d.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+#emacs: -*- mode: python-mode; py-indent-offset: 4; indent-tabs-mode: nil -*-
+#ex: set sts=4 ts=4 sw=4 et:
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the PyMVPA package for the
+#   copyright and license terms.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+"""Example demonstrating a simple classifiction of a 2-D dataset"""
+
 import numpy as N
 import pylab
 
