21ca31c84d37d0d31f316c88240116f5d8c4a056
Per B. Sederberg <psederberg@gmail.com>
2007-12-02 11:17:18 -0500

    Added simple classification example.

---
+++ doc/examples/pylab_2d.py
@@ -0,0 +1,57 @@
+import numpy as N
+import pylab
+
+# local imports
+from mvpa.datasets.dataset import Dataset
+from mvpa.clf.plf import PLF
+
+# set up the labeled data
+# two skewed  2-D distributions
+num_dat=200
+dist = 4
+feat_pos=N.random.randn(2,num_dat)
+feat_pos[0,:] *= 2.
+feat_pos[1,:] *= .5
+feat_pos[0,:] += dist
+feat_neg=N.random.randn(2,num_dat)
+feat_neg[0,:] *= .5
+feat_neg[1,:] *= 2.
+feat_neg[0,:] -= dist
+
+# plot those points
+pylab.clf()
+pylab.plot(feat_pos[0,:], feat_pos[1,:], "r.") ;
+pylab.plot(feat_neg[0,:], feat_neg[1,:], "b.") ;
+
+# create the pymvpa dataset from the labeled features
+patternsPos = Dataset(samples=feat_pos.T,labels=1)
+patternsNeg = Dataset(samples=feat_neg.T,labels=0)
+patterns = patternsPos+patternsNeg
+
+# set up the classifier
+logReg = PLF(criterion=0.00001)
+logReg.train(patterns)
+
+# set up the testing features
+x1=N.linspace(-10,10, 100)
+x2=N.linspace(-10,10, 100)
+x,y=N.meshgrid(x1,x2);
+feat_test=N.array((N.ravel(x), N.ravel(y)))
+
+# this is need b/c I can't access the __f function in PLF
+def logistic(y):
+    return 1./(1+N.exp(-y))
+
+# do the regression prediction (this would be ideally part of the
+# classifier)
+res = N.ravel(logistic(logReg.offset+feat_test.T*logReg.w))
+
+# reshape the results
+z = N.asarray(res).reshape((100,100))
+
+# plot the predictions
+pylab.pcolor(x, y, z, shading='interp')
+pylab.clim(0,1)
+pylab.colorbar()
+pylab.contour(x, y, z, linewidths=1, colors='black', hold=True)
+pylab.show()
