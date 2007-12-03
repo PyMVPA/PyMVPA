3adede605874581fa0aaf68203496a08f9ce8651
Michael Hanke <michael.hanke@gmail.com>
2007-12-03 09:33:14 +0100

    BF: Added a unit test for PLF that passes and put it in main test battery.

--- tests/test_plf.py
+++ tests/test_plf.py
@@ -13,71 +13,26 @@ from mvpa.datasets.dataset import Dataset
 from mvpa.clf.plf import PLF
 import numpy as N
 
+def dumbFeatureSignal():
+    data = [[1,0],[1,1],[2,0],[2,1],[3,0],[3,1],[4,0],[4,1],
+            [5,0],[5,1],[6,0],[6,1],[7,0],[7,1],[8,0],[8,1],
+            [9,0],[9,1],[10,0],[10,1],[11,0],[11,1],[12,0],[12,1]]
+    regs = [0 for i in range(12)] \
+         + [1 for i in range(12)]
 
-def pureMultivariateSignal(patterns, signal2noise = 1.5):
-    """ Create a 2d dataset with a clear multivariate signal, but no
-    univariate information.
-
-    %%%%%%%%%
-    % O % X %
-    %%%%%%%%%
-    % X % O %
-    %%%%%%%%%
-    """
-
-    # start with noise
-    data=N.random.normal(size=(4*patterns,2))
-
-    # add signal
-    data[:2*patterns,1] += signal2noise
-    data[2*patterns:4*patterns,1] -= signal2noise
-    data[:patterns,0] -= signal2noise
-    data[2*patterns:3*patterns,0] -= signal2noise
-    data[patterns:2+patterns,0] += signal2noise
-    data[3*patterns:4*patterns,0] += signal2noise
-
-    # two conditions
-    regs = [0 for i in xrange(patterns)] \
-        + [1 for i in xrange(patterns)] \
-        + [1 for i in xrange(patterns)] \
-        + [0 for i in xrange(patterns)]
-    regs = N.array(regs)
-
-    return Dataset(data, regs, None)
-
+    return Dataset(samples=data, labels=regs)
 
 class PLFTests(unittest.TestCase):
 
+    def testPLF(self):
+        data = dumbFeatureSignal()
 
-    def testMultivariate(self):
+        clf = PLF()
 
-        mv_perf = []
-        uv_perf = []
+        clf.train(data)
 
-        for i in xrange(20):
-            train = pureMultivariateSignal( 20, 3 )
-            test = pureMultivariateSignal( 20, 3 )
-
-            k_mv = PLF()
-            k_mv.train(train)
-            p_mv = k_mv.predict( test.samples )
-            mv_perf.append( N.mean(p_mv==test.labels) )
-
-            k_uv = PLF()
-            k_uv.train(train.selectFeatures([0]))
-            p_uv = k_uv.predict( test.selectFeatures([0]).samples )
-            uv_perf.append( N.mean(p_uv==test.labels) )
-
-        mean_mv_perf = N.mean(mv_perf)
-        mean_uv_perf = N.mean(uv_perf)
-
-        print mean_mv_perf
-        print mean_uv_perf
-
-        # multivariate should be perfect
-        self.failUnless( mean_mv_perf > 0.9 )
-        # univariate should be worse
-        self.failUnless( mean_uv_perf < mean_mv_perf )
+        # prediction has to be perfect
+        self.failUnless((clf.predict(data.samples) == data.labels).all())
 
 
 def suite():
