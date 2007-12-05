9138e0dc82878d5142988dc63dae89050992feba
Michael Hanke <michael.hanke@gmail.com>
2007-12-05 21:09:33 +0100

    RF: Refurbished kNN. Provides raw voting numbers for introspection.
    - Made it a proper classifier by subclassing 'Classifier'.
    - Simplified the code here and there.
    - Minor doc improvements.
    - Added unit test for new state members.

--- tests/test_knn.py
+++ tests/test_knn.py
@@ -76,6 +76,22 @@ class KNNTests(unittest.TestCase):
         self.failUnless( mean_uv_perf < mean_mv_perf )
 
 
+    def testKNNState(self):
+        train = pureMultivariateSignal( 20, 3 )
+        test = pureMultivariateSignal( 20, 3 )
+
+        clf = kNN(k=10)
+        clf.train(train)
+
+        clf.enableState('values')
+        clf.enableState('predictions')
+
+        p = clf.predict(test.samples)
+
+        self.failUnless(p == clf['predictions'])
+        self.failUnless(N.array(clf['values']).shape == (80,2))
+
+
 def suite():
     return unittest.makeSuite(KNNTests)
 
