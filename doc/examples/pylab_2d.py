8d60ae645c84e74891557b092b5d18cabe8c2a82
Per B. Sederberg <psederberg@gmail.com>
2008-01-11 22:00:00 -0500

    BF: Fixed pylab_2d.py example to access state as attribute.

--- doc/examples/pylab_2d.py
+++ doc/examples/pylab_2d.py
@@ -58,7 +58,7 @@ feat_test=N.array((N.ravel(x), N.ravel(y)))
 pre = logReg.predict(feat_test.T)
 
 # get out the values used for the prediction
-res = logReg.states.get('values')
+res = logReg.values
 
 # reshape the results
 z = N.asarray(res).reshape((100,100))
