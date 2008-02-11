f0bec5fa6aff539e4c1a7158d36625b43cff29af
Michael Hanke <michael.hanke@gmail.com>
2008-02-11 11:03:15 +0100

    PL+NF: pylab_2d example: Moved a figures into one (as subplots); minor pylint fixes
    One figure to bind them all! (and easily put it into the manual ;-).

--- doc/examples/pylab_2d.py
+++ doc/examples/pylab_2d.py
@@ -20,27 +20,27 @@ from mvpa.clfs.svm import RbfNuSVMC,LinearNuSVMC
 
 # set up the labeled data
 # two skewed 2-D distributions
-num_dat=200
+num_dat = 200
 dist = 4
-feat_pos=N.random.randn(2,num_dat)
-feat_pos[0,:] *= 2.
-feat_pos[1,:] *= .5
-feat_pos[0,:] += dist
-feat_neg=N.random.randn(2,num_dat)
-feat_neg[0,:] *= .5
-feat_neg[1,:] *= 2.
-feat_neg[0,:] -= dist
+feat_pos=N.random.randn(2, num_dat)
+feat_pos[0, :] *= 2.
+feat_pos[1, :] *= .5
+feat_pos[0, :] += dist
+feat_neg=N.random.randn(2, num_dat)
+feat_neg[0, :] *= .5
+feat_neg[1, :] *= 2.
+feat_neg[0, :] -= dist
 
 # set up the testing features
-x1=N.linspace(-10,10, 100)
-x2=N.linspace(-10,10, 100)
-x,y=N.meshgrid(x1,x2);
-feat_test=N.array((N.ravel(x), N.ravel(y)))
+x1 = N.linspace(-10, 10, 100)
+x2 = N.linspace(-10, 10, 100)
+x,y = N.meshgrid(x1, x2);
+feat_test = N.array((N.ravel(x), N.ravel(y)))
 
 # create the pymvpa dataset from the labeled features
-patternsPos = Dataset(samples=feat_pos.T,labels=1)
-patternsNeg = Dataset(samples=feat_neg.T,labels=0)
-patterns = patternsPos+patternsNeg
+patternsPos = Dataset(samples=feat_pos.T, labels=1)
+patternsNeg = Dataset(samples=feat_neg.T, labels=0)
+patterns = patternsPos + patternsNeg
 
 # set up classifiers to try out
 clfs = {'Ridge Regression': RidgeReg(),
@@ -50,19 +50,21 @@ clfs = {'Ridge Regression': RidgeReg(),
 
 # loop over classifiers and show how they do
 fig = 0
+
+# make a new figure
+P.figure()
 for c in clfs:
     # tell which one we are doing
     print "Running %s classifier..." % (c)
-    
-    # make a new figure
-    fig+=1
-    P.figure(fig)
+
+    # make a new subplot for each classifier
+    fig += 1
+    P.subplot(2,2,fig)
 
     # plot the training points
-    P.clf()
-    P.plot(feat_pos[0,:], feat_pos[1,:], "r.")
-    P.plot(feat_neg[0,:], feat_neg[1,:], "b.")
-    
+    P.plot(feat_pos[0, :], feat_pos[1, :], "r.")
+    P.plot(feat_neg[0, :], feat_neg[1, :], "b.")
+
     # select the clasifier
     clf = clfs[c]
 
@@ -84,14 +86,14 @@ for c in clfs:
         res = N.asarray(clf.values)
     else:
         # get the probabilities from the svm
-        res = N.asarray([(q[1][1] - q[1][0] + 1)/2 for q in clf.values])
+        res = N.asarray([(q[1][1] - q[1][0] + 1) / 2 for q in clf.values])
 
     # reshape the results
-    z = N.asarray(res).reshape((100,100))
+    z = N.asarray(res).reshape((100, 100))
 
     # plot the predictions
     P.pcolor(x, y, z, shading='interp')
-    P.clim(0,1)
+    P.clim(0, 1)
     P.colorbar()
     P.contour(x, y, z, linewidths=1, colors='black', hold=True)
 
