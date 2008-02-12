af0cf9859b70fad71b7bc50433d17f9d033808ed
Michael Hanke <michael.hanke@gmail.com>
2008-02-12 20:00:00 +0100

    BK: Big fat bug in NiftiDataset -- no fix available!
    Added a unit test that shows the symptom, but the origin of the bug is yet to
    be determined.

--- tests/test_niftidataset.py
+++ tests/test_niftidataset.py
@@ -59,6 +59,26 @@ class NiftiDatasetTests(unittest.TestCase):
         self.failUnless((vol.data == 1).all())
 
 
+    def testMultipleCalls(self):
+        # test if doing exactly the same operation twice yields the same
+        # result
+        data = NiftiDataset(samples=os.path.join('data','example4d'),
+                            labels=1)
+        data2 = NiftiDataset(samples=os.path.join('data','example4d'),
+                             labels=1)
+
+        # Currently this test fails and I don't know why!
+        # The problem occurs, because in the second call to
+        # NiftiDataset.__init__() there is already a dsattr that has a 'mapper'
+        # key, although dsattr is set to be an empty dict. Therefore the
+        # constructor does not set the proper elementsize, because it thinks
+        # there is already a mapper present. Actually this test is just looking
+        # for a symptom of a buggy dsattr handling.
+        # The tricky part is: I have no clue, what is going on... :(
+        self.failUnless((data.mapper.metric.elementsize \
+                         == data2.mapper.metric.elementsize).all())
+
+
 
 def suite():
     return unittest.makeSuite(NiftiDatasetTests)
