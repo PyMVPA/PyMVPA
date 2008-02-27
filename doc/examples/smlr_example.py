09af6c9a26512eb3fc4995b386d18693cba91fdf
Per B. Sederberg <psederberg@gmail.com>
2008-02-27 08:27:37 -0500

    NF/BF: First round of SMLR working!!!
    Still needs unit testing and cleaning and optimization (it needs loops so would be really
    really fast in C.)  Try the example :)

--- doc/examples/smlr_example.py
+++ doc/examples/smlr_example.py
@@ -10,42 +10,53 @@
 """Example demonstrating a SMLR classifier"""
 
 import numpy as N
+
 from mvpa.datasets.dataset import Dataset
 from mvpa.clfs.smlr import SMLR
+from mvpa.clfs.transerror import ConfusionMatrix
 
-
+# features of sample data
 nfeat = 10000
 nsamp = 100
 ntrain = 90
 goodfeat = 10
-offset = 0.5
+offset = .5
 
+# create the sample datasets
 samp1 = N.random.randn(nsamp,nfeat)
 samp1[:,:goodfeat] += offset
-lab1 = N.ones((nsamp,1))
-#samp1 = N.hstack((lab1,samp1))
 
 samp2 = N.random.randn(nsamp,nfeat)
 samp2[:,:goodfeat] -= offset
-lab2 = N.ones((nsamp,1))*2
-#samp2 = N.hstack((lab2,samp2))
 
 traindat = N.vstack((samp1[:ntrain,:],samp2[:ntrain,:]))
 testdat = N.vstack((samp1[ntrain:,:],samp2[ntrain:,:]))
 
-# create the pymvpa dataset from the labeled features
+# create the pymvpa training dataset from the labeled features
 patternsPos = Dataset(samples=samp1[:ntrain,:], labels=1)
 patternsNeg = Dataset(samples=samp2[:ntrain,:], labels=0)
-patterns = patternsPos + patternsNeg
+trainpat = patternsPos + patternsNeg
 
-# set up the classifier
-clf = SMLR()
+# create patters for the testing dataset
+patternsPos = Dataset(samples=samp1[ntrain:,:], labels=1)
+patternsNeg = Dataset(samples=samp2[ntrain:,:], labels=0)
+testpat = patternsPos + patternsNeg
+
+# set up the SMLR classifier
+clf = SMLR(lm=1.5)
 
 # enable saving of the values used for the prediction
 clf.states.enable('values')
 
 # train with the known points
-clf.train(patterns)
+clf.train(trainpat)
 
 # run the predictions on the test values
-pre = clf.predict(testdat)
+pre = clf.predict(testpat.samples)
+
+# calculate the confusion matrix
+testing_confusion = ConfusionMatrix(
+    labels=trainpat.uniquelabels, targets=testpat.labels,
+    predictions=pre)
+
+print "Percent Correct: %g%%" % (testing_confusion.percentCorrect)
