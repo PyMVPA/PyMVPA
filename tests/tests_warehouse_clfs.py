f9ead57550b74712dd4106c3c9acba26318e742b
Yaroslav Halchenko <debian@onerussian.com>
2008-03-03 12:26:53 -0500

    reorganized a bit classifier groups

--- tests/tests_warehouse_clfs.py
+++ tests/tests_warehouse_clfs.py
@@ -19,14 +19,14 @@ from mvpa.clfs.knn import *
 
 clfs={'LinearSVMC' : [LinearCSVMC(), LinearNuSVMC()],
       'NonLinearSVMC' : [RbfCSVMC(), RbfNuSVMC()],
-      'clfs_with_sens' : [LinearCSVMC(), LinearNuSVMC(),
-                          SMLR(implementation="Python"),
-                          SMLR(implementation="C"),
-                          ],
       }
 
 clfs['LinearC'] = clfs['LinearSVMC'] + \
                   [ SMLR(implementation="Python"), SMLR(implementation="C") ]
+
 clfs['NonLinearC'] = clfs['NonLinearSVMC'] + [ kNN(k=1), RidgeReg() ]
+
 clfs['all'] = clfs['LinearC'] + clfs['NonLinearC']
+
+clfs['clfs_with_sens'] =  clfs['LinearC']
 
