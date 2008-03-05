159878a4a0a07049e00103808bb691c833c75124
Yaroslav Halchenko <debian@onerussian.com>
2008-03-05 12:22:03 -0500

    RF (+tiny BF) SMLR: pylint friendly commit -- very intrusive

--- mvpa/clfs/smlr.py
+++ mvpa/clfs/smlr.py
@@ -18,10 +18,26 @@ from mvpa.misc.exceptions import ConvergenceError
 from mvpa.misc.state import StateVariable
 
 # Uber-fast C-version of the stepwise regression
-from mvpa.clfs.libsmlr import stepwise_regression as _c_stepwise_regression
+from mvpa.clfs.libsmlr import stepwise_regression as _cStepwiseRegression
 
 if __debug__:
     from mvpa.misc import debug
+
+def _label2oneofm(labels, ulabels):
+    """Convert labels to one-of-M form.
+
+    TODO: Might be useful elsewhere so could migrate into misc/
+    """
+
+    # allocate for the new one-of-M labels
+    new_labels = N.zeros((len(labels), len(ulabels)))
+
+    # loop and convert to one-of-M
+    for i, c in enumerate(ulabels):
+        new_labels[labels == c, i] = 1
+
+    return new_labels
+
 
 
 class SMLR(Classifier):
@@ -38,9 +54,9 @@ class SMLR(Classifier):
 
 
     def __init__(self, lm=.1, convergence_tol=1e-3,
-                 maxiter=10000, bias=True, implementation="C", **kwargs):
+                 maxiter=10000, bias=1.0, implementation="C", **kwargs):
         """
-        Initialize a SMLR analysis.
+        Initialize an SMLR classifier.
 
         :Parameters:
           lm : float
@@ -52,9 +68,11 @@ class SMLR(Classifier):
             lead to tighter convergence.
           maxiter : int
             Maximum number of iterations before stopping if not converged.
-          bias : bool
+          bias : float or list of float
             Whether to add a bias term to allow fits to data not through
-            zero.  Defaults to True.
+            zero.  Value 0 or None disables adding bias. In case of
+            multiclass classification single value would be replicated. If
+            list -- used directly
           implementation : basestr
             Use C (default) or Python as the implementation of
             stepwise_regression. C version brings significant speedup thus
@@ -71,7 +89,11 @@ class SMLR(Classifier):
         self.__lm = lm
         self.__convergence_tol = convergence_tol
         self.__maxiter = maxiter
-        self.__bias = bias
+        self.__base_bias = bias
+
+        # pylint friendly initializations
+        self.__ulabels = None
+        self.__weights = None
 
         if not implementation.upper() in ['C', 'PYTHON']:
             raise ValueError, \
@@ -79,26 +101,18 @@ class SMLR(Classifier):
                   implementation
         self.__implementation = implementation
 
+
     def __repr__(self):
         """String representation of the object
         """
-        return """SMLR(lm=%f, convergence_tol=%g, maxiter=%d, bias=%s, implementation='%s', enabled_states=%s)""" % \
-            (self.__lm, self.__convergence_tol,
-             self.__maxiter, self.__bias, self.__implementation,
-             str(self.states.enabled))
+        return "SMLR(lm=%f, convergence_tol=%g, maxiter=%d, " % \
+               (self.__lm, self.__convergence_tol, self.__maxiter) + \
+               "bias=%s, implementation='%s', enabled_states=%s)" % \
+               (self.__base_bias, self.__implementation,
+                str(self.states.enabled))
 
-    def __label_to_oneofm(self,labels,ulabels):
-        """Convert labels to one-of-M form."""
-        # allocate for the new one-of-M labels
-        new_labels = N.zeros((len(labels),len(ulabels)))
 
-        # loop and convert to one-of-M
-        for i,c in enumerate(ulabels):
-            new_labels[labels==c,i] = 1
-
-        return new_labels
-
-    def _python_stepwise_regression(self, w, X, XY, Xw, E,
+    def _pythonStepwiseRegression(self, w, X, XY, Xw, E,
                                     auto_corr,
                                     lambda_over_2_auto_corr,
                                     S,
@@ -111,7 +125,7 @@ class SMLR(Classifier):
         compare results."""
 
         # get the data information into easy vars
-        ns,nd = X.shape
+        ns, nd = X.shape
 
         # yoh: shouldn't be here and should be derived from the interface
         M = len(self.__ulabels)
@@ -119,16 +133,9 @@ class SMLR(Classifier):
         # initialize the iterative optimization
         converged = False
         incr = N.finfo(N.float).max
-        non_zero = 0
-        basis = 0
-        m = 0
-        wasted_basis = 0
-        cycles = 0
-        decrease_factor = 1.
-        test_zero_basis = 1.
-        sum2_w_diff = 0.0
-        sum2_w_old = 0.0
-        w_diff = 0.0
+        non_zero, basis, m, wasted_basis, cycles = 0, 0, 0, 0, 0
+        decrease_factor, test_zero_basis = 1., 1.
+        sum2_w_diff, sum2_w_old, w_diff = 0.0, 0.0, 0.0
 
         N.random.seed(seed)
         if __debug__:
@@ -136,18 +143,18 @@ class SMLR(Classifier):
 
 
         # perform the optimization
-        while not converged and cycles<maxiter:
+        while not converged and cycles < maxiter:
             # get the starting weight
-            w_old = w[basis,m]
+            w_old = w[basis, m]
 
             # see if we're gonna update
-            if (w_old != 0) or N.random.rand()<= test_zero_basis:
+            if (w_old != 0) or N.random.rand() <= test_zero_basis:
                 # let's do it
                 # get the probability
-                P = E[:,m]/S
+                P = E[:, m]/S
 
                 # set the gradient
-                grad = XY[basis,m] - N.dot(X[:,basis],P)
+                grad = XY[basis, m] - N.dot(X[:, basis], P)
 
                 # calculate the new weight with the Laplacian prior
                 w_new = w_old + grad/auto_corr[basis]
@@ -158,13 +165,13 @@ class SMLR(Classifier):
                     changed = True
                     # unmark from being zero if necessary
                     if w_old == 0:
-                        non_zero+=1
+                        non_zero += 1
                 elif w_new < -lambda_over_2_auto_corr[basis]:
                     w_new += lambda_over_2_auto_corr[basis]
                     changed = True
                     # unmark from being zero if necessary
                     if w_old == 0:
-                        non_zero+=1
+                        non_zero += 1
                 else:
                     # gonna zero it out
                     w_new = 0.0
@@ -172,30 +179,30 @@ class SMLR(Classifier):
                     # set number of non-zero
                     if w_old == 0:
                         changed = False
-                        wasted_basis+=1
+                        wasted_basis += 1
                     else:
                         changed = True
-                        non_zero-=1
+                        non_zero -= 1
 
                 # process any changes
                 if changed:
-                    #print "w[%d,%d] = %g" % (basis,m,w_new)
+                    #print "w[%d, %d] = %g" % (basis, m, w_new)
                     # update the expected values
-                    w_diff = w_new - w_old;
-                    Xw[:,m] = Xw[:,m] + X[:,basis]*w_diff
-                    E_new_m = N.exp(Xw[:,m])
-                    S += E_new_m - E[:,m]
-                    E[:,m] = E_new_m
+                    w_diff = w_new - w_old
+                    Xw[:, m] = Xw[:, m] + X[:, basis]*w_diff
+                    E_new_m = N.exp(Xw[:, m])
+                    S += E_new_m - E[:, m]
+                    E[:, m] = E_new_m
 
                     # update the weight
-                    w[basis,m] = w_new
+                    w[basis, m] = w_new
 
                     # keep track of the sqrt sum squared distances
-                    sum2_w_diff += w_diff*w_diff;
-                    sum2_w_old += w_old*w_old;
+                    sum2_w_diff += w_diff*w_diff
+                    sum2_w_old += w_old*w_old
 
             # update the class and basis
-            m = N.mod(m+1,M-1)
+            m = N.mod(m+1, M-1)
             if m == 0:
                 # we completed a cycle of labels
                 basis = N.mod(basis+1,nd)
@@ -205,7 +212,7 @@ class SMLR(Classifier):
 
                     # assess convergence
                     incr = N.sqrt(sum2_w_diff) / \
-                           (N.sqrt(sum2_w_old)+N.finfo(N.float).eps);
+                           (N.sqrt(sum2_w_old)+N.finfo(N.float).eps)
 
                     # save the new weights
                     converged = incr < convergence_tol
@@ -239,7 +246,7 @@ class SMLR(Classifier):
 #                  "SMLR converged after %d steps. Error: %g" % \
 #                  (cycles, XXX))
 
-#        print 'cycles=%d ; wasted basis=%g\n' % (cycles,wasted_basis/((M-1)*nd))
+#        print 'cycles=%d ; wasted basis=%g\n' % (cycles, wasted_basis/((M-1)*nd))
 
         # save the weights
         self.__weights = w
@@ -250,7 +257,7 @@ class SMLR(Classifier):
         """Train the classifier using `dataset` (`Dataset`).
         """
         # Process the labels to turn into 1 of N encoding
-        labels = self.__label_to_oneofm(dataset.labels,dataset.uniquelabels)
+        labels = _label2oneofm(dataset.labels, dataset.uniquelabels)
         self.__ulabels = dataset.uniquelabels.copy()
         Y = labels
         M = len(self.__ulabels)
@@ -259,12 +266,12 @@ class SMLR(Classifier):
         X = dataset.samples
 
         # see if we are adding a bias term
-        if self.__bias:
+        if self.__base_bias:
             # append the bias term to the features
-            X = N.hstack((X,N.ones((X.shape[0],1),dtype=X.dtype)))
+            X = N.hstack((X, N.ones((X.shape[0], 1), dtype=X.dtype)))
 
         if self.__implementation.upper() == 'C':
-            _stepwise_regression = _c_stepwise_regression
+            _stepwise_regression = _cStepwiseRegression
             #
             # TODO: avoid copying to non-contig arrays, use strides in ctypes?
             if not (X.flags['C_CONTIGUOUS'] and X.flags['ALIGNED']):
@@ -278,25 +285,25 @@ class SMLR(Classifier):
                 X = X.astype(N.double)
         # set the feature dimensions
         elif self.__implementation.upper() == 'PYTHON':
-            _stepwise_regression = self._python_stepwise_regression
+            _stepwise_regression = self._pythonStepwiseRegression
         else:
             raise ValueError, \
                   "Unknown implementation %s of stepwise_regression" % \
-                  implementation
+                  self.__implementation
 
         # set the feature dimensions
-        ns,nd = X.shape
+        ns, nd = X.shape
 
         # Precompute what we can
-        auto_corr = ((M-1.)/(2.*M))*(N.sum(X*X,0))
-        XY = N.dot(X.T,Y[:,:(M-1)])
+        auto_corr = ((M-1.)/(2.*M))*(N.sum(X*X, 0))
+        XY = N.dot(X.T, Y[:, :(M-1)])
         lambda_over_2_auto_corr = (self.__lm/2.)/auto_corr
 
         # set starting values
-        w = N.zeros((nd,M-1),dtype=N.double)
-        Xw = N.zeros((ns,M-1),dtype=N.double)
-        E = N.ones((ns,M-1),dtype=N.double)
-        S = M*N.ones(ns,dtype=N.double)
+        w = N.zeros((nd, M-1), dtype=N.double)
+        Xw = N.zeros((ns, M-1), dtype=N.double)
+        E = N.ones((ns, M-1), dtype=N.double)
+        S = M*N.ones(ns, dtype=N.double)
 
         # set verbosity
         if __debug__:
@@ -344,15 +351,15 @@ class SMLR(Classifier):
         Predict the output for the provided data.
         """
         # see if we are adding a bias term
-        if self.__bias:
+        if self.__base_bias:
             # append the bias term to the features
-            data = N.hstack((data,N.ones((data.shape[0],1),dtype=data.dtype)))
+            data = N.hstack((data, N.ones((data.shape[0], 1), dtype=data.dtype)))
 
         # append the zeros column to the weights
-        w = N.hstack((self.__weights,N.zeros((self.__weights.shape[0],1))))
+        w = N.hstack((self.__weights, N.zeros((self.__weights.shape[0], 1))))
 
         # determine the probability values for making the prediction
-        dot_prod = N.dot(data,w)
+        dot_prod = N.dot(data, w)
         E = N.exp(dot_prod)
         S = N.sum(E, 1)
 
@@ -363,7 +370,7 @@ class SMLR(Classifier):
                   (N.min(w), N.max(w), N.min(dot_prod), N.max(dot_prod),
                    N.min(E), N.max(E)))
 
-        values = E / S[:,N.newaxis].repeat(E.shape[1],axis=1)
+        values = E / S[:, N.newaxis].repeat(E.shape[1], axis=1)
         self.values = values
 
         # generate predictions
