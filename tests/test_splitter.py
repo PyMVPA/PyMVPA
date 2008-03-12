cb54ed52ca1961fb8e17dbd801eac27febfa34e0
Per Sederberg <per@parsec.Princeton.EDU>
2008-03-12 13:23:37 -0400

    BF: Splitter would error when given auto option. Added minor unit test, too.

--- tests/test_splitter.py
+++ tests/test_splitter.py
@@ -107,6 +107,7 @@ class SplitterTests(unittest.TestCase):
 
 
         # test sampling tools
+        # specified value
         nos = NoneSplitter(nrunspersplit=3,
                            nsecondsamples=10)
         splits = [ (train, test) for (train, test) in nos(self.data) ]
@@ -116,6 +117,17 @@ class SplitterTests(unittest.TestCase):
             self.failUnless(split[0] == None)
             self.failUnless(split[1].nsamples == 40)
             self.failUnless(split[1].samplesperlabel.values() == [10,10,10,10])
+
+        # auto-determined
+        nos = NoneSplitter(nrunspersplit=3,
+                           nsecondsamples='auto')
+        splits = [ (train, test) for (train, test) in nos(self.data) ]
+
+        self.failUnless(len(splits) == 3)
+        for split in splits:
+            self.failUnless(split[0] == None)
+            self.failUnless(split[1].nsamples == 100)
+            self.failUnless(split[1].samplesperlabel.values() == [25,25,25,25])
 
 
     def testLabelSplitter(self):
@@ -127,7 +139,6 @@ class SplitterTests(unittest.TestCase):
         self.failUnless((splits[0][1].uniquelabels == [1,3]).all())
         self.failUnless((splits[1][0].uniquelabels == [1,3]).all())
         self.failUnless((splits[1][1].uniquelabels == [0,2]).all())
-
 
 
 def suite():
