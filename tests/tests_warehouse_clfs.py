c0ff105c5f4ba0df8beb47fa61ba756823c42e07
Yaroslav Halchenko <debian@onerussian.com>
2008-04-12 01:29:38 -0400

    silly classifiers moved into warehouse

--- tests/tests_warehouse_clfs.py
+++ tests/tests_warehouse_clfs.py
@@ -30,3 +30,37 @@ clfs['all'] = clfs['LinearC'] + clfs['NonLinearC']
 
 clfs['clfs_with_sens'] =  clfs['LinearC']
 
+#
+# Few silly classifiers
+#
+class SameSignClassifier(Classifier):
+    """Dummy classifier which reports +1 class if both features have
+    the same sign, -1 otherwise"""
+
+    def __init__(self, **kwargs):
+        Classifier.__init__(self, train2predict=False, **kwargs)
+
+    def _train(self, data):
+        # we don't need that ;-)
+        pass
+
+    def _predict(self, data):
+        datalen = len(data)
+        values = []
+        for d in data:
+            values.append(2*int( (d[0]>=0) == (d[1]>=0) )-1)
+        self.predictions = values
+        return values
+
+
+class Less1Classifier(SameSignClassifier):
+    """Dummy classifier which reports +1 class if abs value of max less than 1"""
+    def _predict(self, data):
+        datalen = len(data)
+        values = []
+        for d in data:
+            values.append(2*int(max(d)<=1)-1)
+        self.predictions = values
+        return values
+
+
