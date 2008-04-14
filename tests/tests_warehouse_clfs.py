03d957b236bc4b958c18b83a173014f3de1b009d
Yaroslav Halchenko <debian@onerussian.com>
2008-04-14 18:25:54 -0400

    enabled probabilities estimation for LibSVM SVMs

--- tests/tests_warehouse_clfs.py
+++ tests/tests_warehouse_clfs.py
@@ -20,9 +20,13 @@ from mvpa.clfs.ridge import *
 from mvpa.clfs.knn import *
 
 
-clfs={'LinearSVMC' : [libsvm.svm.LinearCSVMC(), libsvm.svm.LinearNuSVMC(), sg.svm.LinearCSVMC()],
-      'NonLinearSVMC' : [libsvm.svm.RbfCSVMC(), libsvm.svm.RbfNuSVMC(), sg.svm.RbfCSVMC()]
+clfs={'LinearSVMC' : [libsvm.svm.LinearCSVMC(probability=1),
+                      libsvm.svm.LinearNuSVMC(probability=1), sg.svm.LinearCSVMC()],
+      'NonLinearSVMC' : [libsvm.svm.RbfCSVMC(probability=1),
+                         libsvm.svm.RbfNuSVMC(probability=1), sg.svm.RbfCSVMC()]
       }
+
+clfs['SVMC'] = clfs['LinearSVMC'] + clfs['NonLinearSVMC']
 
 clfs['LinearC'] = clfs['LinearSVMC'] + \
                   [ SMLR(implementation="Python"), SMLR(implementation="C") ]
