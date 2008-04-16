87d8bd46fa1fca078e47b3f7e8bdac746c5df6ba
Yaroslav Halchenko <debian@onerussian.com>
2008-04-16 12:21:07 -0400

    RF: misc.clfhelper -> base/externals

---
+++ mvpa/base/__init__.py
@@ -0,0 +1,24 @@
+#emacs: -*- mode: python-mode; py-indent-offset: 4; indent-tabs-mode: nil -*-
+#ex: set sts=4 ts=4 sw=4 et:
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the PyMVPA package for the
+#   copyright and license terms.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+"""Base functionality of PyMVPA
+
+Module Organization
+===================
+
+mvpa.base module contains various modules which are used through out
+PyMVPA code, and are generic building blocks
+
+.. packagetree::
+   :style: UML
+
+:group Basic: externals
+"""
+
+__docformat__ = 'restructuredtext'
+
