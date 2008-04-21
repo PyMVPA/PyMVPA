956c200b88569f2300a2b4260006affd5008fcd1
Michael Hanke <michael.hanke@gmail.com>
2008-04-21 08:29:05 +0200

    RF: All tests now use dumbFeatureDataset from data_generators (not custom def).

--- tests/test_anova.py
+++ tests/test_anova.py
@@ -13,16 +13,7 @@ import numpy as N
 
 from mvpa.datasets.dataset import Dataset
 from mvpa.algorithms.anova import OneWayAnova
-
-
-def dumbFeatureDataset():
-    data = [[0,1],[1,1],[0,2],[1,2],[0,3],[1,3],[0,4],[1,4],
-            [0,5],[1,5],[0,6],[1,6],[0,7],[1,7],[0,8],[1,8],
-            [0,9],[1,9],[0,10],[1,10],[0,11],[1,11],[0,12],[1,12]]
-    regs = [1] * 8 + [2] * 8 + [3] * 8
-
-    return Dataset(samples=data, labels=regs)
-
+from mvpa.misc.data_generators import dumbFeatureDataset
 
 
 class ANOVATests(unittest.TestCase):
@@ -35,8 +26,8 @@ class ANOVATests(unittest.TestCase):
         f = aov(data)
 
         self.failUnless(f.shape == (2,))
-        self.failUnless(f[0] == 0.0)
-        self.failUnless(f[1] > 0.0)
+        self.failUnless(f[1] == 0.0)
+        self.failUnless(f[0] > 0.0)
 
 
 def suite():
