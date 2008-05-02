fa6a67b72a3cf48fb17e95eaea6da05982a7e916
Yaroslav Halchenko <debian@onerussian.com>
2008-05-02 15:20:25 -0400

    XXX disabled MPD implementation for shogun for now
    indefinite looping on XOR dataset in clfs_example

--- mvpa/clfs/sg/svm.py
+++ mvpa/clfs/sg/svm.py
@@ -36,7 +36,7 @@ known_kernels = { "linear": shogun.Kernel.LinearKernel,
 
 known_svm_impl = { "libsvm" : shogun.Classifier.LibSVM,
                    "gmnp" : shogun.Classifier.GMNPSVM,
-                   "mpd"  : shogun.Classifier.MPDSVM,
+                   #"mpd"  : shogun.Classifier.MPDSVM, # disable due to infinite looping on XOR
                    "gpbt" : shogun.Classifier.GPBTSVM,
                    "gnpp" : shogun.Classifier.GNPPSVM,
                    }
