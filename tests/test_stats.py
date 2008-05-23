a8cf435d1625151a57b43a9dc26ad5e437c75071
Michael Hanke <michael.hanke@gmail.com>
2008-05-23 08:50:20 +0200

    NF: Let MCNullDist also work with `FeaturewiseDatasetMeasure`
    In fact all kinds of `DatasetMeasure` objects.
    
    Also added new unit test and updated docs.

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -13,6 +13,9 @@ import unittest
 import numpy as N
 
 from mvpa.misc.stats import chisquare
+from mvpa.clfs.stats import MCNullDist
+from mvpa.measures.anova import OneWayAnova
+from mvpa.misc.data_generators import normalFeatureDataset
 
 
 class StatsTests(unittest.TestCase):
@@ -29,6 +32,23 @@ class StatsTests(unittest.TestCase):
         chi, p = chisquare(tbl)
         self.failUnless(chi == 8.0)
         self.failUnless(p < 0.05)
+
+
+    def testNullDistProb(self):
+        ds = normalFeatureDataset(perlabel=20, nlabels=2, nfeatures=2,
+                                  means=[[0],[0]], snr=1, nchunks=1)
+
+        null = MCNullDist(permutations=10, tail='right')
+
+        null.fit(OneWayAnova(), ds)
+
+        # check reasonable output (F-score always positive and close to zero
+        # for random data
+        prob = null.cdf([3,0])
+        self.failUnless((prob == [0, 1]).all())
+        # has to have matching shape
+        self.failUnlessRaises(ValueError, null.cdf, [5,3,4])
+
 
 
 def suite():
