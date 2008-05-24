ca42cae788dbec75b6bc4d8330e6651346399ac6
Yaroslav Halchenko <debian@onerussian.com>
2008-05-24 02:06:12 -0400

    NF/RF: mvpa.suite is ready for digestion, make testsuite checks for its completeness
    Now, all 'from mvpa.... import ...' can be expressed as
    
    from mvpa.suite import *
    
    on my somewhat elderly box it takes only around 3 seconds to run
    test_suite.py which loads it
    
    next commit will tune up all (if not missed) examples which would make
    use of suite (by leaving original imports in a multiline string for
    appreciation)
    
    make testsuite checks if all imports in unittests are present in the
    suite.

--- mvpa/suite.py
+++ mvpa/suite.py
@@ -21,24 +21,23 @@ or
 
 __docformat__ = 'restructuredtext'
 
-"""
-
-"""
 
 from mvpa.algorithms.cvtranserror import *
 from mvpa.base import *
 from mvpa.clfs.base import *
 from mvpa.clfs.knn import *
-from mvpa.clfs.lars import *
+if externals.exists('lars'):
+    from mvpa.clfs.lars import *
 from mvpa.clfs.plr import *
 from mvpa.clfs.ridge import *
 from mvpa.clfs.smlr import *
+from mvpa.clfs.gpr import *
 from mvpa.clfs.stats import *
 from mvpa.clfs.svm import *
 from mvpa.clfs.transerror import *
 from mvpa.clfs.warehouse import *
-from mvpa.datasets import *
 from mvpa.datasets.base import *
+from mvpa.datasets import *
 from mvpa.datasets.maskeddataset import *
 from mvpa.datasets.metric import *
 from mvpa.datasets.misc import *
@@ -54,10 +53,11 @@ from mvpa.measures.base import *
 from mvpa.measures.noiseperturbation import *
 from mvpa.measures.searchlight import *
 from mvpa.measures.splitmeasure import *
-from mvpa.misc import *
+from mvpa.misc.cmdline import *
 from mvpa.misc.copy import *
 from mvpa.misc.data_generators import *
 from mvpa.misc.exceptions import *
+from mvpa.misc import *
 from mvpa.misc.iohelpers import *
 from mvpa.misc.param import *
 from mvpa.misc.signal import *
@@ -66,3 +66,6 @@ from mvpa.misc.stats import *
 from mvpa.misc.support import *
 from mvpa.misc.transformers import *
 from mvpa.misc.verbosity import *
+
+if externals.exists("pylab"):
+	import pylab as P
