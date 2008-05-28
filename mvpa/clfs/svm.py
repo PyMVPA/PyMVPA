51fff7791b809d3ccf8a2f69a4bc93e90aabdb89
Yaroslav Halchenko <debian@onerussian.com>
2008-05-28 16:16:39 -0400

    RF: we should allow working PyMVPA with no SVM implementations
    warning instead of raise

--- mvpa/clfs/svm.py
+++ mvpa/clfs/svm.py
@@ -12,7 +12,7 @@ __docformat__ = 'restructuredtext'
 
 # take care of conditional import of external classifiers
 import mvpa.base.externals as externals
-
+from mvpa.misc import warning
 
 if externals.exists('libsvm'):
     # By default for now we want simply to import all SVMs from libsvm
@@ -25,18 +25,4 @@ if externals.exists('shogun'):
         from mvpa.clfs.sg.svm import *
 
 if not 'LinearCSVMC' in locals():
-    raise RuntimeError, "None of SVM implementions libraries was found"
-
-#try:
-#    from sg.svm import *
-#
-#    # Nu-SVMs are not provided by SG thus reverting to libsvm-wrappers
-#    from libsvm.svm import LinearNuSVMC, RbfNuSVMC
-#
-#    # Or just bind them to C-SVMs ;)
-#    #LinearNuSVMC = LinearCSVMC
-#    #RbfNuSVMC = RbfCSVMC
-#except:
-#    from mvpa.misc import warning
-#    warning("Cannot import shogun libraries. Reverting back to LibSVM wrappers")
-#    from libsvm.svm import *
+    warning("None of SVM implementions libraries was found")
