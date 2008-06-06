abb5a01677aced1bd0ee2bcbb75fb0dc2b147c06
Yaroslav Halchenko <debian@onerussian.com>
2008-06-05 23:13:13 -0400

    RF: import all FSLs functions in fsl.__init__

--- mvpa/misc/fsl/__init__.py
+++ mvpa/misc/fsl/__init__.py
@@ -11,4 +11,9 @@
 if __debug__:
     from mvpa.misc import debug
     debug('INIT', 'mvpa.misc.fsl')
+
+from base import *
+from flobs import *
+
+if __debug__:
     debug('INIT', 'mvpa.misc.fsl end')
