5cd45033df49c0ec385bfd3d6f874359ce5413e5
Yaroslav Halchenko <debian@onerussian.com>
2008-06-20 13:30:45 -0400

    BF: one more bugfix in test_verbosity toward fortified unittests battery

--- tests/test_verbosity.py
+++ tests/test_verbosity.py
@@ -8,7 +8,7 @@
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 """Unit tests for PyMVPA verbose and debug output"""
 
-import unittest
+import unittest, re
 from StringIO import StringIO
 
 from mvpa.misc.verbosity import OnceLogger
@@ -136,8 +136,12 @@ class VerboseOutputTest(unittest.TestCase):
             debug('SLC', self.msg, lf=False)
             self.failUnlessRaises(ValueError, debug, 3, 'bugga')
             #Should complain about unknown debug id
-            self.failUnlessEqual(self.sout.getvalue(),
-                                 "[SLC] DBG: %s" % self.msg)
+            svalue = self.sout.getvalue()
+            regexp = "\[SLC\] DBG(?:{.*})?: %s" % self.msg
+            rematch = re.match(regexp, svalue)
+            self.failUnless(rematch, msg="Cannot match %s with regexp %s" %
+                            (svalue, regexp))
+
 
         def testDebugRgexp(self):
             verbose.handlers = []           # so debug doesn't spoil it
