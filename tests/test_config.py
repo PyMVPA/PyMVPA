57f6ca67c75252a6a5ec9114f6415813aeef5d5a
Michael Hanke <michael.hanke@gmail.com>
2008-06-22 21:54:01 +0200

    NF: Registry-monster for PYMVPA.
    This is an attempt to merge all ways to change the runtime behavior of PyMVPA
    into a single entity (and their documentation as well).
    
    Advantages are that we provide the user with the possibility to configure
    user- and analysis-specifc settings using simple textfiles, while keeping
    are current approach using env variables.
    
    The configuration is exposed via `mvpa.cfg` and can be queried/modified by
    library and user-code. Another nice feature is, that users can now easily dump
    the configuration they used for an analysis into a file by doing::
    
    	mvpa.cfg.save('filename.cfg')
    
    and re-use/inspect it later on.
    
    Additonally, we expose a logic to handle any configuration options and
    therefore save users from having to do it themselves.
    
    Unittest fragment is added, but very useful ATM.
    
    So far mvpa.cfg is only used to query and set the RNG seed, because you might
    not like having a registry ;-)

---
+++ tests/test_config.py
@@ -0,0 +1,30 @@
+#emacs: -*- mode: python-mode; py-indent-offset: 4; indent-tabs-mode: nil -*-
+#ex: set sts=4 ts=4 sw=4 et:
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the PyMVPA package for the
+#   copyright and license terms.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+"""Unit tests for PyMVPA dense array mapper"""
+
+
+import unittest
+from mvpa.base.config import Config
+
+class ConfigTests(unittest.TestCase):
+
+    def testConfig(self):
+        cfg = Config()
+
+        # does nothing so far, but will be used to test the default
+        # configuration from doc/examples/pymvpa.cfg
+
+
+def suite():
+    return unittest.makeSuite(ConfigTests)
+
+
+if __name__ == '__main__':
+    import runner
+
