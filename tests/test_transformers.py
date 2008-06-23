b807c0dfbb2e913d953e67ab1616b01d60f2d8eb
Yaroslav Halchenko <debian@onerussian.com>
2008-06-23 15:57:46 -0400

    BF+NF: fixed RankOrder transformer + UnitNorm transformer
    RankOrder for some reason looks ugly. It could be neater

--- tests/test_transformers.py
+++ tests/test_transformers.py
@@ -11,10 +11,15 @@
 import unittest
 import numpy as N
 
-from mvpa.misc.transformers import Absolute, OneMinus
+from mvpa.misc.transformers import Absolute, OneMinus, RankOrder, UnitNorm, \
+     ReverseRankOrder
 
 
 class TransformerTests(unittest.TestCase):
+
+    def setUp(self):
+        self.d1 = N.array([ 1,  0, -1, -2, -3])
+        self.d2 = N.array([ 2.3,  0, -1, 2, -30, 1])
 
     def testAbsolute(self):
         # generate 100 values (gaussian noise mean -1000 -> all negative)
@@ -23,12 +28,28 @@ class TransformerTests(unittest.TestCase):
         self.failUnless(out.min() >= 0)
         self.failUnless(len(out) == 100)
 
-
-    def testAbsolute(self):
-        target = N.array([ 1,  0, -1, -2, -3])
-
+    def testAbsolute2(self):
+        target = self.d1
         out = OneMinus(N.arange(5))
         self.failUnless((out == target).all())
+
+    def testRankOrder(self):
+        nelements = len(self.d2)
+        out = RankOrder(self.d2)
+        outr = ReverseRankOrder(self.d2)
+        uout = N.unique(out)
+        uoutr = N.unique(outr)
+        self.failUnless((uout == N.arange(nelements)).all(),
+                        msg="We should get all indexes. Got just %s" % uout)
+        self.failUnless((uoutr == N.arange(nelements)).all(),
+                        msg="We should get all indexes. Got just %s" % uoutr)
+        self.failUnless((out+outr+1 == nelements).all())
+        self.failUnless((out == [ 0,  3,  4,  1,  5,  2]).all())
+
+
+    def testUnitNorm(self):
+        out = UnitNorm(self.d2)
+        self.failUnless(N.abs(N.sum(out*out)-1.0) < 1e-10)
 
 
 def suite():
