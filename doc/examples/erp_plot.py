66066c1d5022eefcd10ad2c8360063f3833f0a34
Yaroslav Halchenko <debian@onerussian.com>
2008-09-04 13:16:16 -0400

    NF: an example of plotERPs function

---
+++ doc/examples/erp_plot.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+#emacs: -*- mode: python-mode; py-indent-offset: 4; indent-tabs-mode: nil -*-
+#ex: set sts=4 ts=4 sw=4 et:
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the PyMVPA package for the
+#   copyright and license terms.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+"""Example demonstrating an ERP plot. Actually it is ERF plot since we have MEG data"""
+
+from mvpa.suite import *
+
+# load data
+meg = TuebingenMEG(os.path.join('data', 'tueb_meg.dat.gz'))
+
+
+# Define plots for easy feeding into plotERP
+plots = []
+colors = ['r', 'b', 'g']
+
+# figure out pre-stimulus onset interval
+t0 = -meg.timepoints[0]
+
+plots = [ {'label' : meg.channelids[i],
+           'color' : colors[i],
+           'data' : meg.data[:, i, :]}
+          for i in xrange(len(meg.channelids)) ]
+
+# Common arguments for all plots
+cargs = {
+    'SR' : meg.samplingrate,
+    'pre_onset' : t0,
+    # Plot only 50ms before and 100ms after the onset since we have
+    # just few trials
+    'pre' : 0.05, 'post' : 0.1,
+    # Plot all 'errors' in different degrees of shadings
+    'errtype' : ['ste', 'ci95', 'std'],
+    'legend' : False, # legend manages to obscure the plot
+    }
+
+# Create a new figure
+fig = P.figure(figsize=(12, 8))
+
+# Following plots are plotted inverted (negative up) for the
+# demonstration of this capability and elderly convention for ERP
+# plots. That is controlled with ymult (negative gives negative up)
+
+
+# Plot MEG sensors
+
+# frame_on=False guarantees abent outside rectangular axis with
+# labels. plotERP recreates its own axes centered at (0,0)
+ax = fig.add_subplot(2, 1, 1, frame_on=False)
+plotERPs(plots[:2], ylabel='pT', ymult=-1e12, ax=ax, **cargs)
+
+# Plot EEG sensor
+ax = fig.add_subplot(2, 1, 2, frame_on=False)
+plotERPs(plots[2:3], ax=ax, ymult=-1e6, **cargs)
+
+# Additional example: plotting a single ERP on an existing plot
+# without drawing axis:
+#
+# plotERP(data=meg.data[:, 0, :], SR=meg.samplingrate, pre=pre, pre_mean=pre, errtype=errtype, ymult=-1.0)
+
+if cfg.getboolean('examples', 'interactive', True):
+    # show all the cool figures
+    P.show()
