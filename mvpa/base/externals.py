ed81d7eff056c85ebc3ff280529177da4bfe55b3
Yaroslav Halchenko <debian@onerussian.com>
2008-10-09 00:07:00 -0400

    BF: externals('ctypes') and dynamic default backend for SMLR

--- mvpa/base/externals.py
+++ mvpa/base/externals.py
@@ -81,6 +81,7 @@ def __check_weave():
 # contains list of available (optional) external classifier extensions
 _KNOWN = {'libsvm':'import mvpa.clfs.libsvm._svm as __; x=__.convert2SVMNode',
           'nifti':'from nifti import NiftiImage as __',
+          'ctypes':'import ctypes as __',
           'shogun':'import shogun as __',
           'shogun.mpd': 'import shogun.Classifier as __; x=__.MPDSVM',
           'shogun.lightsvm': 'import shogun.Classifier as __; x=__.SVMLight',
