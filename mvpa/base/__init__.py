fc885a12ae838ca1253f2cb0c3a5e0271ddae982
Yaroslav Halchenko <debian@onerussian.com>
2008-10-12 00:25:40 -0400

    NF(+RF): not yet finished -- matchDistribution to help in determining the underlying distribution

--- mvpa/base/__init__.py
+++ mvpa/base/__init__.py
@@ -224,7 +224,9 @@ if __debug__:
     debug.register('CLFFS',  "FeatureSelectionClassifier")
     debug.register('CLFFS_', "FeatureSelectionClassifier (verbose)")
 
-    debug.register('STAT',   "Some statistics estimates")
+    debug.register('STAT',   "Statistics estimates")
+    debug.register('STAT_',  "Statistics estimates (verbose)")
+    debug.register('STAT__', "Statistics estimates (very verbose)")
 
     debug.register('FS',     "FeatureSelections")
     debug.register('FS_',    "FeatureSelections (verbose)")
