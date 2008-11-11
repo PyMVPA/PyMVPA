2c19b26750407bb87bc30667a8258f0c2dcd5c66
Michael Hanke <michael.hanke@gmail.com>
2008-11-11 08:39:09 +0100

    Bump version to 0.4.0

--- doc/conf.py
+++ doc/conf.py
@@ -277,9 +277,9 @@ copyright = '2006-2008, Michael Hanke, Yaroslav Halchenko, Per B. Sederberg'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.3.1'
+version = '0.4.0'
 # The full version, including alpha/beta/rc tags.
-release = '0.3.1'
+release = '0.4.0'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
