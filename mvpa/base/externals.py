d4239b82c73c6de767aec817213cd7330b1d8f9b
Michael Hanke <michael.hanke@gmail.com>
2008-11-14 17:43:22 +0100

    BF: Adjust externals test to test for pynifti >= 0.20081017.1

--- mvpa/base/externals.py
+++ mvpa/base/externals.py
@@ -93,7 +93,7 @@ def __check_atlas_family(family):
 
 # contains list of available (optional) external classifier extensions
 _KNOWN = {'libsvm':'import mvpa.clfs.libsvmc._svm as __; x=__.convert2SVMNode',
-          'nifti':'from nifti import NiftiImage as __',
+          'nifti':'from nifti.nifticlib import detachDataFromImage as __',
           'ctypes':'import ctypes as __',
           'shogun':'import shogun as __',
           'shogun.mpd': 'import shogun.Classifier as __; x=__.MPDSVM',
