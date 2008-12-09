f11515724db7f2af796ecdd8ca0bab0ae9dbcbf5
Yaroslav Halchenko <debian@onerussian.com>
2008-12-09 01:25:22 -0500

    BF: adding forgotten part

---
+++ mvpa/support/copy.py
@@ -0,0 +1,23 @@
+#emacs: -*- mode: python-mode; py-indent-offset: 4; indent-tabs-mode: nil -*-
+#ex: set sts=4 ts=4 sw=4 et:
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the PyMVPA package for the
+#   copyright and license terms.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+"""Support for python's copy module.
+
+"""
+
+__docformat__ = 'restructuredtext'
+
+import sys
+
+# We have to use deepcopy from python 2.5, since otherwise it fails to
+# copy sensitivity analyzers with assigned combiners which are just
+# functions not functors
+if sys.version_info[0] > 2 or sys.version_info[1] > 4:
+    from copy import copy, deepcopy
+else:
+    from mvpa.support._copy import copy, deepcopy
