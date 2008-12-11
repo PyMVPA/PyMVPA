8e71415eef9d6dbae66246dd262dc38e414af80b
Michael Hanke <michael.hanke@gmail.com>
2008-12-11 21:51:01 +0100

    DOC: Polish SOM example.

--- doc/examples/som.py
+++ doc/examples/som.py
@@ -17,7 +17,13 @@ This is a demonstration that shows how a self-organizing map (SOM) can be used
 to map high-dimensional data into a two-dimensional representation. For the sake
 of an easy visualization 'high-dimensional' in this case is 3D.
 
-The SOM will map a number of colors into a rectangular area.
+In general SOMs might be useful for visualizing high-dimensional data in terms
+of the its similarity structure. Especially large SOMs (i.e. large number of
+Kohonen units) are known to perform mappings that preserve the topology of the
+original data, i.e. neighboring data points in input space will also be
+represented in adjacent locations on the SOM.
+
+In this example the SOM will map a number of colors into a rectangular area.
 """
 
 import pylab as P
@@ -31,23 +37,23 @@ First, we define some colors as RGB values from the interval (0,1), i.e. with
 white being (1, 1, 1) and black being (0, 0, 0).
 """
 
-colors = [[0., 0., 0.],
-          [0., 0., 1.],
-          [0., 1., 0.],
-          [1., 0., 0.],
-          [0., 1., 1.],
-          [1., 0., 1.],
-          [1., 1., 0.],
-          [1., 1., 1.],
-          [.33, .33, .33],
-          [.5, .5, .5],
-          [.66, .66, .66]]
+colors = [[0., 0., 0.],    # black
+          [0., 0., 1.],    # blue
+          [0., 1., 0.],    # green
+          [1., 0., 0.],    # red
+          [0., 1., 1.],    # cyan
+          [1., 0., 1.],    # violet
+          [1., 1., 0.],    # yellow
+          [1., 1., 1.],    # white
+          [.33, .33, .33], # darkgrey
+          [.5, .5, .5],    # mediumgrey
+          [.66, .66, .66]] # lightgrey
 
 """
 Since we are going to use a mapper, we will put the color vectors into a
-dataset. To be able to do this, we will assign an arbitrary labels. This
-will not be considered at all, since this SOM mapper uses an unsupervised
-training algorithm.
+dataset. To be able to do this, we will assign an arbitrary label, although
+it will not be used at all, since this SOM mapper uses an unsupervised training
+algorithm.
 """
 
 ds = Dataset(samples=colors, labels=1)
@@ -56,7 +62,7 @@ ds = Dataset(samples=colors, labels=1)
 Now we can instanciate the mapper. It will internally use a so-called Kohonen
 layer to map the data onto. We tell the mapper to use a rectangular layer with
 30 x 20 units. This will be the output space of the mapper. Additionally, we
-tell it to train the network using 400 iterations and some learning rate.
+tell it to train the network using 400 iterations and to use some learning rate.
 """
 
 som = SimpleSOMMapper(30, 20, 400, learning_rate=0.05)
