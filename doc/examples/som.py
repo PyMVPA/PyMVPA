2ea47b61ea19117a3c1448894c0f3d266ee572d6
Michael Hanke <michael.hanke@gmail.com>
2008-12-15 14:22:46 +0100

    NF: Added fMRI data analysis to the SOM example -- Nice!

--- doc/examples/som.py
+++ doc/examples/som.py
@@ -26,11 +26,7 @@ represented in adjacent locations on the SOM.
 In this example the SOM will map a number of colors into a rectangular area.
 """
 
-import pylab as P
-
-from mvpa import cfg
-from mvpa.datasets import Dataset
-from mvpa.mappers.som import SimpleSOMMapper
+from mvpa.suite import *
 
 """
 First, we define some colors as RGB values from the interval (0,1), i.e. with
@@ -98,10 +94,98 @@ the training data to the mapper and plot the output.
 
 mapped = som(colors)
 
+P.title('Color SOM')
 # SOM's kshape is (rows x columns), while matplotlib wants (X x Y)
 for i, m in enumerate(mapped):
     P.text(m[1], m[0], color_names[i], ha='center', va='center',
            bbox=dict(facecolor='white', alpha=0.5, lw=0))
+
+
+"""
+But now, let's do something more interesting and take a look at similarity
+structures in fMRI data. This example makes use of the small dataset shipped
+with the source distribution of PyMVPA. First, we loaded the full dataset,
+while preserving the literal labels of each sample ...
+"""
+
+attr = SampleAttributes('data/attributes_literal.txt', literallabels=True)
+ds = NiftiDataset(samples='data/bold.nii.gz',
+                  labels=attr.labels,
+                  chunks=attr.chunks,
+                  mask='data/mask.nii.gz',
+                  labels_map=True)
+
+"""
+... detrend the whole timeseries ...
+"""
+
+detrend(ds, perchunk=True, model='linear')
+
+"""
+... and finally select four of the included stimulus conditions for this
+example: faces, houses, shoes and chairs.
+"""
+
+ds = ds.select(labels=[ds.labels_map[i] for i in ['face', 'house', 'shoe', 'chair']])
+
+"""
+For a significant speed-up of the procedure, we simply use the 5% of the voxels
+with the highest ANOVA F-scores.
+"""
+
+fs = SensitivityBasedFeatureSelection(
+        OneWayAnova(),
+        FractionTailSelector(0.05,
+                             tail='upper',
+                             mode='select'))
+ds = fs(ds)[0]
+
+"""
+The last preprocessing step is a simple normalization of all thresholded
+features.
+"""
+
+zscore(ds, perchunk=True, targetdtype='float32')
+
+"""
+More or less identical to the previous example we train a SOM on this new fMRI
+dataset. The only difference is a slightly larger Kohonen layer and an adjusted
+initial learning rate. Due to the size of the dataset and the larger network
+the SOM training will take one or two minutes, therefore we enable some debug
+output to entertain ourselves while waiting...
+"""
+
+if __debug__:
+  debug.active += ['SOM']
+
+
+som = SimpleSOMMapper((40, 40), 400, learning_rate=0.001)
+som.train(ds)
+
+"""
+After the training has been completed, we can now take a look at the data
+structure by simply mapping the whole dataset using the SOM and plot the
+resulting 2D vectors.
+"""
+
+mapped = som(ds.samples)
+
+# start new figure
+P.figure()
+P.title('Object category fMRI SOM')
+
+# we need a reversed labels map for the figure legend
+rlm = dict([(v, k) for k, v in ds.labels_map.iteritems()])
+
+# define arbitrary colors for each stimulus condition
+pcolors = ['red', 'blue', 'yellow', 'black']
+for i, l in enumerate(ds.uniquelabels):
+    P.scatter(mapped[ds.labels==l, 0],
+              mapped[ds.labels==l, 1],
+              color=pcolors[i],
+              label=rlm[l])
+# auto-generate legend
+P.legend()
 
 # show the figure
 if cfg.getboolean('examples', 'interactive', True):
