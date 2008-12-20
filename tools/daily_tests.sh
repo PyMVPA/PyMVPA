1b71646ff4b35f1e6fcd9e8bee2c9da6baf565c3
Yaroslav Halchenko <debian@onerussian.com>
2008-12-20 00:46:44 -0500

    BF: daily_tests beta version + few ignores

--- tools/daily_tests.sh
+++ tools/daily_tests.sh
@@ -8,60 +8,92 @@
 #
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 #
-# Helper to run all the tests daily at night
+#                 Helper to run all the tests daily at night
 #
 
-set -eu
+set -e
 
 # what branches to test
 BRANCHES='master yoh/master mh/master maint/0.4'
 # where to send reports
-EMAILS=yoh@onerussian.com,michael.hanke@gmail.com
+# XXX
+EMAILS='yoh@onerussian.com,yarikoptic@gmail.com'
+#,michael.hanke@gmail.com
 
-repo=git://git.debian.org/git/pkg-exppsy/pymvpa.git
+precmd=
+#precmd="echo  C: "
+# XXX
+repo="git://git.debian.org/git/pkg-exppsy/pymvpa.git"
 
-tmpdir=/tmp/pymvpa_tests-$(date +"20%y%m%d-%H:%M:%S")
-mkdir $tmpdir
-tmpfile=$tmpdir.tmp
-logfile=$tmpdir.log
-trap "rm -fr $tmpdir;" EXIT
+ds=`date +"20%y%m%d_%H%M%S"`
+topdir=$HOME/proj/pymvpa
+logdir="$topdir/logs/daily/pymvpa_tests-$ds"
+tmpfile="$logdir/tmp.log"
+logfile="$logdir/all.log"
+
+# Remove
+#trap "rm -fr $logdir/pymvpa $logdir/tmp.log;" EXIT
+
+mkdir -p "$logdir"
 
 indent() {
 	sed -e 's/^/  /g'
 }
 
 do_checkout() {
-    git clean -df | indent
-    git reset --hard
-    if [ ! $branch = 'master' ]; then
-    	git checkout -b $branch origin/$branch
-    fi
-    git checkout $branch
+    $precmd git clean -df | indent
+    $precmd git reset --hard
+    #if [ ! $branch = 'master' ]; then
+    $precmd git checkout -b $branch origin/$branch || :
+    #fi
+    $precmd git checkout $branch
 }
 
 do_build() {
-    make clean
-    make
+    $precmd make clean
+    $precmd make
 }
 
-do_test() {
-    make -k test
+do_clean() {
+	# verify that cleaning works as desired
+    $precmd make clean
+    $precmd git clean -n | grep -q . \
+        && { git clean -n; return 1; } \
+        || return 0
 }
 
-ACTIONS='checkout build test'
+# Not yet can do fine scale unittest separation since maint/0.4, nor
+# master have it that way... leaving it for future.
+#MAKE_TESTS="unittest unittest-optimization unittest-debug unittest-badexternals
+MAKE_TESTS="unittests testmanual testsuite testapiref testexamples"
 
+for c in $MAKE_TESTS; do
+    eval "do_$c() { $precmd make $c; }"
+done
+
+# What actions/tests to run per each branch
+ACTIONS="checkout build $MAKE_TESTS clean"
+
+# Counters
+failed=0
+succeeded=0
+
+# skip the tests we can't fully trust to sleep well
+export MVPA_TESTS_LABILE=no
+
+# need to be a function to share global failed/succeded
+sweep()
 {
-    cd $tmpdir
+    cd $logdir
 
     echo "I:" $(date)
 
     # checkout the repository
     echo "I: Cloning repository"
-    git clone $repo 2>&1 | indent
-    cd pymvpa
+    $precmd git clone $repo 2>&1 | indent
+    $precmd cd pymvpa
+    # no need to check here since checkout would fail below otherwise
 
-	failed=0
-	succeeded=0
     #
     # Sweep through the branches and actionsto test
     #
@@ -87,15 +119,21 @@ ACTIONS='checkout build test'
 			branches_with_problems+="\n  $branch: $branch_has_problems"
 		fi
     done
-	echo "I: Succeeded $succeeded actions, failed $failed."
+	echo "I: Succeeded $succeeded actions, failed $failed actions."
 	if [ "x$branches_with_problems" != x ]; then
 		echo -e "I: Branches which experienced problems: $branches_with_problems"
 	fi
 
+    echo
     echo "I:" $(date)
     echo "I: Exiting. Logfile $logfile"
+}
 
-} >| $logfile 2>&1
+sweep >| $logfile 2>&1
 
-sline=$(grep 'I: Succeeded' $logfile)
-cat $logfile | mail -s "PyMVPA: daily testing $sline" $EMAILS
+# Email only if any test has failed
+#[ ! $failed = 0 ] && \
+# Email always since it is better to see that indeed everything is smooth
+# and to confirm that it is tested daily
+cat $logfile | mail -s "PyMVPA: daily testing: +$succeeded/-$failed" $EMAILS
+
