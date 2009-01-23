63fa3c72993293dc661e642e5329f55d7addaebc
Michael Hanke <michael.hanke@gmail.com>
2009-01-23 15:19:01 +0100

    DOC: Typo.

--- mvpa/misc/support.py
+++ mvpa/misc/support.py
@@ -281,7 +281,7 @@ class Event(dict):
     """Simple class to define properties of an event.
 
     The class is basically a dictionary. Any properties can
-    be pass as keyword arguments to the constructor, e.g.:
+    be passed as keyword arguments to the constructor, e.g.:
 
       >>> ev = Event(onset=12, duration=2.45)
 
