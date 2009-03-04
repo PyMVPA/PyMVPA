38f23f3d3a2970d4dd6c3013e60dcc8f6a3fee77
Yaroslav Halchenko <debian@onerussian.com>
2009-03-04 10:33:08 -0500

    BF: getUniqueLengthNCombinations temporary fix for n>2 use binary representation of a number

--- mvpa/tests/test_support.py
+++ mvpa/tests/test_support.py
@@ -85,7 +85,8 @@ class SupportFxTests(unittest.TestCase):
                         )
         self.failUnlessEqual(
             getUniqueLengthNCombinations(
-                        range(4), 3 ), [[0, 1, 2], [0, 1, 3], [0, 2, 3]] )
+                        range(4), 3 ),
+                        [[0, 1, 2], [0, 1, 3], [0, 2, 3], [1, 2, 3]] )
 
 
     def testBreakPoints(self):
