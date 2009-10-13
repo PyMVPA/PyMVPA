7d5d7940a1ab6b2c56f45d0d3b0f3ee38b6f9052
Yaroslav Halchenko <debian@onerussian.com>
2009-10-13 13:36:22 -0400

    NF: temporary solution to run nose tests as well -- revert/redo whenever

--- mvpa/tests/__init__.py
+++ mvpa/tests/__init__.py
@@ -121,6 +121,29 @@ def collectTestSuites():
     return dict([(t[5:], eval(t + '.suite()')) for t in tests ])
 
 
+def collectNoseTests():
+    """Return list of tests which are pure nose-based
+    """
+    tests = [ 'test_datasetng',
+              'test_attrmap',
+              ]
+    return tests
+
+def runNoseTests():
+    """Run nose-based tests -- really really silly way, just to get started
+
+    TODO: just switch to using numpy.testing framework, for that
+          unittests need to be cleaned and unified first
+    """
+    nosetests = collectNoseTests()
+    if not externals.exists('nose'):
+        warning("You do not have python-nose installed -- no tests %s were ran"
+                % (', '.join(nosetests)))
+        return
+    from nose import main
+    # main.config.verbosity = int(cfg.get('tests', 'verbosity', default=1))
+    for nt in nosetests:
+        main(defaultTest='mvpa.tests.' + nt, exit=False)
 
 def run(limit=None, verbosity=None):
     """Runs the full or a subset of the PyMVPA unittest suite.
@@ -171,6 +194,9 @@ def run(limit=None, verbosity=None):
 
     # finally run it
     TextTestRunnerPyMVPA(verbosity=verbosity).run(ts)
+
+    # and nose tests
+    runNoseTests()
 
     # restore warning handlers
     warning.handlers = handler_backup
