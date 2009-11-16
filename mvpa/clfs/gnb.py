ecc7f5db123205c600c8364b325e6359f38089cd
Yaroslav Halchenko <debian@onerussian.com>
2009-11-15 23:21:02 -0500

    very sloppy implementation of GNB is done -- not sure even if syntax is alrgight ;)

--- mvpa/clfs/gnb.py
+++ mvpa/clfs/gnb.py
@@ -47,6 +47,12 @@ class GNB(Classifier):
     common_variance = Parameter(False, allowedtype='bool',
              doc="""Assume variance common across all classes.""")
 
+    full = Parameter(True, allowedtype='bool',
+             doc="""Full computation of probabilities.
+             Just for classification (if not interested in actual
+             probabilities stored in values) with `common_variance`=True
+             it is possible to avoid computation of exponents... XXX""")
+
     def __init__(self, **kwargs):
         """Initialize an GNB classifier.
         """
@@ -56,8 +62,13 @@ class GNB(Classifier):
 
         # pylint friendly initializations
         self.means = None
+        """Means of features per class"""
         self.std2s = None
-
+        """Variances per class, but "vars" is taken ;)"""
+        self.ulabels = None
+        """Labels classifier was trained on"""
+        self.class_prob = None
+        """Class probabilities"""
 
     def _train(self, dataset):
         """Train the classifier using `dataset` (`Dataset`).
@@ -65,9 +76,10 @@ class GNB(Classifier):
         # get the dataset information into easy vars
         X = dataset.samples
         labels = dataset.labels
-        ulabels = dataset.uniquelabels
+        self.ulabels = ulabels = dataset.uniquelabels
         nlabels = len(ulabels)
         #params = self.params        # for quicker access
+        label2index = dict((l, il) for il,l in enumerate(ulabels))
 
         common_variance = self.params.common_variance
 
@@ -77,49 +89,45 @@ class GNB(Classifier):
         # XXX Sloppy implementation for now but it would have its advantages:
         #     0.  simple and straightforward
         #     1.  no data copying
-        #     2.  uses dictionaries, so no need to care about
-        #         mapping labels back and forth
+        #     2.  should work for any dimensionality of samples
         #  Let's later see how more efficient, more numpy-friendly
         #  approaches would perform
         s_shape = X.shape[1:]           # shape of a single sample
 
-        stats = np.zeros((2, nlabels, ) + s_shape)
-
-        # dictionaries are actually just lookups into arrays above
-        means = dict([(l, stats[0][il]) for il,l in enumerate(ulabels)])
-        std2s = dict([(l, stats[1][il]) for il,l in enumerate(ulabels)])
-        # for this one we would need a labels map
-        label2index = dict((l, il) for il,l in enumerate(ulabels))
-        nsamples = np.zeros((nlabels,) + (1,)*len(s_shape)) # degenerate dimension for easy broadcasting later on
+        self.means = means = \
+                     N.zeros((nlabels, ) + s_shape)
+        self.std2s = std2s = \
+                     N.zeros((nlabels, ) + s_shape)
+        # degenerate dimension are added for easy broadcasting later on
+        nsamples_per_class = N.zeros((nlabels,) + (1,)*len(s_shape))
 
         # Estimate means and number of samples per each label
         for s, l in zip(X, labels):
-            nsamples[label2index[l]] += 1
-            means[l] += s
+            il = label2index[l]         # index of the label
+            nsamples_per_class[il] += 1
+            means[il] += s
 
         ## Actually compute the means
-        ## XXX disadvantage of dicts -- explicit loops :-/
-        ##     may be we could trick dicts by binding into
-        ##     corresponding rows... later on
-        non0labels = nsamples != 0
-        stats[0, non0labels] /= nsamples[non0labels]
+        non0labels = nsamples_per_class != 0
+        means[non0labels] /= nsamples_per_class[non0labels]
 
         # Estimate std2s
         # better loop than repmat! ;)
         for s, l in zip(X, labels):
-            std2s[l] += (s - means[l])**2
+            il = label2index[l]         # index of the label
+            std2s[il] += (s - means[il])**2
 
         ## Actually compute the std2s
         if common_variance:
             # we need to get global std
-            cvar = N.sum(stats[1], axis=0)/nsamples # sum across labels
-            # broadcast same variance across labels
-            stats[1][:] = cvar
+            cvar = N.sum(std2s, axis=0)/nsamples # sum across labels
+            # broadcast the same variance across labels
+            std2s[:] = cvar
         else:
-            stats[1][non0labels] /= nsamples[non0labels]
+            std2s[non0labels] /= nsamples_per_class[non0labels]
 
-        # Store means and std2s
-        self.
+        # Store class_probabilities
+        self.class_prob = nsamples_per_class / float(nsamples)
 
         if __debug__ and 'GNB' in debug.active:
             debug('GNB', "training finished on data.shape=%s " %
@@ -127,33 +135,57 @@ class GNB(Classifier):
                   "min:max(data)=%f:%f, got min:max(w)=%f:%f" %
                   (N.min(X), N.max(X), N.min(b_mean), N.max(b_mean)))
 
+
     def untrain(self):
         """Untrain classifier and reset all learnt params
         """
         self.means = None
         self.std2s = None
+        self.ulabels = None
+        self.class_prob = None
         super(GNB, self).reset()
 
 
     def _predict(self, data):
         """Predict the output for the provided data.
         """
+        if self.params.full:
+            # Just a regular Normal distribution with per
+            # feature/class mean and std2s
+            prob_csfs = \
+                 1.0/N.sqrt(2*N.pi*self.std2s) * \
+                 N.exp(-0.5 * (((data - self.means[:, N.newaxis, ...])**2)\
+                               / self.std2s[:, N.newaxis, ...]))
+        else:
+            # if self.params.common_variance:
+            # XXX YOH:
+            # For decision there is no need to actually compute
+            # properly scaled p, ie 1/sqrt(2pi * sigma_i) could be
+            # simply discarded since it is common across features AND
+            # classes
+            raise NotImplemented, '"Optimized" GNB prediction is not here yet'
 
-        #if self.params.common_variance:
-        # XXX YOH:
-        # For decision there is no need to actually compute
-        # properly scaled p, ie 1/sqrt(2pi * sigma_i) could be
-        # simply discarded since it is common across features AND
-        # classes
+        # Naive part -- just a product of probabilities across features
+        ## First we need to reshape to get class x samples x features
+        prob_csf = prob_csfs.reshape(
+            prob_csfs.shape[:2] + (-1,))
+        ## Now -- product across features
+        prob_cs = prob_csf.prod(axis=2)
 
-        # XXX
-        self.values = None              # set to the probabilities
+        # Incorporate class probabilities:
+        prob_cs_cp = prob_cs * self.class_prob[:, N.newaxis]
+
+        # Take the class with maximal probability
+        winners = prob_cs_cp.argmax(axis=0)
+        predictions = [self.ulabels[c] for c in winners]
+
+        self.values = prob_cs.T         # set to the probabilities per class
 
         if __debug__ and 'GNB' in debug.active:
             debug('GNB', "predict on data.shape=%s min:max(data)=%f:%f " %
                   (`data.shape`, N.min(data), N.max(data)))
 
-        return dot_prod
+        return predictions
 
 
     # XXX Later come up with some
