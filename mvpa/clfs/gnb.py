e4822c2a6da63b7be1eea1177de73a502baaafa6
Yaroslav Halchenko <debian@onerussian.com>
2009-11-15 22:05:21 -0500

    on the way to insane implementation of GNB... lets simplify

--- mvpa/clfs/gnb.py
+++ mvpa/clfs/gnb.py
@@ -56,7 +56,7 @@ class GNB(Classifier):
 
         # pylint friendly initializations
         self.means = None
-        self.stds = None
+        self.std2s = None
 
 
     def _train(self, dataset):
@@ -66,26 +66,60 @@ class GNB(Classifier):
         X = dataset.samples
         labels = dataset.labels
         ulabels = dataset.uniquelabels
+        nlabels = len(ulabels)
         #params = self.params        # for quicker access
 
         common_variance = self.params.common_variance
 
         # set the feature dimensions
-        ns, nd = X.shape
+        nsamples = len(X)
 
         # XXX Sloppy implementation for now but it would have its advantages:
         #     0.  simple and straightforward
         #     1.  no data copying
         #     2.  uses dictionaries, so no need to care about
         #         mapping labels back and forth
+        #  Let's later see how more efficient, more numpy-friendly
+        #  approaches would perform
         s_shape = X.shape[1:]           # shape of a single sample
-        means = dict([(l, {}) for l in ulabels])
-        stds = dict([(l, {}) for l in ulabels])
-        nsamples = dict([(l, 0) for l in ulabels])
+
+        stats = np.zeros((2, nlabels, ) + s_shape)
+
+        # dictionaries are actually just lookups into arrays above
+        means = dict([(l, stats[0][il]) for il,l in enumerate(ulabels)])
+        std2s = dict([(l, stats[1][il]) for il,l in enumerate(ulabels)])
+        # for this one we would need a labels map
+        label2index = dict((l, il) for il,l in enumerate(ulabels))
+        nsamples = np.zeros((nlabels,) + (1,)*len(s_shape)) # degenerate dimension for easy broadcasting later on
 
         # Estimate means and number of samples per each label
         for s, l in zip(X, labels):
-            XXX
+            nsamples[label2index[l]] += 1
+            means[l] += s
+
+        ## Actually compute the means
+        ## XXX disadvantage of dicts -- explicit loops :-/
+        ##     may be we could trick dicts by binding into
+        ##     corresponding rows... later on
+        non0labels = nsamples != 0
+        stats[0, non0labels] /= nsamples[non0labels]
+
+        # Estimate std2s
+        # better loop than repmat! ;)
+        for s, l in zip(X, labels):
+            std2s[l] += (s - means[l])**2
+
+        ## Actually compute the std2s
+        if common_variance:
+            # we need to get global std
+            cvar = N.sum(stats[1], axis=0)/nsamples # sum across labels
+            # broadcast same variance across labels
+            stats[1][:] = cvar
+        else:
+            stats[1][non0labels] /= nsamples[non0labels]
+
+        # Store means and std2s
+        self.
 
         if __debug__ and 'GNB' in debug.active:
             debug('GNB', "training finished on data.shape=%s " %
@@ -97,8 +131,7 @@ class GNB(Classifier):
         """Untrain classifier and reset all learnt params
         """
         self.means = None
-        self.stds = None
-        self._class_labels = None
+        self.std2s = None
         super(GNB, self).reset()
 
 
@@ -106,6 +139,12 @@ class GNB(Classifier):
         """Predict the output for the provided data.
         """
 
+        #if self.params.common_variance:
+        # XXX YOH:
+        # For decision there is no need to actually compute
+        # properly scaled p, ie 1/sqrt(2pi * sigma_i) could be
+        # simply discarded since it is common across features AND
+        # classes
 
         # XXX
         self.values = None              # set to the probabilities
@@ -127,7 +166,7 @@ class GNB(Classifier):
 
     # XXX Is there any reason to use properties?
     #means = property(lambda self: self.__biases)
-    #stds = property(lambda self: self.__weights)
+    #std2s = property(lambda self: self.__weights)
 
 
 
