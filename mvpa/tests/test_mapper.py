61ccec175ebecd97aabf2f92673f94c4057fe996
Michael Hanke <michael.hanke@gmail.com>
2009-11-19 19:47:21 -0500

    NF+RF: Functional ChainMapper (as a mapper container)

--- mvpa/tests/test_mapper.py
+++ mvpa/tests/test_mapper.py
@@ -9,13 +9,15 @@
 '''Tests for basic mappers'''
 
 import numpy as N
+# for repr
+from numpy import array
 
 from numpy.testing import assert_array_equal
 from nose.tools import ok_, assert_raises, assert_false, assert_equal, \
         assert_true
 
 from mvpa.mappers.flatten import FlattenMapper
-from mvpa.mappers.base import FeatureSubsetMapper
+from mvpa.mappers.base import FeatureSubsetMapper, ChainMapper
 from mvpa.support.copy import copy
 
 def test_flatten():
@@ -29,64 +31,68 @@ def test_flatten():
               [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63]]
     target = N.array(target)
 
-    fm = FlattenMapper()
-    # not working if untrained
-    assert_raises(RuntimeError, fm.forward, N.arange(N.sum(samples_shape) + 1))
+    # actually, there should be no difference between a plain FlattenMapper and
+    # a chain that only has a FlattenMapper as the one element
+    for fm in [FlattenMapper(), ChainMapper([FlattenMapper()])]:
+        # not working if untrained
+        assert_raises(RuntimeError,
+                      fm.forward,
+                      N.arange(N.sum(samples_shape) + 1))
 
-    fm.train(data)
+        fm.train(data)
 
-    assert_array_equal(fm.forward(data), target)
-    assert_array_equal(fm.forward(data[2]), target[2])
-    assert_raises(ValueError, fm.forward, N.arange(4))
-    assert_raises(ValueError, fm.forward, N.array(data[0], order='F'))
+        assert_array_equal(fm.forward(data), target)
+        assert_array_equal(fm.forward(data[2]), target[2])
+        assert_raises(ValueError, fm.forward, N.arange(4))
+        assert_raises(ValueError, fm.forward, N.array(data[0], order='F'))
 
-    # check coordinate2id conversion
-    got_coord = N.zeros((N.prod(samples_shape)), dtype='bool')
-    for i in xrange(samples_shape[0]):
-        for j in xrange(samples_shape[1]):
-            for k in xrange(samples_shape[2]):
-                id = fm.get_outids([(i, j, k)])[0]
-                assert_equal(len(id), 1)
-                # mark that we got this particular ID
-                got_coord[id[0]] = True
-    # each id has to have occurred
-    ok_(got_coord.all())
-    # invalid caught?
-    assert_raises(ValueError, fm.get_outids, samples_shape)
-    assert_raises(ValueError, fm.get_outids, (0,0,0,0))
+        # check coordinate2id conversion
+        got_coord = N.zeros((N.prod(samples_shape)), dtype='bool')
+        for i in xrange(samples_shape[0]):
+            for j in xrange(samples_shape[1]):
+                for k in xrange(samples_shape[2]):
+                    id = fm.get_outids([(i, j, k)])[0]
+                    assert_equal(len(id), 1)
+                    # mark that we got this particular ID
+                    got_coord[id[0]] = True
+        # each id has to have occurred
+        ok_(got_coord.all())
+        # invalid caught?
+        assert_raises(ValueError, fm.get_outids, samples_shape)
+        assert_raises(ValueError, fm.get_outids, (0,0,0,0))
 
-    # all of that leaves that data unmodified
-    assert_array_equal(data, pristinedata)
+        # all of that leaves that data unmodified
+        assert_array_equal(data, pristinedata)
 
-    # reverse mapping
-    assert_array_equal(fm.reverse(target), data)
-    assert_array_equal(fm.reverse(target[0]), data[0])
-    assert_array_equal(fm.reverse(target[1:2]), data[1:2])
-    assert_raises(ValueError, fm.reverse, N.arange(14))
-    assert_raises(ValueError, fm.reverse, N.array(target, order='F'))
+        # reverse mapping
+        assert_array_equal(fm.reverse(target), data)
+        assert_array_equal(fm.reverse(target[0]), data[0])
+        assert_array_equal(fm.reverse(target[1:2]), data[1:2])
+        assert_raises(ValueError, fm.reverse, N.arange(14))
+        assert_raises(ValueError, fm.reverse, N.array(target, order='F'))
 
-    # check one dimensional data, treated as scalar samples
-    oned = N.arange(5)
-    oned_target = [[0],[1],[2],[3],[4]]
-    fm.train(oned)
-    assert_array_equal(fm.forward(oned), oned_target)
-    assert_array_equal(fm.reverse(N.array(oned_target)), oned)
-    assert_equal(fm.get_outids([0])[0], [0])
-    assert_raises(ValueError, fm.get_outids, [5])
-    assert_raises(ValueError, fm.get_outids, [(0,0)])
+        # check one dimensional data, treated as scalar samples
+        oned = N.arange(5)
+        oned_target = [[0],[1],[2],[3],[4]]
+        fm.train(oned)
+        assert_array_equal(fm.forward(oned), oned_target)
+        assert_array_equal(fm.reverse(N.array(oned_target)), oned)
+        assert_equal(fm.get_outids([0])[0], [0])
+        assert_raises(ValueError, fm.get_outids, [5])
+        assert_raises(ValueError, fm.get_outids, [(0,0)])
 
-    # check one dimensional samples
-    oneds = N.array([range(5)])
-    oneds_target = [[0, 1, 2, 3, 4]]
-    fm.train(oneds)
-    assert_array_equal(fm.forward(oneds), oneds_target)
-    assert_array_equal(fm.forward(oneds[0]), oneds_target[0])
-    assert_array_equal(fm.reverse(N.array(oneds_target)), oneds)
-    assert_array_equal(fm.reverse(N.array(oneds_target[0])), oneds[0])
-    assert_equal(fm.get_outids([0])[0], [0])
-    assert_equal(fm.get_outids([3])[0], [3])
-    assert_raises(ValueError, fm.get_outids, [5])
-    assert_raises(ValueError, fm.get_outids, [(0,0)])
+        # check one dimensional samples
+        oneds = N.array([range(5)])
+        oneds_target = [[0, 1, 2, 3, 4]]
+        fm.train(oneds)
+        assert_array_equal(fm.forward(oneds), oneds_target)
+        assert_array_equal(fm.forward(oneds[0]), oneds_target[0])
+        assert_array_equal(fm.reverse(N.array(oneds_target)), oneds)
+        assert_array_equal(fm.reverse(N.array(oneds_target[0])), oneds[0])
+        assert_equal(fm.get_outids([0])[0], [0])
+        assert_equal(fm.get_outids([3])[0], [3])
+        assert_raises(ValueError, fm.get_outids, [5])
+        assert_raises(ValueError, fm.get_outids, [(0,0)])
 
 
 
@@ -163,7 +169,12 @@ def test_subset():
 
 
 def test_coordspaces():
-    sm = FeatureSubsetMapper(N.ones(5), inspace='myspace')
+    # this time give mask only by its target length
+    sm = FeatureSubsetMapper(5, inspace='myspace')
+
+    # check reproduction
+    sm_clone = eval(repr(sm))
+    assert_equal(repr(sm_clone), repr(sm))
 
     # id transformation
     # works without spaces (see above) should still work with them
@@ -188,3 +199,65 @@ def test_coordspaces():
         out = sm.get_outids(unknown=[id])
         assert_equal(out[0], [])
         assert_true(out[1].has_key('unknown'))
+
+
+def test_chainmapper():
+    # the chain needs at lest one mapper
+    assert_raises(ValueError, ChainMapper, [])
+    # a typical first mapper is to flatten
+    cm = ChainMapper([FlattenMapper()])
+
+    # few container checks
+    assert_equal(len(cm), 1)
+    assert_true(isinstance(cm[0], FlattenMapper))
+
+    # now training
+    # come up with data
+    samples_shape = (2, 2, 4)
+    data_shape = (4,) + samples_shape
+    data = N.arange(N.prod(data_shape)).reshape(data_shape)
+    pristinedata = data.copy()
+    target = [[ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15],
+              [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
+              [32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47],
+              [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63]]
+    target = N.array(target)
+
+    # if it is not trained it knows nothing
+    assert_equal(cm.get_outsize(), None)
+    cm.train(data)
+    assert_equal(cm.get_outsize(), 16)
+
+    # a new mapper should appear when doing feature selection
+    cm.discard_out([0])
+    assert_equal(cm.get_outsize(), 15)
+    assert_equal(len(cm), 2)
+    # if there is already a FeatureSubsetMapper at the end, nothing is added
+    cm.select_out([9,14])
+    assert_equal(cm.get_outsize(), 2)
+    assert_equal(len(cm), 2)
+    # only two left
+    assert_true(cm.is_valid_outid(0))
+    assert_true(cm.is_valid_outid(1))
+    assert_false(cm.is_valid_outid(2))
+    # but input is still full
+    assert_equal(cm.get_insize(), 16)
+
+    # we cannot add a mapper with the wrong size
+    assert_raises(ValueError, cm.append, FeatureSubsetMapper(16))
+
+    # check reproduction
+    print repr(cm)
+    cm_clone = eval(repr(cm))
+    assert_equal(repr(cm_clone), repr(cm))
+
+    # what happens if we retrain the whole beast an same data as before
+    cm.train(data)
+    assert_equal(cm.get_outsize(), 2)
+    assert_equal(len(cm), 2)
+    # only two left
+    assert_true(cm.is_valid_outid(0))
+    assert_true(cm.is_valid_outid(1))
+    assert_false(cm.is_valid_outid(2))
+    # but input is still full
+    assert_equal(cm.get_insize(), 16)
