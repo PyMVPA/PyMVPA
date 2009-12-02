318bae7ac43fd2d7e452a7b59d84b2965ec2c94a
Yaroslav Halchenko <debian@onerussian.com>
2009-12-01 19:20:07 -0500

    base structure for new and unified kernels

---
+++ mvpa/kernels/sg.py
@@ -0,0 +1,19 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the PyMVPA package for the
+#   copyright and license terms.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+"""PyMVPA shogun-based kernels
+
+"""
+
+__docformat__ = 'restructuredtext'
+
+from mvpa.base import externals, warning
+if externals.exists('shogun', raiseException=True):
+    import shogun.Kernel as sgk
+
+
