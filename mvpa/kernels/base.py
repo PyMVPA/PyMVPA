816d471c26d6ba3de5e7ef5f504721c19a8c5ec3
Yaroslav Halchenko <debian@onerussian.com>
2009-12-02 09:33:32 -0500

    kernels/base: minor changes + header + 1 BF

--- mvpa/kernels/base.py
+++ mvpa/kernels/base.py
@@ -1,3 +1,16 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the PyMVPA package for the
+#   copyright and license terms.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+"""Base Kernel classes
+
+"""
+
+__docformat__ = 'restructuredtext'
 
 import numpy as N
 
@@ -10,32 +23,36 @@ import numpy as N
 
 class Kernel(object):
     """Abstract class which calculates a kernel function between datasets
-    
+
     Each instance has an internal representation self._k which might be of
     a different form depending on the intended use.  Some kernel types should
-    be translatable to other representations where possible, e.g., between 
+    be translatable to other representations where possible, e.g., between
     Numpy and Shogun-based kernels.
-    
+
     This class should not be used directly, but rather use a subclass which
     enforces a consistent internal representation.
     """
 
     def __init__(self):
         self._k = None
+        """Implementation specific version of the kernel"""
 
     def compute(self, ds1, ds2=None):
         raise NotImplemented, "Abstract method"
 
-    # NB: __array__ is circular with as_np.  Any subclass must override either
-    # one of these and the other will work
     def __array__(self):
         return self.as_np()._k
-    
+
     def as_np(self):
         """Converts this kernel to a Numpy-based representation"""
         return StaticKernel(N.array(self))
 
     def cleanup(self):
+        """Wipe out internal representation
+
+        XXX unify: we have reset in other places to accomplish similar
+        thing
+        """
         self._k = None
 
 
@@ -44,11 +61,11 @@ class NumpyKernel(Kernel):
     # Conversions
     def __init__(self):
         Kernel.__init__(self)
-        
+
     def __array__(self):
         # By definintion, a NumpyKernel's internal representation is an array
         return self._k
-    
+
     def as_np(self):
         # Already numpy!!
         return self
@@ -59,12 +76,13 @@ class CustomKernel(NumpyKernel):
     def __init__(self, kernelfunc):
         NumpyKernel.__init__(self)
         self._kf = kernelfunc
-        
+
     def compute(self, d1,d2=None):
         if d2 is None:
             d2=d1
         self._k = self._kf(d1, d2)
-        
+
+
 class LinearKernel(CustomKernel):
     def __init__(self):
         CustomKernel.__init__(self, self._compute)
@@ -73,10 +91,14 @@ class LinearKernel(CustomKernel):
         if d2 is None:
             d2=d1
         return N.dot(d1.samples, d2.samples.T)
-            
-class StaticKernel(NumpyKernel):
 
+
+class StaticKernel(NumpyKernel):
+    """Precomputed matrix
+    """
     def __init__(self, matrix):
+        """Initialize StaticKernel
+        """
         super(StaticKernel, self).__init__()
         self._k = N.array(matrix)
 
@@ -85,8 +107,17 @@ class StaticKernel(NumpyKernel):
 
 
 class CachedKernel(NumpyKernel):
+    """Kernel decorator to cache all data to avoid duplicate computation
+    """
 
     def __init__(self, kernel):
+        """Initialize CachedKernel
+
+        Parameters
+        ----------
+          kernel : Kernel
+            Base kernel to cache
+        """
         super(CachedKernel, self).__init__()
         self._ckernel = kernel
         self._ds_cached_info = None
@@ -124,20 +155,20 @@ class CachedKernel(NumpyKernel):
             try:
                 lhsids = self._lhsids(ds1)
                 if ds2 is None:
-                    rhids = lhsids
+                    rhsids = lhsids
                 else:
                     rhsids = self._rhsids(ds2)
                 self._k = self._kfull.take(
                     lhsids, axis=0).take(
                     rhsids, axis=1)
-            except:
+            except KeyError:
                 self._init(ds1, ds2)
 
 """
 if ds1 is the "derived" dataset as it was computed on:
     * ds2 is None
       ds2 bound to ds1
-      - 
+      -
     * ds1 and ds2 present
       - ds1 and ds2 come from the same dataset
         - whatever CachedKernel was computed on is a superset
@@ -148,4 +179,3 @@ else:
     compute (ds1, ds2)
       - different data ids
 """
-
