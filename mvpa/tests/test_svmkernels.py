549f025a10e2968fbccd67e9882dd89d303cdc1d
Scott Gorlin <gorlins@mit.edu>
2009-12-03 18:06:05 -0500

    NF+RF: Beginning switch of SVMs to Kernel interface
    Now libsvm files will run (ie no syntax errors in the SVM
    class definitions!), nothing yet useable
    
    Blank unittest file for future test_svmkernels

---
+++ mvpa/tests/test_svmkernels.py
@@ -0,0 +1,28 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the PyMVPA package for the
+#   copyright and license terms.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+"""Unit tests for new Kernel-based SVMs"""
+
+import unittest
+import numpy as N
+from mvpa.datasets import Dataset
+from mvpa.clfs.libsvmc import SVM as lsSVM
+from mvpa.clfs.sg import SVM as sgSVM
+
+from tests_warehouse import datasets
+
+class SVMKernelTests(unittest.TestCase):
+    pass
+
+def suite():
+    return unittest.makeSuite(KernelTests)
+
+
+if __name__ == '__main__':
+    import runner
+
