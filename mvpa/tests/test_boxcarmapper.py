2ac94387a23476244eb8f64764cf601249cfbfae
Michael Hanke <michael.hanke@gmail.com>
2009-12-18 17:29:17 -0500

    BF: BoxcarMapper (first pass, and unittest run too)...
    However, a lot of work needs to be done for attribute mapping.

--- mvpa/tests/test_boxcarmapper.py
+++ mvpa/tests/test_boxcarmapper.py
@@ -9,110 +9,84 @@
 """Unit tests for PyMVPA Boxcar mapper"""
 
 
-import unittest
-from mvpa.support.copy import deepcopy
 import numpy as N
+from numpy.testing import assert_array_equal
+from nose.tools import ok_, assert_raises, assert_false, assert_equal, \
+        assert_true
 
 from mvpa.mappers.boxcar import BoxcarMapper
 from mvpa.datasets import Dataset
 
 
-class BoxcarMapperTests(unittest.TestCase):
+def test_simpleboxcar():
+    """Just the same tests as for transformWithBoxcar.
 
-    def testSimple(self):
-        """Just the same tests as for transformWithBoxcar.
+    Mention that BoxcarMapper doesn't apply function with each boxcar
+    """
+    data = N.atleast_2d(N.arange(10)).T
+    sp = N.arange(10)
 
-        Mention that BoxcarMapper doesn't apply function with each boxcar
-        """
-        data = N.arange(10)
-        sp = N.arange(10)
+    # check if stupid thing don't work
+    assert_raises(ValueError, BoxcarMapper, sp, 0)
 
-        # check if stupid thing don't work
-        self.failUnlessRaises(ValueError,
-                              BoxcarMapper,
-                              sp,
-                              0 )
+    # now do an identity transformation
+    bcm = BoxcarMapper(sp, 1)
+    
+    trans = bcm.forward(data)
+    # ,0 is a feature below, so we get explicit 2D out of 1D
+    assert_array_equal(trans[:,0], data)
 
-        # now do an identity transformation
-        bcm = BoxcarMapper(sp, 1)
-        trans = bcm(data)
-        # ,0 is a feature below, so we get explicit 2D out of 1D
-        self.failUnless( (trans[:,0] == data).all() )
+    # now check for illegal boxes
+    assert_raises(ValueError, BoxcarMapper(sp, 2).train, data)
 
-        # now check for illegal boxes
-        self.failUnlessRaises(ValueError,
-                              BoxcarMapper(sp, 2),
-                              data)
-
-        # now something that should work
-        sp = N.arange(9)
-        bcm = BoxcarMapper(sp,2)
-        trans = bcm(data)
-        self.failUnless( (trans == N.vstack((N.arange(9),
-                                             N.arange(9)+1)).T ).all() )
+    # now something that should work
+    nbox = 9
+    boxlength = 2
+    sp = N.arange(nbox)
+    bcm = BoxcarMapper(sp, boxlength)
+    trans = bcm(data)
+    # check that is properly upcasts the dimensionality
+    assert_equal(trans.shape, (nbox, boxlength) + data.shape[1:])
+    # check actual values, squeezing the last dim for simplicity
+    assert_array_equal(trans.squeeze(), N.vstack((N.arange(9), N.arange(9)+1)).T)
 
 
-        # now test for proper data shape
-        data = N.ones((10,3,4,2))
-        sp = [ 2, 4, 3, 5 ]
-        trans = BoxcarMapper(sp, 4)(data)
-        self.failUnless( trans.shape == (4,4,3,4,2) )
+    # now test for proper data shape
+    data = N.ones((10,3,4,2))
+    sp = [ 2, 4, 3, 5 ]
+    trans = BoxcarMapper(sp, 4)(data)
+    assert_equal(trans.shape, (4,4,3,4,2))
 
-        # test reverse
-        data = N.arange(240).reshape(10, 3, 4, 2)
-        sp = [ 2, 4, 3, 5 ]
-        m = BoxcarMapper(sp, 2)
-        mp = m.forward(data)
-        self.failUnless(mp.shape == (4, 2, 3, 4, 2))
+    # test reverse
+    data = N.arange(240).reshape(10, 3, 4, 2)
+    sp = [ 2, 4, 3, 5 ]
+    boxlength = 2
+    m = BoxcarMapper(sp, boxlength)
+    m.train(data)
+    mp = m.forward(data)
+    assert_equal(mp.shape, (4, 2, 3, 4, 2))
 
-        # try full reconstruct
-        mr = m.reverse(mp)
-        # shape has to match
-        self.failUnless(mr.shape == data.shape)
-        # first two samples where not in any of the boxcars and cannot be
-        # reconstructed
-        self.failUnless(N.sum(mr[:2]) == 0)
-        # same for the last ones
-        self.failUnless(N.sum(mr[7:]) == 0)
+    # try full reconstruct
+    mr = m.reverse(mp)
+    # shape has to match
+    assert_equal(mr.shape, (len(sp) * boxlength,) + data.shape[1:])
+    # only known samples are part of the results
+    assert_true((mr >= 24).all())
+    assert_true((mr < 168).all())
 
-        # check proper reconstruction of non-conflicting sample
-        self.failUnless((mr[2].ravel() == N.arange(48, 72)).all())
+    # check proper reconstruction of non-conflicting sample
+    assert_array_equal(mr[0].ravel(), N.arange(48, 72))
 
-        # check proper reconstruction of samples being part of multiple
-        # mapped samples
-        self.failUnless((mr[3].ravel() == N.arange(72, 96)).all())
+    # check proper reconstruction of samples being part of multiple
+    # mapped samples
+    assert_array_equal(mr[1].ravel(), N.arange(72, 96))
 
-        # test reverse of a single sample
-        singlesample = N.arange(48).reshape(2, 3, 4, 2)
-        self.failUnless((singlesample == m.reverse(singlesample)).all())
-        # should not work for shape mismatch
-        self.failUnlessRaises(ValueError, m.reverse, singlesample[0])
+    # test reverse of a single sample
+    singlesample = N.arange(48).reshape(2, 3, 4, 2)
+    assert_array_equal(singlesample, m.reverse1(singlesample))
+    # should not work for shape mismatch
+    assert_raises(ValueError, m.reverse, singlesample[0])
 
-        # check broadcasting of 'raw' samples into proper boxcars on forward()
-        bc = m.forward(N.arange(24).reshape(3, 4, 2))
-        self.failUnless((bc ==
-                         N.array(2 * [N.arange(24).reshape(3, 4, 2)])).all())
-
-
-    def testIds(self):
-        data = N.arange(20).reshape( (10,2) )
-        bcm = BoxcarMapper([1, 4, 6], 3)
-        trans = bcm(data)
-
-        self.failUnlessEqual(bcm.is_valid_inid( [1] ), True)
-        self.failUnlessEqual(bcm.is_valid_inid( [0,1] ), False)
-
-        self.failUnlessEqual(bcm.is_valid_outid( [1] ), True)
-        self.failUnlessEqual(bcm.is_valid_outid( [3] ), False)
-        self.failUnlessEqual(bcm.is_valid_outid( [0,1] ), True)
-        self.failUnlessEqual(bcm.is_valid_outid( [0,1,0] ), False)
-
-
-
-def suite():
-    return unittest.makeSuite(BoxcarMapperTests)
-
-
-if __name__ == '__main__':
-    import runner
-
+    # check broadcasting of 'raw' samples into proper boxcars on forward()
+    bc = m.forward(N.arange(24).reshape(3, 4, 2))
+    assert_array_equal(bc, N.array(2 * [N.arange(24).reshape(3, 4, 2)]))
