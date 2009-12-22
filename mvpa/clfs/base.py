8a78396a503dcfea748dfcba4d367b8692b5aac3
Yaroslav Halchenko <debian@onerussian.com>
2009-12-22 15:10:36 -0500

    NF: exception to throw whenever data is bogus or there is problem with training of clf

--- mvpa/clfs/base.py
+++ mvpa/clfs/base.py
@@ -32,6 +32,15 @@ from mvpa.base import warning
 if __debug__:
     from mvpa.base import debug
 
+class DegenerateInputError(Exception):
+    """Exception to be thrown by learners if input data is bogus, i.e. no
+    features or samples"""
+    pass
+
+class FailedToTrainError(Exception):
+    """Exception to be thrown whenever classifier fails to learn for
+    some reason"""
+    pass
 
 class Classifier(ClassWithCollections):
     """Abstract classifier class to be inherited by all classifiers
@@ -355,6 +364,9 @@ class Classifier(ClassWithCollections):
         Shouldn't be overridden in subclasses unless explicitly needed
         to do so
         """
+        if dataset.nfeatures == 0 or dataset.nsamples == 0:
+            raise DegenerateInputError, \
+                  "Cannot train classifier on degenerate data %s" % dataset
         if __debug__:
             debug("CLF", "Training classifier %(clf)s on dataset %(dataset)s",
                   msgargs={'clf':self, 'dataset':dataset})
