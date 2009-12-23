513882a508cf21a4c2cf891f4e7bd2e3ae0cfb69
Michael Hanke <michael.hanke@gmail.com>
2009-12-23 13:03:04 -0500

    RF: Remoeve obsolete EventDataset

--- mvpa/suite.py
+++ mvpa/suite.py
@@ -63,7 +63,6 @@ from mvpa.datasets import *
 from mvpa.datasets.base import *
 from mvpa.datasets.miscfx import *
 from mvpa.datasets.channel import *
-from mvpa.datasets.event import *
 from mvpa.datasets.eep import *
 if externals.exists('nifti'):
     from mvpa.datasets.mri import *
