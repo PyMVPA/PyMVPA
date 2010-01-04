fa3b71f393c05cc07d92c961f81a76f542ca0179
Michael Hanke <michael.hanke@gmail.com>
2010-01-04 17:30:35 -0500

    NF: hstack and vstack become important (IMHO).

--- mvpa/datasets/__init__.py
+++ mvpa/datasets/__init__.py
@@ -38,6 +38,7 @@ if __debug__:
 
 # nothing in here that works without the base class
 from mvpa.datasets.base import Dataset, dataset
+from mvpa.base.dataset import hstack, vstack
 
 if __debug__:
     debug('INIT', 'mvpa.datasets end')
