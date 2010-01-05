6396163b3a2370cea176c074c6533e30585fd6e1
Yaroslav Halchenko <debian@onerussian.com>
2010-01-05 17:15:49 -0500

    DOC: facilift for model_selector

--- mvpa/clfs/model_selector.py
+++ mvpa/clfs/model_selector.py
@@ -66,32 +66,29 @@ class ModelSelector(object):
         the log_marginal_likelihood.
 
         :Parameters:
-
           parametric_model : Classifier
             the actual parameteric model to be optimized.
-
           hyp_initial_guess : numpy.ndarray
             set of hyperparameters' initial values where to start
             optimization.
-
           optimization_algorithm : string
             actual name of the optimization algorithm. See
             http://scipy.org/scipy/scikits/wiki/NLP
             for a comprehensive/updated list of available NLP solvers.
             (Defaults to 'ralg')
-
           ftol : float
             threshold for the stopping criterion of the solver,
             which is mapped in OpenOpt NLP.ftol
             (Defaults to 1.0e-3)
-
           fixedHypers : numpy.ndarray (boolean array)
             boolean vector of the same size of hyp_initial_guess;
             'False' means that the corresponding hyperparameter must
             be kept fixed (so not optimized).
             (Defaults to None, which during means all True)
 
-        NOTE: the maximization of log_marginal_likelihood is a non-linear
+        Notes
+        -----
+        The maximization of log_marginal_likelihood is a non-linear
         optimization problem (NLP). This fact is confirmed by Dmitrey,
         author of OpenOpt.
         """
