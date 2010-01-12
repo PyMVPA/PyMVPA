ddf2da347537fe842bce1d3b0a191aa96feb6de0
Michael Hanke <michael.hanke@gmail.com>
2010-01-11 22:48:56 -0500

    DOC: Ported Rajeev's demo to Python; enabled pngmath extension.
    Played a bit with the math extension -- probably a little too much. We
    could now extend this demo by showing how a simple regression-based
    classifier in PyMVPA would do the same, and plot the results on top of
    the current plot.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -41,6 +41,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.coverage',
               'sphinx.ext.ifconfig',
               'sphinx.ext.inheritance_diagram',
+              'sphinx.ext.pngmath',
               # we have a local copy of autosummary from the unreleased sphinx
               # 1.0 -- reason: the 0.6 extension creates half-empty summaries
               'autosummary',
