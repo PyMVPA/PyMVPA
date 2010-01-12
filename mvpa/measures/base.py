c49ac82b3acd2b92ce2681b2575a195bf68bbd93
Yaroslav Halchenko <debian@onerussian.com>
2010-01-12 11:50:40 -0500

    NF: provide labels tuple for RegressionAsClassifierSA

--- mvpa/measures/base.py
+++ mvpa/measures/base.py
@@ -789,9 +789,11 @@ class RegressionAsClassifierSensitivityAnalyzer(ProxyClassifierSensitivityAnalyz
     def _call(self, dataset):
         sens = super(RegressionAsClassifierSensitivityAnalyzer,
                      self)._call(dataset)
-        if 'labels' in sens.sa:
-            # TODO : handle tuple labels case
-            sens.labels[:] = [self._trained_attrmap[l] for l in sens.sa.labels]
+        # We can have only a single sensitivity out of regression
+        assert(sens.shape[0] == 1)
+        if 'labels' not in sens.sa:
+            # We just assign a tuple of all labels sorted
+            sens.sa['labels'] = [tuple(sorted(self.clf._trained_attrmap.values()))]
         return sens
 
 
