a6b975a2d01992f9b9647d547cdcdd9205b5a8b6
Michael Hanke <michael.hanke@gmail.com>
2010-01-12 17:24:51 -0500

    NF: Store datasets in HDF5 files.
    This is all preliminary and does not support storage of arbitrary
    dataset attributes yet. But it should work find for datasets with just
    samples and feature attributes.
    
    Do not store valuable stuff yet ;-)

--- mvpa/base/dataset.py
+++ mvpa/base/dataset.py
@@ -13,6 +13,7 @@ __docformat__ = 'restructuredtext'
 import numpy as N
 import copy
 
+from mvpa.base import externals
 from mvpa.base.collections import SampleAttributesCollection, \
         FeatureAttributesCollection, DatasetAttributesCollection, \
         SampleAttribute, FeatureAttribute, DatasetAttribute
@@ -21,6 +22,8 @@ from mvpa.base.dochelpers import _str
 
 if __debug__:
     from mvpa.base import debug
+from mvpa.base import warning
+
 
 
 class AttrDataset(object):
@@ -543,6 +546,35 @@ class AttrDataset(object):
     def __len__(self):
         return self.shape[0]
 
+
+    @classmethod
+    def from_hdf5(cls, source, name='dataset'):
+        if not externals.exists('h5py'):
+            raise RuntimeError("Missing 'h5py' package -- saving is not possible.")
+
+        import h5py
+
+        # look if we got an hdf file instance already
+        if isinstance(source, h5py.highlevel.File):
+            own_file = False
+            hdf = source
+        else:
+            own_file = True
+            hdf = h5py.File(source, 'r')
+
+        if not name in hdf:
+            raise ValueError("Cannot find '%s' group in HDF file."
+                             % name)
+
+        # acces the group that should contain the dataset
+        dsgrp = hdf[name]
+        samples = dsgrp['samples']
+        sa = _hdf2col(dsgrp, 'sa')
+        fa = _hdf2col(dsgrp, 'fa')
+        ds = cls(samples, sa=sa, fa=fa)
+        return ds
+
+
     # shortcut properties
     nsamples = property(fget=lambda self:len(self))
     nfeatures = property(fget=lambda self:self.shape[1])
@@ -737,3 +769,96 @@ class DatasetAttributeExtractor(object):
 # attribute extraction
 DAE = DatasetAttributeExtractor
 
+
+@datasetmethod
+def save(dataset, to, name='dataset', compression=None):
+    if not externals.exists('h5py'):
+        raise RuntimeError("Missing 'h5py' package -- saving is not possible.")
+
+    import h5py
+
+    # look if we got an hdf file instance already
+    if isinstance(to, h5py.highlevel.File):
+        own_file = False
+        hdf = to
+    else:
+        own_file = True
+        hdf = h5py.File(to, 'w')
+
+    # first create a group of the desired name for this dataset
+    # this call will fail in case there is already a group of that name
+    dsgrp = hdf.create_group(name)
+
+    # local binding
+    samples = dataset.samples
+
+    # store the samples in an hdf-dataset
+    dsgrp.create_dataset('samples', None, None, samples,
+                         compression=compression)
+
+    # store samples attributes collection
+    _col2hdf(dsgrp, dataset.sa, 'sa', compression=compression)
+    # store feature attributes collection
+    _col2hdf(dsgrp, dataset.fa, 'fa', compression=compression)
+    # store dataset attributes collection (this is special)
+    # TODO
+
+    # if we opened the file ourselves we close it now
+    if own_file:
+        hdf.close()
+
+
+def _col2hdf(hdf, col, name, **kwargs):
+    """Helper to store a collection into an HDF5 group.
+
+    Parameters
+    ----------
+    hdf : HDF group
+      This group will contain a new group with the content of the collection.
+    col : Collection
+      Collection to store
+    name : str
+      Name of the HDF group to hold the collection content
+    **kwargs
+      All additional arguments are pass to the `create_dataset()` call for each
+      collection attribute.
+    """
+    # nothing to do if no elements
+    if not len(col):
+        return
+
+    colgrp = hdf.create_group(name)
+    not_stored = []
+    for attr in col:
+        try:
+            colgrp.create_dataset(attr, None, None, col[attr].value, **kwargs)
+        except TypeError:
+            not_stored.append(attr)
+    if len(not_stored):
+        warning("Could not store all attributes into HDF file '%s' -- "
+                "probably unsupported dtype (not stored: %s)"
+                % (to, not_stored))
+
+
+def _hdf2col(hdf, name):
+    """Helper to load a collection from an HDF5 group.
+
+    Parameters
+    ----------
+    hdf : HDF group
+      This group will contain a new group with the content of the collection.
+    name : str
+      Name of the HDF group to hold the collection content
+
+    Returns
+    -------
+    dict
+      All attributes of the collection as (name: value) pairs.
+    """
+    if not name in hdf:
+        raise ValueError("Cannot find '%s' group in HDF file."
+                         % name)
+
+    colgrp = hdf[name]
+    col = dict([(attr, value) for attr, value in colgrp.iteritems()])
+    return col
