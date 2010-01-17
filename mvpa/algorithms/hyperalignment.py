f8a3836bbb607ee657bae45360147d82f6557a0f
Swaroop Guntupalli <swaroopgj@gmail.com>
2010-01-17 15:33:43 -0500

    RF: working on Hyperalignment
    * renaming of variables/parameters/etc
    * commenting out zscoring and removal of subject-specifics
    * placeholder for Swaroop's test

--- mvpa/algorithms/hyperalignment.py
+++ mvpa/algorithms/hyperalignment.py
@@ -18,6 +18,8 @@ __docformat__ = 'restructuredtext'
 
 from mvpa.support.copy import deepcopy
 
+import numpy as N
+
 from mvpa.base import warning
 from mvpa.misc.state import StateVariable, ClassWithCollections
 from mvpa.misc.param import Parameter
@@ -41,93 +43,94 @@ class Hyperalignment(ClassWithCollections):
 
     # Lets use built-in facilities to specify parameters which
     # constructor should accept
-    alignment = Parameter(ProcrusteanMapper(), # might provide allowedtype later on
+    alignment = Parameter(ProcrusteanMapper(), # might provide allowedtype
             doc="""... XXX If `None` (default) an instance of
             :class:`~mvpa.mappers.procrustean.ProcrusteanMapper` is
             used.""")
 
-    levels = Parameter(3, allowedtype='int', min=1,
+    levels = Parameter(3, allowedtype='int', min=2,
             doc="Number of levels ....XXX ")
 
-    combiner1 = Parameter('mean', #
-            doc="XXX ")
+    ref_ds = Parameter(None, allowedtype='int', min=0,
+            doc="""Index of a dataset to use as a reference.  If `None`, then
+            dataset with maximal number of features is used.""")
 
-    combiner2 = Parameter('mean', #
-            doc="XXX ")
+    combiner1 = Parameter(lambda x,y: 0.5*(x+y), #
+            doc="How to update common space in the 1st loop")
 
+    combiner2 = Parameter(lambda l: N.mean(l, axis=0),
+            doc="How to combine all individual spaces to common space.")
 
-    def __init__(self,
-                 alignment=ProcrusteanMapper(),
-                 levels=3,
-                 combiner1='mean',
-                 combiner2='mean',
-				 ref_subj=0,
-                 **kwargs):
-
+    def __init__(self, **kwargs):
         ClassWithCollections.__init__(self, **kwargs)
 
-    
-    def __call__(self, data):
-        """Estimate mappers for each data(set)
+
+    def __call__(self, datasets):
+        """Estimate mappers for each dataset
 
         Parameters
         ----------
-          data : list or tuple of dataset of data
-            XXX
+          datasets : list or tuple of datasets
 
         Returns
         -------
-        A list of trained Mappers ... of length equal to len(data)
+        A list of trained Mappers of the same length as datasets
         """
         params = self.params            # for quicker access ;)
-        nelements = len(data)
-        nfeatures = [data[i].shape[1] for i in xrange(nelements)]
-		
-        if min(vox_size) == max(vox_size):
-            if ref_subj < 0 or ref_subj >= nelements:
-                ref_subj = 0
+        nelements = len(datasets)
+        nfeatures = [datasets[i].shape[1] for i in xrange(nelements)]
+
+        if params.ref_ds is None:
+            ref_ds = N.argmax(nfeatures)
         else:
-            nf = max(nfeatures)
-            ref_subj = nfeatures.index(nf) 
-		
+            ref_ds = params.ref_ds
+            if ref_ds < 0 and ref_ds >= nelements:
+                raise ValueError, "Requested reference dataset %i is out of " \
+                      "bounds. We have only %i datasets provided" \
+                      % (ref_ds, nelements)
+
         # might prefer some other way to initialize... later
-        result = [deepcopy(params.alignment) for i in xrange(nelements)]
+        mappers = [deepcopy(params.alignment) for ds in datasets]
         # zscore all data sets
-        ds = [ zscore(ds[i], perchunk=False) for i in xrange(nelements)]
-        # Level 1
-        commonspace = data[ref_subj]
-        ds_mapped = []
-        for m, d, i in zip(mappers[0:], data[0:], xrange(nelements)):
-            if i!=ref_subj:
-				# XXX For now lets just call this way:
-            	m.train(d, commonspace)
-            	if ds_mapped == []:
-            	    ds_mapped = zscore( m.forward(d), perchunk=False)
-            	else:
-            	    ds_mapped += zscore( m.forward(d), perchunk=False)
-            	commonspace = mean( ds_mapped[i], commonspace)   # zscore before adding
-            	# here yarik stopped ;)
+        # ds = [ zscore(ds, perchunk=False) for ds in datasets]
+
+        # Level 1 (first)
+        commonspace = N.asanyarray(datasets[ref_ds])
+        data_mapped = [N.asanyarray(ds) for ds in datasets]
+        for i, (m, data) in enumerate(zip(mappers, data_mapped)):
+            if i == ref_ds:
+                continue
+
+            # XXX For now lets just call this way:
+            m.train(data, commonspace)
+            data_mapped[i] = m.forward(data)
+
+            ## if ds_mapped == []:
+            ##     ds_mapped = [zscore(m.forward(d), perchunk=False)]
+            ## else:
+            ##     ds_mapped += [zscore(m.forward(d), perchunk=False)]
+
+            # zscore before adding
+            # TODO: make just a function so we dont' waste space
+            commonspace = params.combiner1(data_mapped[i], commonspace)
 
         # update commonspace to mean of ds_mapped
-        # XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-        commonspace = mean(ds_mapped)
-        # Level 2 to params.levels to derive common space
-        for m, d, i in zip(mappers[0:], data[0:], xrange(nelements)):
-            ds_temp = zscore( (commonspace*nelements - ds_mapped[i])/(nelements-1), perchunk=False )
-            m.train(d, ds_temp)
-            ds_mapped[i] = zscore( m.forward(ds_temp), perchunk=False)
+        commonspace = params.combiner2(data_mapped)
 
-        # update commonspace to mean of ds_mapped
-        # XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+        # Level 2 -- might iterate multiple times
+        for loop in xrange(params.levels - 2):
+            for i, (m, ds) in enumerate(zip(mappers, datasets)):
+                ## ds_temp = zscore( (commonspace*nelements - ds_mapped[i])/(nelements-1), perchunk=False )
+                m.train(ds, commonspace) # ds_temp)
+                data_mapped[i] = m.forward(N.asanyarray(ds))
+                #ds_mapped[i] = zscore( m.forward(ds_temp), perchunk=False)
 
-        commonspace = mean(ds_mapped)
-        # Level 3 to params.levels
-        for m, d, i in zip(mappers[0:], data[0:], xrange(nelements)):
-            ds_temp = zscore( (commonspace*nelements - ds_mapped[i])/(nelements-1), perchunk=False )
-            m.train(d, ds_temp )
+            commonspace = params.combiner2(data_mapped)
 
-        # might prefer some other way to initialize... later
-        result = [deepcopy(mappers[i]) for i in xrange(nelements)]
-        
-        return result
+        # Level 3 (last) to params.levels
+        for i, (m, ds) in enumerate(zip(mappers, datasets)):
+            ## ds_temp = zscore( (commonspace*nelements - ds_mapped[i])/(nelements-1), perchunk=False )
+            m.train(ds, commonspace) #ds_temp)
+
+        return mappers
 
