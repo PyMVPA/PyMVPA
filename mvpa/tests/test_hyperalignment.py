3f43297b74be28389cc2c69ada11d251a4a9e845
Yaroslav Halchenko <debian@onerussian.com>
2010-01-20 14:44:38 -0500

    BR NF/RF: refactored hyperalignment a bit, added unittest for it (fails)
    unittest fails on test either we are succesfull at reconstructing
    original dataset (unrotated)
    because that was wrong assumption!!! need to rotate back using that
    reference dataset rotation! ... will be next commit I guess ;)

--- mvpa/tests/test_hyperalignment.py
+++ mvpa/tests/test_hyperalignment.py
@@ -8,11 +8,14 @@
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 """Unit tests for PyMVPA ..."""
 
+import unittest
+import numpy as N
+
 # See other tests and test_procrust.py for some example on what to do ;)
 from mvpa.algorithms.hyperalignment import Hyperalignment
 
 # Somewhat slow but provides all needed ;)
-from tests_warehouse import *
+from tests_warehouse import datasets, get_random_rotation
 
 # if you need some classifiers
 #from tests_warehouse_clfs import *
@@ -21,11 +24,34 @@ class HyperAlignmentTests(unittest.TestCase):
 
 
     def testBasicFunctioning(self):
-        # TODO
-        pass
+        # get a dataset with some prominent trends in it
+        ds_orig = datasets['uni4large']
+        # lets select for now only meaningful features
+        ds_orig = ds_orig[:, ds_orig.a.nonbogus_features]
+        n = 5 # # of datasets to generate
+        Rs, dss_rotated = [], []
+        # now lets compose derived datasets by using some random
+        # rotation(s)
+        for _ in xrange(n):
+            R = get_random_rotation(ds_orig.nfeatures)
+            Rs.append(R)
+            ds_ = ds_orig.copy()
+            ds_.samples = N.dot(ds_orig.samples, R)
+            dss_rotated.append(ds_)
 
-    def testPossibleInputs(self):
-        # get a dataset with a very high SNR
+        ha = Hyperalignment()
+        mappers = ha(dss_rotated)
+        # Map data back
+        dss_back = [m.forward(ds_) for m, ds_ in zip(mappers, dss_rotated)]
+        ds_orig_norm = N.linalg.norm(ds_orig.samples)
+        nddss = []
+        for ds_back in dss_back:
+            dds = ds_back.samples - ds_orig.samples
+            ndds = N.linalg.norm(dds) / ds_orig_norm
+            nddss += [ndds]
+        self.failUnless(N.all(ndds <= 1e-5),
+            msg="Should have reconstructed original dataset more or less."
+                " Got normed differences %s" % nddss)
         pass
 
 
