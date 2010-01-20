4cad9b12b568611c037ee6fea207614d6ea6ec29
Yaroslav Halchenko <debian@onerussian.com>
2010-01-20 16:01:14 -0500

    blind pass over _testOnSwaroopData - making it proper Python code + use list comprehensions correctly

--- mvpa/tests/test_hyperalignment.py
+++ mvpa/tests/test_hyperalignment.py
@@ -96,10 +96,16 @@ class HyperAlignmentTests(unittest.TestCase):
     def _testOnSwaroopData(self):
         #
         print "Running swaroops test on data we don't have"
+        #from mvpa.datasets.miscfx import zscore
+        #from mvpa.featsel.helpers import FixedNElementTailSelector
+        #from mvpa.algorithms.cvtranserror import CrossValidatedTransferError
+        #   or just for lazy ones like yarik atm
+        from mvpa.suite import *
         subj = ['cb', 'dm', 'hj', 'kd', 'kl', 'mh', 'ph', 'rb', 'se', 'sm']
         ds = []
-        for sub in subj: 
-            ds.append(fmri_dataset(samples=sub+'_movie.nii.gz',mask=sub+'_mask_vt.nii.gz'))
+        for sub in subj:
+            ds.append(fmri_dataset(samples=sub+'_movie.nii.gz',
+                                   mask=sub+'_mask_vt.nii.gz'))
 
         '''
         Compute feature ranks in each dataset
@@ -121,48 +127,56 @@ class HyperAlignmentTests(unittest.TestCase):
             ds_temp = sd.copy()
             zscore(ds_temp, perchunk=False)
             for j, sd2 in enumerate(ds[i+1:]):
-            ds_temp2 = sd2.copy()
-            zscore(ds_temp2, perchunk=False)
-            corr_temp= N.asarray(N.mat(N.transpose(ds_temp.samples))*N.mat(ds_temp2.samples))
-            feature_scores[i] = feature_scores[i] + N.max(corr_temp, axis = 1)
-            feature_scores[j+i+1] = feature_scores[j+i+1] + N.max(corr_temp, axis = 0)
+                ds_temp2 = sd2.copy()
+                zscore(ds_temp2, perchunk=False)
+                corr_temp = N.dot(ds_temp.samples.T, ds_temp2.samples)
+                feature_scores[i] = feature_scores[i] + \
+                                    N.max(corr_temp, axis = 1)
+                feature_scores[j+i+1] = feature_scores[j+i+1] + \
+                                        N.max(corr_temp, axis = 0)
 
         for i, sd in enumerate(ds):
             sd.fa['bsc_scores'] = feature_scores[i]
 
-        fselector = FixedNElementTailSelector(2000, tail='upper', mode='select')
+        fselector = FixedNElementTailSelector(2000,
+                                              tail='upper', mode='select')
 
         ds_fs = [ sd[:, fselector(sd.fa.bsc_scores)] for sd in ds]
 
         hyper = Hyperalignment()
         mapper_results = hyper(datasets=ds_fs)
 
-        md_cd = ColumnData('labels.txt',header=['label'])
+        md_cd = ColumnData('labels.txt', header=['label'])
         md_labels = [int(x) for x in md_cd['label']]
         for run in range(8):
             md_labels[192*run:192*run+3] = [-1]*3
 
         mkdg_ds = []
         for sub in subj:
-            mkdg_ds.append(fmri_dataset(samples=sub+'_mkdg.nii.gz',labels=md_labels, chunks=N.repeat(range(8),192) , mask=sub+'_mask_vt.nii.gz'))
+            mkdg_ds.append(fmri_dataset(
+                samples=sub+'_mkdg.nii.gz', labels=md_labels,
+                chunks=N.repeat(range(8), 192), mask=sub+'_mask_vt.nii.gz'))
 
         m=mean_group_sample(['labels', 'chunks'])
 
-        mkdg_ds = [mkdg_ds[i].get_mapped(m) for i in range(len(mkdg_ds))]
-        mkdg_ds = [mkdg_ds[i][mkdg_ds[i].sa.labels != -1] for i in range(len(mkdg_ds))]
-        [mkdg_ds[i].zscore(baselinelabels=0) for i in range(len(mkdg_ds))]
-        mkdg_ds = [mkdg_ds[i][mkdg_ds[i].sa.labels != 0] for i in range(len(mkdg_ds))]
+        mkdg_ds = [ds_.get_mapped(m) for ds_ in mkdg_ds]
+        mkdg_ds = [ds_[ds_.sa.labels != -1] for ds_ in mkdg_ds]
+        [ds_.zscore(baselinelabels=0) for ds_ in mkdg_ds]
+        mkdg_ds = [ds_[ds_.sa.labels != 0] for ds_ in mkdg_ds]
 
         for i, sd in enumerate(mkdg_ds):
             sd.fa['bsc_scores'] = feature_scores[i]
 
         mkdg_ds_fs = [ sd[:, fselector(sd.fa.bsc_scores)] for sd in mkdg_ds]
-        mkdg_ds_mapped = [ sd.get_mapped(mapper_results[i]) for i, sd in enumerate(mkdg_ds_fs)]
+        mkdg_ds_mapped = [ sd.get_mapped(mapper_results[i])
+                           for i, sd in enumerate(mkdg_ds_fs)]
 
         # within-subject classification
         within_acc = []
-        clf = clfswh['multiclass','linear','NU_SVC'][0]
-        cvterr = CrossValidatedTransferError(TransferError(clf), splitter=NFoldSplitter(), enable_states=['confusion'])
+        clf = clfswh['multiclass', 'linear', 'NU_SVC'][0]
+        cvterr = CrossValidatedTransferError(
+            TransferError(clf),
+            splitter=NFoldSplitter(), enable_states=['confusion'])
         for sd in mkdg_ds_fs:
             wsc = cvterr(sd)
             within_acc.append(1-N.mean(wsc))
@@ -176,13 +190,13 @@ class HyperAlignmentTests(unittest.TestCase):
         print N.mean(within_acc_mapped)
 
         mkdg_ds_all = vstack(mkdg_ds_mapped)
-        mkdg_ds_all.sa['subject'] = N.repeat(range(10),56)
+        mkdg_ds_all.sa['subject'] = N.repeat(range(10), 56)
         mkdg_ds_all.sa['chunks'] = mkdg_ds_all.sa['subject']
 
         bsc = cvterr(mkdg_ds_all)
         print 1-N.mean(bsc)
         mkdg_all = vstack(mkdg_ds_fs)
-        mkdg_all.sa['chunks'] = N.repeat(range(10),56)
+        mkdg_all.sa['chunks'] = N.repeat(range(10), 56)
         bsc_orig = cvterr(mkdg_all)
         print 1-N.mean(bsc_orig)
         pass
