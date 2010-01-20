b539f76e0e14d76b4a80847af286d7b3a9396e69
Yaroslav Halchenko <debian@onerussian.com>
2010-01-20 15:13:22 -0500

    NF: elaborated Hyperalignment test
    - data gets random offset and scale
    - testing on both noiseless and noisy data.  On noisy allow large
      mismatch error

--- mvpa/tests/test_hyperalignment.py
+++ mvpa/tests/test_hyperalignment.py
@@ -25,36 +25,55 @@ class HyperAlignmentTests(unittest.TestCase):
 
     def testBasicFunctioning(self):
         # get a dataset with some prominent trends in it
-        ds_orig = datasets['uni4large']
+        ds4l = datasets['uni4large']
         # lets select for now only meaningful features
-        ds_orig = ds_orig[:, ds_orig.a.nonbogus_features]
+        ds_orig = ds4l[:, ds4l.a.nonbogus_features]
         n = 5 # # of datasets to generate
-        Rs, dss_rotated = [], []
+        Rs, dss_rotated, dss_rotated_clean, random_shifts, random_scales \
+            = [], [], [], [], []
         # now lets compose derived datasets by using some random
         # rotation(s)
-        for _ in xrange(n):
+        for i in xrange(n):
             R = get_random_rotation(ds_orig.nfeatures)
             Rs.append(R)
             ds_ = ds_orig.copy()
-            ds_.samples = N.dot(ds_orig.samples, R)
+            # reusing random data from dataset itself
+            random_scales += [ds_orig.samples[i, 3] * 100]
+            random_shifts += [ds_orig.samples[i+10] * 10]
+            random_noise = ds4l.samples[:, ds4l.a.bogus_features[:4]]
+            ds_.samples = N.dot(ds_orig.samples, R) * random_scales[-1] \
+                          + random_shifts[-1]
+            dss_rotated_clean.append(ds_)
+
+            ds_ = ds_.copy()
+            ds_.samples = ds_.samples + 0.1 * random_noise
             dss_rotated.append(ds_)
 
         ref_ds = 0                      # by default should be this one
         ha = Hyperalignment()
-        mappers = ha(dss_rotated)
-        # Map data back
-        dss_back = [m.forward(ds_) for m, ds_ in zip(mappers, dss_rotated)]
+        # Lets test two scenarios -- in one with no noise -- we should get
+        # close to perfect reconstruction.  If noise was added -- not so good
+        for noisy, dss in ((False, dss_rotated_clean),
+                           (True, dss_rotated)):
+            mappers = ha(dss)
+            # Map data back
 
-        ds_orig_norm = N.linalg.norm(ds_orig.samples)
-        nddss = []
-        ds_orig_Rref = N.dot(ds_orig.samples, Rs[ref_ds])
-        for ds_back in dss_back:
-            dds = ds_back.samples - ds_orig_Rref
-            ndds = N.linalg.norm(dds) / ds_orig_norm
-            nddss += [ndds]
-        self.failUnless(N.all(ndds <= 1e-10),
-            msg="Should have reconstructed original dataset more or less."
-                " Got normed differences %s" % nddss)
+            dss_clean_back = [m.forward(ds_)
+                              for m, ds_ in zip(mappers, dss_rotated_clean)]
+
+            ds_norm = N.linalg.norm(dss[ref_ds].samples)
+            nddss = []
+            ds_orig_Rref = N.dot(ds_orig.samples, Rs[ref_ds]) \
+                           * random_scales[ref_ds] \
+                           + random_shifts[ref_ds]
+            for ds_back in dss_clean_back:
+                dds = ds_back.samples - ds_orig_Rref
+                ndds = N.linalg.norm(dds) / ds_norm
+                nddss += [ndds]
+            self.failUnless(N.all(ndds <= (1e-10, 1e-2)[int(noisy)]),
+                msg="Should have reconstructed original dataset more or less."
+                    " Got normed differences %s in %s case."
+                    % (nddss, ('clean', 'noisy')[int(noisy)]))
         pass
 
 
