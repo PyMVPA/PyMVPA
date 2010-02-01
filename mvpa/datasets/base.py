65e15a391e7df411a399d37bcd200b6010108c45
Yaroslav Halchenko <debian@onerussian.com>
2010-02-01 17:14:33 -0500

    BF: do not crash in summary() if dataset managed to have no features left

--- mvpa/datasets/base.py
+++ mvpa/datasets/base.py
@@ -793,9 +793,12 @@ class Dataset(object):
             # TODO -- avg per chunk?
             # XXX We might like to use scipy.stats.describe to get
             # quick summary statistics (mean/range/skewness/kurtosis)
-            s += "%sstats: mean=%g std=%g var=%g min=%g max=%g\n" % \
-                 (ssep, N.mean(samples), N.std(samples),
-                  N.var(samples), N.min(samples), N.max(samples))
+            if self.nfeatures:
+                s += "%sstats: mean=%g std=%g var=%g min=%g max=%g\n" % \
+                     (ssep, N.mean(samples), N.std(samples),
+                      N.var(samples), N.min(samples), N.max(samples))
+            else:
+                s += "%sstats: dataset has no features\n" % ssep
 
         if lstats:
             s += self.summary_labels(maxc=maxc, maxl=maxl)
