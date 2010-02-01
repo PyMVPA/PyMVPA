b5d7d873525311897bdd628b0ffc223edf1473b8
Yaroslav Halchenko <debian@onerussian.com>
2010-02-01 14:04:48 -0500

    Boosting version to 0.4.4

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ if have_libsvm:
 
 # define the setup
 setup(name         = 'pymvpa',
-      version      = '0.4.3',
+      version      = '0.4.4',
       author       = 'Michael Hanke, Yaroslav Halchenko, Per B. Sederberg',
       author_email = 'pkg-exppsy-pymvpa@lists.alioth.debian.org',
       license      = 'MIT License',
