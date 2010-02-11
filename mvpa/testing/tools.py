33e8f663806beef96d2b22606e4a08964fd761e5
Yaroslav Halchenko <debian@onerussian.com>
2010-02-11 15:29:50 -0500

    NF: providing SkipTest and skip_if_no_external helper from testing.tools

--- mvpa/testing/tools.py
+++ mvpa/testing/tools.py
@@ -17,6 +17,7 @@ from mvpa.base import externals
 
 if externals.exists('nose'):
     # We use nose now
+    from nose import SkipTest
     from nose.tools import (
         ok_, eq_,
         # Asserting (pep8-ed from unittest)
@@ -37,8 +38,43 @@ else:
     assert_equal = assert_equals = assert_not_equal = asserte_not_equals = \
     timed = with_setup = raises = istest = nottest = make_decorator = _need_nose
 
+    class SkipTest(Exception):
+        """Raise this exception to mark a test as skipped.
+        """
+        pass
+
 # Some pieces are useful from numpy.testing
 from numpy.testing import (
     assert_almost_equal, assert_approx_equal,
     assert_array_almost_equal, assert_array_equal, assert_array_less,
     assert_string_equal)
+
+
+def skip_if_no_external(dep, min_version=None, max_version=None):
+    """Raise SkipTest if external is missing
+
+    Parameters
+    ----------
+    dep : string
+      Name of the external
+    min_version : None or string or tuple
+      Minimal required version
+    max_version : None or string or tuple
+      Maximal required version
+    """
+
+    if not externals.exists(dep):
+        raise SkipTest, \
+              "External %s is not present thus tests battery skipped" % dep
+
+    if min_version is not None and externals.versions[dep] < min_version:
+        raise SkipTest, \
+              "Minimal version %s of %s is required. Present version is %s" \
+              ". Test was skipped." \
+              % (min_version, dep, externals.versions[dep])
+
+    if max_version is not None and externals.versions[dep] > max_version:
+        raise SkipTest, \
+              "Maximal version %s of %s is required. Present version is %s" \
+              ". Test was skipped." \
+              % (min_version, dep, externals.versions[dep])
