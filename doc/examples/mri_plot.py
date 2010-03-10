9e78b64b86fda7ea9e380be313f319af2b04677d
Yaroslav Halchenko <debian@onerussian.com>
2010-03-10 09:00:52 -0500

    use tutorial_data for mri_plot example

--- doc/examples/mri_plot.py
+++ doc/examples/mri_plot.py
@@ -37,7 +37,8 @@ elsewhere, hence we only provide the code here for the sake of completeness.
 from mvpa.suite import *
 
 # load PyMVPA example dataset
-datapath = os.path.join(pymvpa_datadbroot, 'demo_blockfmri', 'demo_blockfmri')
+datapath = os.path.join(pymvpa_datadbroot,
+                        'tutorial_data', 'tutorial_data', 'data')
 attr = SampleAttributes(os.path.join(datapath, 'attributes.txt'))
 dataset = fmri_dataset(samples=os.path.join(datapath, 'bold.nii.gz'),
                        targets=attr.targets,
