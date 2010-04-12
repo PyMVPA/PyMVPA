ace5f8c78437f023f51f60283d805992f5c6ea85
Michael Hanke <michael.hanke@gmail.com>
2010-04-12 13:05:01 -0400

    RF: Remove duplicate 'mode' argument of NoneSplitter()
    That functionality is already supported by the base class (reverse).

--- mvpa/tests/test_datameasure.py
+++ mvpa/tests/test_datameasure.py
@@ -352,7 +352,7 @@ class SensitivityAnalysersTests(unittest.TestCase):
         sana = SplitFeaturewiseDatasetMeasure(
             analyzer=SMLR(
               fit_all_weights=True).get_sensitivity_analyzer(),
-            splitter=NoneSplitter(npertarget=0.25, mode='first',
+            splitter=NoneSplitter(npertarget=0.25, reverse=True,
                                   nrunspersplit=2),
             enable_ca=['splits', 'sensitivities'])
         sens = sana(ds)
