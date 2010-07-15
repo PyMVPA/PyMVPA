f6b1e7bb37b00e3a693b00e7207c0f4ddebe3d5f
Yaroslav Halchenko <debian@onerussian.com>
2010-07-15 11:22:56 -0400

    unifying some modelines even more

--- doc/examples/som.py
+++ doc/examples/som.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#emacs: -*- mode: python-mode; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 #
