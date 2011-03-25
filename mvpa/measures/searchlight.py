923d20d7095ffea71bcd53f1f1f1261cc106dd26
Michael Hanke <michael.hanke@gmail.com>
2011-03-25 08:58:28 -0400

    DOC: BaseSearchlight is not limited to spheres (note in docs).

--- mvpa/measures/searchlight.py
+++ mvpa/measures/searchlight.py
@@ -49,10 +49,10 @@ class BaseSearchlight(Measure):
           Engine to use to discover the "neighborhood" of each feature.
           See :class:`~mvpa.misc.neighborhood.QueryEngine`.
         roi_ids : None or list(int) or str
-          List of feature ids (not coordinates) the shall serve as sphere
-          centers. Alternatively, this can be the name of a feature attribute
-          of the input dataset, whose non-zero values determine the feature
-          ids. By default all features will be used.
+          List of feature ids (not coordinates) the shall serve as ROI seeds
+          (e.g. sphere centers). Alternatively, this can be the name of a
+          feature attribute of the input dataset, whose non-zero values
+          determine the feature ids. By default all features will be used.
         nproc : None or int
           How many processes to use for computation.  Requires `pprocess`
           external module.  If None -- all available cores will be used.
