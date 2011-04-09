7bb7db158afb1d9737a8f2de30096cb3052b74a6
Yaroslav Halchenko <debian@onerussian.com>
2011-04-09 10:54:42 -0400

    ENH: svndclf.py -- no unneeded \n in printout

--- doc/examples/svdclf.py
+++ doc/examples/svdclf.py
@@ -81,7 +81,7 @@ results = []
 labels = []
 
 for desc, clf in clfs:
-    print desc
+    print desc.replace('\n', ' ')
     cv = CrossValidation(clf, NFoldPartitioner())
     res = cv(dataset)
     # there is only one 'feature' i.e. the error in the returned
