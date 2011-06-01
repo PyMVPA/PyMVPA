b1467318946757d1967a6aa33e0cbec79cbd9114
Yaroslav Halchenko <debian@onerussian.com>
2011-06-01 13:42:01 -0400

    GPRWeights -- minimalistic smoke test

--- mvpa/tests/test_gpr.py
+++ mvpa/tests/test_gpr.py
@@ -10,10 +10,13 @@
 
 import unittest
 
+from mvpa.base import externals
+
 from mvpa.misc import data_generators
 from mvpa.clfs.kernel import KernelLinear as GeneralizedLinearKernel
 from mvpa.clfs.gpr import GPR
 
+from tests_warehouse import *
 from numpy.testing import assert_array_equal, assert_array_almost_equal
 
 if __debug__:
@@ -32,6 +35,29 @@ class GPRTests(unittest.TestCase):
 
     def test_linear(self):
         pass
+
+    def test_gpr_model_selection(self):
+        """Smoke test for running model selection while getting GPRWeights
+        """
+        if not externals.exists('openopt'):
+            return
+
+        dataset = datasets['uni2small'] #data_generators.linear1d_gaussian_noise()
+        k = GeneralizedLinearKernel()
+        clf = GPR(k, enable_states=['log_marginal_likelihood'])
+        sa = clf.getSensitivityAnalyzer() # should be regular weights
+        sa_ms = clf.getSensitivityAnalyzer(flavor='model_select') # with model selection
+        def prints():
+            print clf.states.log_marginal_likelihood, clf.kernel.Sigma_p, clf.kernel.sigma_0
+
+        sa(dataset)
+        lml = clf.states.log_marginal_likelihood
+
+        sa_ms(dataset)
+        lml_ms = clf.states.log_marginal_likelihood
+
+        self.failUnless(lml_ms > lml)
+
 
 
 def suite():
