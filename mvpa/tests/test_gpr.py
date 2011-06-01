af025d398586f1e5ccefde229e9154c9c8e7222e
Yaroslav Halchenko <debian@onerussian.com>
2011-06-01 13:42:01 -0400

    GPRWeights -- minimalistic smoke test (disabled in 0.6, marked TODO)
    Conflicts:
    
    	mvpa/tests/test_gpr.py -- states -> ca, etc

--- mvpa/tests/test_gpr.py
+++ mvpa/tests/test_gpr.py
@@ -8,16 +8,19 @@
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 """Unit tests for PyMVPA GPR."""
 
+from mvpa.base import externals
 from mvpa.misc import data_generators
+from mvpa.misc.attrmap import AttributeMap
 from mvpa.kernels.np import GeneralizedLinearKernel
 from mvpa.clfs.gpr import GPR
 
 from mvpa.testing import *
+from mvpa.testing.datasets import datasets
 from mvpa.testing.tools import assert_array_equal, assert_array_almost_equal
 
 if __debug__:
     from mvpa.base import debug
-    
+
 
 class GPRTests(unittest.TestCase):
 
@@ -31,6 +34,32 @@ class GPRTests(unittest.TestCase):
 
     def test_linear(self):
         pass
+
+    def __test_gpr_model_selection(self):
+        """Smoke test for running model selection while getting GPRWeights
+
+        TODO: DISABLED because setting of hyperparameters was not adopted for 0.6 (yet)
+        """
+        if not externals.exists('openopt'):
+            return
+        amap = AttributeMap()           # we would need to pass numbers into the GPR
+        dataset = datasets['uni2small'].copy() #data_generators.linear1d_gaussian_noise()
+        dataset.targets = amap.to_numeric(dataset.targets).astype(float)
+        k = GeneralizedLinearKernel()
+        clf = GPR(k, enable_ca=['log_marginal_likelihood'])
+        sa = clf.get_sensitivity_analyzer() # should be regular weights
+        sa_ms = clf.get_sensitivity_analyzer(flavor='model_select') # with model selection
+        def prints():
+            print clf.ca.log_marginal_likelihood, clf.kernel.Sigma_p, clf.kernel.sigma_0
+
+        sa(dataset)
+        lml = clf.ca.log_marginal_likelihood
+
+        sa_ms(dataset)
+        lml_ms = clf.ca.log_marginal_likelihood
+
+        self.failUnless(lml_ms > lml)
+
 
 
 def suite():
