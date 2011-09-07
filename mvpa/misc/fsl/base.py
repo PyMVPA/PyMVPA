0ecac039bf77241526df49749a199ff84d6fca85
Yaroslav Halchenko <debian@onerussian.com>
2011-09-07 18:27:38 -0400

    BF: while reading .fsf split() fields only twice (thanks Russel for the report and tentative patch)

--- mvpa/misc/fsl/base.py
+++ mvpa/misc/fsl/base.py
@@ -269,7 +269,7 @@ def read_fsl_design(fsf_file):
             continue
 
         # strip leading TCL 'set'
-        key, value = line.split()[1:]
+        key, value = line.split(None, 2)[1:]
 
         # fixup the 'y-' thing
         if value == 'y-':
