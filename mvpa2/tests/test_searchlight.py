dfcac36b2e4d4ce8bbdb048291f846f402e692a9
Yaroslav Halchenko <debian@onerussian.com>
2012-02-06 19:24:16 -0500

    ENH: just 1 tiny usecase for search of corrcoefs among searchlights

--- mvpa2/tests/test_searchlight.py
+++ mvpa2/tests/test_searchlight.py
@@ -339,6 +339,37 @@ class SearchlightTests(unittest.TestCase):
 
         # elsewhere they should tend to be better but not guaranteed
 
+    def test_usecase_concordancesl(self):
+        import numpy as np
+        from mvpa2.base.dataset import vstack
+        from mvpa2.mappers.fx import mean_sample
+
+        # Take our sample 3d dataset
+        ds1 = datasets['3dsmall'].copy(deep=True)
+        ds1.fa['voxel_indices'] = ds1.fa.myspace
+        ds1.sa['subject'] = [1]  # not really necessary -- but let's for clarity
+        ds1 = mean_sample()(ds1) # so we get just a single representative sample
+
+        def corr12(ds):
+            corr = np.corrcoef(ds.samples)
+            assert(corr.shape == (2,2)) # for paranoid ones
+            return corr[0, 1]
+
+        for nsc, thr in ((0, 1.0),
+                         (0.1, 0.6)):   # just a bit of noise
+            ds2 = ds1.copy(deep=True)    # make a copy for the 2nd subject
+            ds2.sa['subject'] = [2]
+            ds2.samples += nsc * np.random.normal(size=ds1.shape)
+
+            # make sure that both have the same voxel indices
+            assert(np.all(ds1.fa.voxel_indices == ds2.fa.voxel_indices))
+            ds_both = vstack((ds1, ds2))# join 2 images into a single dataset
+                                        # with .sa.subject distinguishing both
+
+            sl = sphere_searchlight(corr12, radius=2)
+            slmap = sl(ds_both)
+            ok_(np.all(slmap.samples >= thr))
+
 def suite():
     return unittest.makeSuite(SearchlightTests)
 
