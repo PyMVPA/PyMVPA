da4f33ea078a0179db2ef7d9f7ebd078f48835f1
Swaroop Guntupalli <swaroopgj@gmail.com>
2012-03-27 14:19:51 -0400

    Modified hyperalignment measure to return 2 mappers per subject as one sample.

--- mvpa2/measures/hyperalignment.py
+++ mvpa2/measures/hyperalignment.py
@@ -1,30 +1,34 @@
 import numpy as np
 from mvpa2.algorithms.hyperalignment import Hyperalignment
 from mvpa2.measures.base import Measure
+from mvpa2.featsel.base import StaticFeatureSelection
+from mvpa2.datasets import Dataset
 from scipy.linalg import LinAlgError
     
 class HyperalignmentMeasure(Measure):
     is_trained=True
-    def __init__(self, nsubjs=11, scale=0.0, **kwargs):
+    def __init__(self, ndatasets=11, scale=0.0, index_attr='index', **kwargs):
         Measure.__init__(self, **kwargs)
-        self.nsubjs = nsubjs
+        self.ndatasets = ndatasets
         self.scale = scale
+        self._index_attr = index_attr
         
     def __call__(self, dataset):
         # create the dissimilarity matrix for the data in the input dataset
         ds = []
-        nsamples = dataset.nsamples/self.nsubjs
+        nsamples = dataset.nsamples/self.ndatasets
         seed_index = np.where(dataset.fa.roi_seed)
         if self.scale>0.0:
             dist = np.sum(np.abs(dataset.fa.voxel_indices-dataset.fa.voxel_indices[seed_index]), axis=1)
             dist = np.exp(-(self.scale*dist/np.float(max(dist)) )**2)
             dataset.samples = dataset.samples*dist
-        for i in range(self.nsubjs):
+        for i in range(self.ndatasets):
             ds.append(dataset[0+i*nsamples:nsamples*(i+1),])
-        for ref_ds in range(self.nsubjs):
+        for ref_ds in range(self.ndatasets):
             try:
                 hyper = Hyperalignment(zscore_common=True, ref_ds = ref_ds)
                 mappers = hyper(datasets=ds)
+                # Extract only the row/column corresponding to the center voxel
                 mappers = [ np.squeeze(m.proj[:,seed_index]) for m in mappers]
                 break
             except LinAlgError:
@@ -32,14 +36,13 @@ class HyperalignmentMeasure(Measure):
                 print "Nevermind, this is not a good idea for searchlight hyperalignment"
                 raise
                 break
-                if ref_ds == self.nsubjs-1:
+                if ref_ds == self.ndatasets-1:
                     mappers = []
                     print "SVD didn't converge with any reference. We are screwed :("
                     raise
-                
             else:
                 print "We are Screwed..."
         
-        # Extract only the row/column corresponding to the center voxel
-        return {'proj':mappers,'ids':dataset.fa.index}
+        
+        return Dataset(samples=np.asanyarray([{'proj':mapper,'fsel':StaticFeatureSelection(dataset.fa[self._index_attr].value)} for mapper in mappers]))
 
