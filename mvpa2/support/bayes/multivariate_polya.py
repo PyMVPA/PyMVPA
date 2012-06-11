ea1a56f64081ebb0157892223f52b13ae57ea169
Yaroslav Halchenko <debian@onerussian.com>
2012-06-11 13:05:28 -0400

    ENH: added our headers for support.bayes + minor DOC change

--- mvpa2/support/bayes/multivariate_polya.py
+++ mvpa2/support/bayes/multivariate_polya.py
@@ -1,5 +1,11 @@
-# -*- coding: iso-8859-15 -*-
-
+# emacs: coding: iso-8859-15; -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the PyMVPA package for the
+#   copyright and license terms.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 """PDF of the multivariate Pólya distribution.
 
 See: http://en.wikipedia.org/wiki/Multivariate_P%C3%B3lya_distribution
