7a6e1bc6bf981733bc44bc1ce8da143c5f181fe1
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2012-06-11 13:37:50 +0200

    bit of cleanup

--- mvpa2/misc/surfing/surf_fs_asc.py
+++ mvpa2/misc/surfing/surf_fs_asc.py
@@ -98,11 +98,11 @@ def write(surf, fn, overwrite=False, comment=None):
     comment : str
         Comments to add to 'fn'
     '''
-    
-    if isinstance(surf,str) and isinstance(fn,Surface):
-        surf,fn=fn,surf
+
+    if isinstance(surf, str) and isinstance(fn, Surface):
+        surf, fn = fn, surf
         print "Swap"
-    
+
     if not overwrite and os.path.exists(fn):
         raise Exception("File already exists: %s" % fn)
 
@@ -124,114 +124,114 @@ def write(surf, fn, overwrite=False, comment=None):
     f = open(fn, 'w')
     f.write("\n".join(s))
     f.close()
-    
+
 def inflated_hemi_pairs_reposition(surf_left, surf_right, touching_side, min_distance=10.):
-    touching_side=touching_side[0].lower()
-    
-    mn, mx=np.min, np.max
+    touching_side = touching_side[0].lower()
+
+    mn, mx = np.min, np.max
     #min=-1, max=1
-    side2dimsigns=dict(m=(0,-1),i=(1,1),s=(1,-1),a=(2,1),p=(2,-1))
-    
-    dim, rotatesign=side2dimsigns[touching_side]
-    if dim==0:
-        rotate_axis=None
+    side2dimsigns = dict(m=(0, -1), i=(1, 1), s=(1, -1), a=(2, 1), p=(2, -1))
+
+    dim, rotatesign = side2dimsigns[touching_side]
+    if dim == 0:
+        rotate_axis = None
     else:
-        rotate_axis=dim #1+((dim+1) % 2)
-        rotate_angle=90
-    
-    surfs=[surf_left, surf_right]
-    nsurfs=len(surfs)    
-    hemisigns=[1,-1]
+        rotate_axis = dim #1+((dim+1) % 2)
+        rotate_angle = 90
+
+    surfs = [surf_left, surf_right]
+    nsurfs = len(surfs)
+    hemisigns = [1, -1]
     if not rotate_axis is None:
-        theta=[0]*3
-        
+        theta = [0] * 3
+
         for i in xrange(nsurfs):
-            theta[rotate_axis]=rotate_angle*hemisigns[i]*rotatesign
-            surfs[i]=surfs[i].rotate(theta,unit='deg')
+            theta[rotate_axis] = rotate_angle * hemisigns[i] * rotatesign
+            surfs[i] = surfs[i].rotate(theta, unit='deg')
             print "Rotating"
-    
+
     for i in xrange(nsurfs):
-        hemisign=hemisigns[i]
-        sign=rotatesign*hemisign
+        hemisign = hemisigns[i]
+        sign = rotatesign * hemisign
         print "dim", dim, rotate_axis
-        print np.min(surfs[i].v(),axis=0)
-        print np.max(surfs[i].v(),axis=0)
-        
-        
-        coords=surfs[i].v()
-        
-        
+        print np.min(surfs[i].v(), axis=0)
+        print np.max(surfs[i].v(), axis=0)
+
+
+        coords = surfs[i].v()
+
+
         """xtreme=np.min(coords[:,0]*hemisign)
         
         delta=np.zeros((1,3))
         delta[0,0]=hemisign*(xtreme-min_distance*.5)
         surfs[i]=surfs[i]+(-delta)
         """
-        xtreme=np.min(coords[:,0]*-hemisign)
-        
-        delta=np.zeros((1,3))
-        delta[0,0]=hemisign*(xtreme-min_distance*.5)
-        surfs[i]=surfs[i]+(delta)
-        
-        print np.min(surfs[i].v(),axis=0), delta
-        print np.max(surfs[i].v(),axis=0)
-    
-    return tuple(surfs)
-    
-            
-            
-            
-        
-    
-    
-    
-    
-    
-    
-        
-        
-    
-    
-    
-        
+        xtreme = np.min(coords[:, 0] * -hemisign)
 
-if __name__=="__main__":
-    d='/Users/nick/organized/211_ak12_andy/ref/ab00/'
+        delta = np.zeros((1, 3))
+        delta[0, 0] = hemisign * (xtreme - min_distance * .5)
+        surfs[i] = surfs[i] + (delta)
+
+        print np.min(surfs[i].v(), axis=0), delta
+        print np.max(surfs[i].v(), axis=0)
+
+    return tuple(surfs)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    d = '/Users/nick/organized/211_ak12_andy/ref/ab00/'
     #surffn=d+'ico32_lh.pial_al.asc'
-    
-    
+
+
     #surffn2=d+'ico32_rh.pial_al.asc'
-    
-    surffn=d+'ico32_lh.inflated_al.asc'
-    surffn2=d+'ico32_rh.inflated_al.asc'
-    
-    
-    p=read(surffn)
-    q=read(surffn2)
-    
+
+    surffn = d + 'ico32_lh.inflated_al.asc'
+    surffn2 = d + 'ico32_rh.inflated_al.asc'
+
+
+    p = read(surffn)
+    q = read(surffn2)
+
     for touching in 'msiap':
-        pr,qr=inflated_hemi_pairs_reposition(p,q,touching)
-    
-        fnout=d+'_' + touching + 'bh.asc'
-    
-        both=pr.merge(qr)
-        write(fnout,both,overwrite=True)
-        
+        pr, qr = inflated_hemi_pairs_reposition(p, q, touching)
+
+        fnout = d + '_' + touching + 'bh.asc'
+
+        both = pr.merge(qr)
+        write(fnout, both, overwrite=True)
+
     for i in []:
-        cm=s.center_of_mass()
+        cm = s.center_of_mass()
         print cm
-        
-        cm=None
-        
+
+        cm = None
+
         #cm=[100,100,100]
-        
-        theta=[0,0,0]
-        theta[2]=i*45.
-        theta[1]=i*45.
-        r=s.rotate(theta,unit='deg',center=cm)
-        fnout=d+'_s_%d.asc' % i
-        
-        m=s.merge(r,r)
-        
-        write(m,fnout,overwrite=True)
-    
+
+        theta = [0, 0, 0]
+        theta[2] = i * 45.
+        theta[1] = i * 45.
+        r = s.rotate(theta, unit='deg', center=cm)
+        fnout = d + '_s_%d.asc' % i
+
+        m = s.merge(r, r)
+
+        write(m, fnout, overwrite=True)
+
