37ddb1f46516deb2632ff0418d80c7809291a78f
Michael Hanke <michael.hanke@gmail.com>
2012-09-25 14:19:36 +0200

    ENH: More messing around with the cmdline interface.

--- mvpa2/cmdline/cmd_hyperalign.py
+++ mvpa2/cmdline/cmd_hyperalign.py
@@ -10,30 +10,61 @@
 
 __docformat__ = 'restructuredtext'
 
-
+import argparse
 from mvpa2.base import verbose
 if __debug__:
     from mvpa2.base import debug
-from mvpa2.cmdline.helpers import parser_add_common_args, args2datasets
+from mvpa2.cmdline.helpers \
+        import parser_add_common_args, args2datasets, strip_from_docstring, \
+               param2arg, ca2arg
+
+from mvpa2.algorithms.hyperalignment import Hyperalignment
 
 parser_args = {
-    'description': 'subparse descr'
+    'description': strip_from_docstring(Hyperalignment.__doc__,
+                                        paragraphs=(4,),
+                                        sections=(('Examples', 'Notes'))),
+    'formatter_class': argparse.RawDescriptionHelpFormatter,
 }
 
+_supported_cas = (
+    'residual_errors', 'training_residual_errors',
+)
+_supported_parameters = (
+    'alpha', 'level2_niter', 'ref_ds', 'zscore_all', 'zscore_common',
+)
+
 def setup_parser(parser):
-    parser_add_common_args(
-            parser,
-            pos=['multidata'],
-            opt=['multimask']
-        )
-    return parser
+    # order of calls is relevant!
+    inputargs = parser.add_argument_group('input data arguments')
+    parser_add_common_args(inputargs, pos=['multidata'], opt=['multimask'])
+    algoparms = parser.add_argument_group('algorithm parameters')
+    for param in _supported_parameters:
+        param2arg(algoparms, Hyperalignment, param)
+    outputopts = parser.add_argument_group('output options')
+    for ca in _supported_cas:
+        ca2arg(outputopts, Hyperalignment, ca)
 
 
 def run(args):
-    print 'Running with', repr(args)
+    if __debug__:
+        debug('CMDLINE', "loading input data from %s" % args.data)
     dss = args2datasets(args.data, args.masks)
     verbose(1, "Loaded %i input datasets" % len(dss))
-    print [str(ds) for ds in dss]
+    # TODO at this point more check could be done, e.g. ref_ds > len(dss)
+    # assemble parameters
+    params = dict([(param, getattr(args, param)) for param in _supported_parameters])
+    if __debug__:
+        debug('CMDLINE', "configured parameters: '%s'" % params)
+    # assemble CAs
+    enabled_ca = [ca for ca in _supported_cas if getattr(args, ca)]
+    if __debug__:
+        debug('CMDLINE', "enabled conditional attributes: '%s'" % enabled_ca)
+    hyper = Hyperalignment(enable_ca=enabled_ca, **params)
+    verbose(1, "Running hyperalignment")
+    promappers = hyper(dss)
+    # think about how to output the results
+    #print [str(ds) for ds in hyper.ca.residual_errors]
 
 
 
--- mvpa2/cmdline/helpers.py
+++ mvpa2/cmdline/helpers.py
@@ -10,6 +10,8 @@
 
 __docformat__ = 'restructuredtext'
 
+import argparse
+
 from mvpa2.base import verbose
 if __debug__:
     from mvpa2.base import debug
@@ -44,7 +46,7 @@ def _mask_data(data, mask):
                 raise ValueError("masks for readily loaded datasets need to be "
                                  "valid slice arguments")
             else:
-                raise NotImplementedError("create static feature selection from mask")
+                data = data[:, mask]
         return data
     elif data is None:
         return None
@@ -83,4 +85,134 @@ def args2datasets(data_specs, mask_specs=None):
     dss = [_mask_data(*vals) for vals in maskdata]
     return dss
 
+def strip_from_docstring(doc, paragraphs=None, sections=None):
+    if paragraphs is None:
+        paragraphs = []
+    if sections is None:
+        sections = []
+    out = []
+    # split into paragraphs
+    doc = doc.split('\n\n')
+    section = ''
+    for par_i, par in enumerate(doc):
+        lines = par.split('\n')
+        if len(lines) > 1 \
+           and len(lines[0]) == len(lines[1]) \
+           and lines[1] == '-' * len(lines[0]):
+               section = lines[0]
+        if (par_i in paragraphs) or (section in sections):
+            continue
+        out.append(par)
+    return '\n\n'.join(out)
+
+def param2arg(parser, klass, param, arg_names=None):
+    param = klass._collections_template['params'][param]
+    if arg_names is None:
+        arg_names = ('--%s' % param.name.replace('_', '-'),)
+    # TODO add more useful information from the parameter definition
+    # into the parser
+    parser.add_argument(*arg_names, help=param.__doc__,
+                        default=param.default,
+                        type=CmdArg2ParameterType(param))
+
+def ca2arg(parser, klass, ca, arg_names=None):
+    ca = klass._collections_template['ca'][ca]
+    if arg_names is None:
+        arg_names = ('--%s' % ca.name.replace('_', '-'),)
+    parser.add_argument(*arg_names, help=ca.__doc__, default=False,
+                        action='store_true')
+
+
+class CmdArg2ParameterType(object):
+    def __init__(self, param):
+        supported = ('allowedtype', 'min', 'max', 'name')
+        for p in supported:
+            varname = '_%s' % p
+            if hasattr(param, p):
+                setattr(self, varname, getattr(param, p))
+
+    def __call__(self, arg):
+        # interpret out type specs for parameters and return callables that convert
+        # cmdline args (i.e. strings) into the required format
+        # look here for more info: http://docs.python.org/library/argparse.html#type
+        converted = self._arg2types(arg)
+        self._range_check(converted)
+        return converted
+
+    def _range_check(self, val):
+        if hasattr(self, '_min'):
+            if val < self._min:
+                raise argparse.ArgumentTypeError(
+                        "range error "
+                        "(value '%s' less than configured minimum '%s')"
+                        % (val, self._min))
+        if hasattr(self, '_max'):
+            if val > self._max:
+                raise argparse.ArgumentTypeError(
+                        "range error "
+                        "(value '%s' larger than configured maximum '%s')"
+                        % (val, self._max))
+
+    def _arg2types(self, arg):
+        allowed = self._allowedtype
+        if __debug__:
+            debug('CMDLINE',
+                    "type conversion for parameter '%s': %s -> %s"
+                    % (self._name, arg, allowed))
+        # loop over alternatives
+        types = allowed.split(' or ')
+        # strip out types we would not need to convert anyway
+        not_convert_types = ('basestring', 'str')
+        effective_types = [t for t in types if not t in not_convert_types]
+        for type_ in effective_types:
+            try:
+                return self._arg2type(arg, type_)
+            except argparse.ArgumentTypeError:
+                if __debug__:
+                    debug('CMDLINE', "type conversion into '%s' failed" % type_)
+        # we only get here if conversion failed
+        if len([t for t in types if t in not_convert_types]):
+            # continuing without exception is possible
+            return arg
+        raise argparse.ArgumentTypeError(
+                "cannot convert '%s' into '%s'" % (arg, allowed))
+
+    def _arg2type(self, arg, allowed):
+        if allowed == None:
+            # we know nothing
+            if __debug__:
+                debug('CMDLINE',
+                      "skipping parameter value conversion -- no type "
+                      "spec for '%s' available" % self._name)
+            return arg
+        elif allowed == 'bool':
+            return arg2bool(arg)
+        elif allowed == 'int':
+            return int(arg)
+        elif allowed in ('None', 'none'):
+            return arg
+        elif allowed in ('float', 'float32', 'float64'):
+            return float(arg)
+        else:
+            raise argparse.ArgumentTypeError(
+                "unsupported parameter type specification: '%s'" % allowed)
+
+
+def arg2bool(arg):
+    arg = arg.lower()
+    if arg in ['0', 'no', 'off', 'disable', 'false']:
+        return False
+    elif arg in ['1', 'yes', 'on', 'enable', 'true']:
+        return True
+    else:
+        raise argparse.ArgumentTypeError(
+                "'%s' cannot be converted into a boolean" % arg)
+
+def arg2none(arg):
+    arg = arg.lower()
+    if arg == 'none':
+        return None
+    else:
+        raise argparse.ArgumentTypeError(
+                "'%s' cannot be converted into `None`" % arg)
 
