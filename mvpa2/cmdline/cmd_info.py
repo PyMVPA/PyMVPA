9503af52deab33e79c7134a6432e84d23fca049c
Michael Hanke <michael.hanke@gmail.com>
2012-10-04 20:01:42 +0200

    ENH: Make parser_args optional, use module __doc__ if needed

--- mvpa2/cmdline/cmd_info.py
+++ mvpa2/cmdline/cmd_info.py
@@ -6,17 +6,14 @@
 #   copyright and license terms.
 #
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
-""""""
+"""Query various information about a PyMVPA installation.
+
+If no option is given, a  useful subset of the available information is printed.
+"""
 
 import mvpa2
 
 __docformat__ = 'restructuredtext'
-
-parser_args = {
-    'description':
-"""Query various information about this PyMVPA installation. If no option is
-given useful subset of the available information is printed."""
-}
 
 def setup_parser(parser):
     excl = parser.add_mutually_exclusive_group()
