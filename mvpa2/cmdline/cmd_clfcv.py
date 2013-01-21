6bf15019f1e3dbafecfa694d8cb0987da2b218bd
Michael Hanke <michael.hanke@gmail.com>
2013-01-21 10:33:41 +0100

    Various small steps forward.

--- mvpa2/cmdline/cmd_clfcv.py
+++ mvpa2/cmdline/cmd_clfcv.py
@@ -15,13 +15,29 @@ if __debug__:
     from mvpa2.base import debug
 from mvpa2.cmdline.helpers \
         import parser_add_common_args, parser_add_common_opt
-
+from mvpa2.base.hdf5 import h5save, h5load
+from mvpa2.measures.base import CrossValidation
+from mvpa2.base.node import ChainNode
 
 def setup_parser(parser):
     # order of calls is relevant!
-    parser_add_common_args(parser, pos=['multidata'],
-                           opt=['multimask', 'classifier', 'partitioner'])
-    parser_add_common_opt(parser, 'partitioner', names=('-p', '--part'))
+    parser_add_common_args(parser, pos=['data'])
+    parser_add_common_opt(parser, 'classifier',
+                          names=('-c', '--clf', '--classifier'),
+                          required=True)
+    parser_add_common_opt(parser, 'partitioner',
+                          names=('-f', '--fold'),
+                          required=True)
 
 def run(args):
-    print args
+    ds = h5load(args.data)
+    # XXX remove me
+    ds = ds[:,:2]
+    gennode = ChainNode([args.fold], space=args.fold.get_space())
+    cv = CrossValidation(args.clf, gennode, enable_ca=['stats'])
+    res = cv(ds)
+    raise NotImplementedError
+    print cv.ca.stats
+    print res
+    print res.samples
+    return res
