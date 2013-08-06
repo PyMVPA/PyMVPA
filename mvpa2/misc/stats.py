c1329caaeb207b4abbbd045587dd05db8b1e8a16
Dylan Wagner <Dylan.D.Wagner@dartmouth.edu>
2013-08-06 15:55:05 -0400

    Cleaned up, fixed bug, and removed redundant methods

--- mvpa2/misc/stats.py
+++ mvpa2/misc/stats.py
@@ -14,9 +14,9 @@ from mvpa2.base import externals
 
 if externals.exists('scipy', raise_=True):
     import scipy.stats as st
+    from scipy.spatial.distance import pdist, squareform
     # evaluate once the fact of life
     __scipy_prior0101 = externals.versions['scipy'] < '0.10.1'
-
 import numpy as np
 import copy
 
@@ -95,63 +95,52 @@ class DSMatrix(object):
         Parameters
         ----------
         data_vectors : ndarray
-           m x n collection of vectors, where m is the number of exemplars
-           and n is the number of features per exemplar
+           m x n collection of vectors, where m is the number of samples
+           and n is the number of features per sample
         metric : string
            Distance metric to use (e.g., 'euclidean', 'spearman', 'pearson',
            'confusion')
         """
         # init members
         self.full_matrix = []
-        self.u_triangle = None
         self.vector_form = None
-        self._u_triangle_vecs = None # vectorized versions
-
-        # this one we know straight away, so set it
+        self.u_triangle = None
+        self.u_triangle_vector_form = None # vectorized versions
         self.metric = metric
 
-        # size of dataset (checking if we're dealing with a column vector only)
-        num_exem = np.shape(data_vectors)[0]
-        flag_1d = False
-        # changed 4/26/09 to new way of figuring out if array is 1-D
-        #if (isinstance(data_vectors, np.ndarray)):
-        if (not(num_exem == np.size(data_vectors))):
+        # Determine number of samples and features and check that if ndim is 1
+        # then the metric is confusion (other metrics require ndim > 1)
+        num_exem = np.shape(data_vectors)[0] #num samples
+        if not(data_vectors.ndim == 1):
+            flag_1d = False
             num_features = np.shape(data_vectors)[1]
-        else:
+        elif data_vectors.ndim == 1 and metric == 'confusion':
             flag_1d = True
             num_features = 1
+        else:
+            raise ValueError, \
+              "DSMatrix: %s metric requires multidimensional array. Provided" \
+              " array has only one dimension." %metric
 
         # generate output (dissimilarity) matrix
         dsmatrix = np.mat(np.zeros((num_exem, num_exem)))
 
+        # calculate dissimilarities
         if (metric == 'euclidean'):
-            #print 'Using Euclidean distance metric...'
-            # down rows
-            for i in range(num_exem):
-                # across columns
-                for j in range(num_exem):
-                    if (not(flag_1d)):
-                        dsmatrix[i, j] = np.linalg.norm(
-                            data_vectors[i, :] - data_vectors[j, :])
-                    else:
-                        dsmatrix[i, j] = np.linalg.norm(
-                            data_vectors[i] - data_vectors[j])
+            dsmatrix = squareform(pdist(data_vectors,'euclidean'))
 
         elif (metric == 'spearman'):
-            #print 'Using Spearman rank-correlation metric...'
-            # down rows
             for i in range(num_exem):
                 # across columns
                 for j in range(num_exem):
                     dsmatrix[i, j] = 1 - st.spearmanr(
                         data_vectors[i, :], data_vectors[j, :])[0]
+            dsmatrix = np.asarray(dsmatrix)
 
         elif (metric == 'pearson'):
-            dsmatrix = 1 - np.corrcoef(data_vectors) #changed to 1-corr
+            dsmatrix = squareform(pdist(data_vectors,'correlation'))
 
         elif (metric == 'confusion'):
-            #print 'Using confusion correlation metric...'
-            # down rows
             for i in range(num_exem):
                 # across columns
                 for j in range(num_exem):
@@ -163,99 +152,32 @@ class DSMatrix(object):
                     else:
                         dsmatrix[i, j] = 1 - int(
                             data_vectors[i] == data_vectors[j])
+            dsmatrix = np.asarray(dsmatrix)
+        else:
+            raise ValueError, \
+              "DSMatrix: %s metric is not a valid metric" %metric
 
         self.full_matrix = dsmatrix
 
     ##REF: Name was automagically refactored
-    def get_triangle(self):
+    def get_triangle(self, k=1):
         # if we need to create the u_triangle representation, do so
         if (self.u_triangle is None):
-            self.u_triangle = np.triu(self.full_matrix)
-
+            self.u_triangle = np.triu(self.full_matrix, k)
         return self.u_triangle
 
     def get_triangle_vector_form(self, k=1):
-        '''
-        Returns values from a triangular part of the matrix in vector form
+        # if we need to create the u_triangle_vector_form representation, do so
+        if (self.u_triangle_vector_form is None):
+            self.u_triangle_vector_form =  np.squeeze(np.asarray(
+            self.full_matrix[np.triu_indices_from(self.full_matrix,k)]))
+        return self.u_triangle_vector_form
 
-        Parameters
-        ----------
-        k: int
-            offset from diagonal. k=0 means all values from the diagonal and those
-            above it, k=1 all values from the cells above the diagonal, etc
-
-        Returns
-        -------
-        v: np.ndarray (vector)
-            array with values from the similarity matrix. If the matrix is shaped
-            p xp, then if k>=0, then v has (p-k)*(p-k+1)/2 elements. If k<0, it has
-            p*p-(p+k)*(p+k-1)/2 elements.
-        '''
-
-        n = self.full_matrix.shape[0]
-        if k < -n or k > n:
-            raise IndexError("Require %d <= k <= %d" % (-n, n))
-
-        if self._u_triangle_vecs is None:
-            self._u_triangle_vecs = dict()
-
-        if not k in self._u_triangle_vecs:
-            msk = np.zeros((n, n), dtype=np.bool_)
-            for i in range(n):
-                for j in range(n):
-                    if i < j + k:
-                        break
-                    msk[i, j] = np.True_
-
-            self._u_triangle_vecs[k] = self.full_matrix[msk]
-
-        return self._u_triangle_vecs[k]
-
-    # create the dissimilarity matrix on the (upper) triangle of the two
-    # two dissimilarity matrices; we can just reuse the same dissimilarity
-    # matrix code, but since it will return a matrix, we need to pick out
-    # either dsm[0,1] or dsm[1,0]
-    # note:  this is a bit of a kludge right now, but it's the only way to solve
-    # certain problems:
-    #  1.  Set all 0-valued elements in the original matrix to -1 (an impossible
-    #        value for a dissimilarity matrix)
-    #  2.  Find the upper triangle of the matrix
-    #  3.  Create a vector from the upper triangle, but only with the
-    #      elements whose absolute value is greater than 0 -- this
-    #      will keep everything from the original matrix that wasn't
-    #      part of the zero'ed-out portion when we took the upper
-    #      triangle
-    #  4.  Set all the -1-valued elements in the vector to 0 (their
-    #      original value)
-    #  5.  Cast to numpy array
-    ##REF: Name was automagically refactored
     def get_vector_form(self):
-        if (self.vector_form is not None):
-            return self.vector_form
-
-        #orig_dsmatrix = copy.deepcopy(self.get_full_matrix())
-
-        #orig_dsmatrix[orig_dsmatrix == 0] = -1
-
-        #orig_tri = np.triu(orig_dsmatrix)
-
-        #vector_form = orig_tri[abs(orig_tri) > 0]
-
-        #vector_form[vector_form == -1] = 0
-        #can use pdist or just grab the upper triangular. ddw
-        self.vector_form = self.get_full_matrix()[np.triu_indices_from(self.get_full_matrix(),1)]
-
+        # if we need to create the vector form representation, do so
+        if (self.vector_form is None):
+            self.vector_form  = np.squeeze(np.asarray(self.full_matrix.flatten()))
         return self.vector_form
-
-    # XXX is there any reason to have these get* methods
-    #     instead of plain access to full_matrix and method?
-    ##REF: Name was automagically refactored
-    def get_full_matrix(self):
-        return self.full_matrix
-
-    ##REF: Name was automagically refactored
-    def get_metric(self):
-        return self.metric
 
 
 def _chk_asanyarray(a, axis):
