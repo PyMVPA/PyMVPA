affc7c19dda58323b1a7d2cbb0b5b7436c294529
Michael Hanke <michael.hanke@gmail.com>
2013-12-05 19:53:59 +0100

    NF: Add spectral filtering as a preproc command option

--- mvpa2/cmdline/cmd_preproc.py
+++ mvpa2/cmdline/cmd_preproc.py
@@ -13,10 +13,32 @@ supported are
 
 1. Polynomial de-trending
 
-2. Feature-wise Z-scoring
+2. Spectral filtering
+
+3. Feature-wise Z-scoring
 
 All preprocessing steps are applied in the above order. If a different order is
 required, preprocessing has to be split into two separate command calls.
+
+
+POLYNOMIAL DE-TRENDING
+
+This type of de-trending can be used to regress out arbitrary signals. In
+addition to polynomials of any degree arbitrary timecourses stored as sample
+attributes in a dataset can be used as confound regressors. This detrending
+functionality is, in contrast to the implementation of spectral filtering,
+also applicable to sparse-sampled data with potentially irregular inter-sample
+intervals.
+
+
+SPECTRAL FILTERING
+
+Several option are provided that are used to construct a Butterworth low-,
+high-, or band-pass filter. It is advised to inspect the filtered data
+carefully as inappropriate filter settings can lead to unintented side-effect.
+Only dataset with a fixed sampling rate are supported. The sampling rate
+must be provided.
+
 
 Examples:
 
@@ -92,6 +114,32 @@ normalize_args = ('options for data normalization', [
                instead of computing from actual data.""")),
 ])
 
+bandpassfilter_args = ('options for spectral filtering', [
+    ('--filter-passband',
+     dict(metavar='FREQ', nargs='+', type=float,
+          help="""critical frequencies of a Butterworth filter's pass band.
+          Critical frequencies need to match the unit of the specified sampling
+          rate (see: --sampling-rate). In case of a band pass filter low and
+          high frequency cutoffs need to be specified (in this order). For
+          low and high-pass filters is single cutoff frequency must be
+          provided. The type of filter (low/high-pass) is determined from the
+          relation to the stop band frequency (--filter-stopband).""")),
+    ('--filter-stopband',
+     dict(metavar='FREQ', nargs='+', type=float,
+          help="""Analog setting to --filter-passband for specifying the
+          filter's stop band.""")),
+    ('--sampling-rate',
+     dict(metavar='FREQ', type=float,
+          help="""sampling rate of the dataset. All frequency specifications
+          need to match the unit of the sampling rate.""")),
+    ('--filter-passloss',
+     dict(metavar='dB', type=float, default=1.0,
+          help="""maximum loss in the passband (dB). Default: 1 dB""")),
+    ('--filter-stopattenuation',
+     dict(metavar='dB', type=float, default=30.0,
+         help="""minimum attenuation in the stopband (dB). Default: 30 dB""")),
+])
+
 common_args = ('common options for all preprocessing', [
     ('--chunks',
      dict(metavar='CHUNKS_ATTR',
@@ -110,7 +158,8 @@ common_args = ('common options for all preprocessing', [
 def setup_parser(parser):
     parser_add_common_args(parser, pos=['data'])
     # order of calls is relevant!
-    for src in (common_args, detrend_args, normalize_args):
+    for src in (common_args, detrend_args, bandpassfilter_args,
+                normalize_args):
         srcgrp = parser.add_argument_group(src[0])
         for opt in src[1]:
             srcgrp.add_argument(opt[0], **opt[1])
@@ -139,6 +188,45 @@ def run(args):
                      chunks_attr=args.detrend_chunks,
                      opt_regs=args.detrend_regrs,
                      space=args.detrend_coords)
+    if args.filter_passband is not None:
+        from mvpa2.mappers.filters import iir_filter
+        from scipy.signal import butter, buttord
+        if args.sampling_rate is None or args.filter_stopband is None:
+            raise ValueError(
+                "spectral filtering requires specification of "
+                "--filter-stopband and --sampling-rate")
+        # determine filter type
+        nyquist = args.sampling_rate / 2.0
+        if len(args.filter_passband) > 1:
+            btype = 'bandpass'
+            if not len(args.filter_passband) == len(args.filter_stopband):
+                raise ValueError("passband and stopband specifications have to "
+                        "match in size")
+            wp = [v / nyquist for v in args.filter_passband]
+            ws = [v / nyquist for v in args.filter_stopband]
+        elif args.filter_passband[0] < args.filter_stopband[0]:
+            btype = 'lowpass'
+            wp = args.filter_passband[0] / nyquist
+            ws = args.filter_stopband[0] / nyquist
+        elif args.filter_passband[0] > args.filter_stopband[0]:
+            btype = 'highpass'
+            wp = args.filter_passband[0] / nyquist
+            ws = args.filter_stopband[0] / nyquist
+        else:
+            raise ValueError("invalid specification of Butterworth filter")
+        # create filter
+        verbose(1, "Spectral filtering (%s)" % (btype,))
+        try:
+            ord, wn = buttord(wp, ws,
+                              args.filter_passloss,
+                              args.filter_stopattenuation,
+                              analog=False)
+            b, a = butter(ord, wn, btype=btype)
+        except OverflowError:
+            raise ValueError("cannot contruct Butterworth filter for the given "
+                             "specification")
+        ds = iir_filter(ds, b, a)
+
     if args.zscore:
         from mvpa2.mappers.zscore import zscore
         verbose(1, "Z-score")
