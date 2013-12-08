fd5a43073e7c90a6b8c498434625ab55bbf91677
Michael Hanke <michael.hanke@gmail.com>
2013-12-08 22:03:53 +0100

    ENH: Allow for multiple expressions in pytest

--- mvpa2/cmdline/cmd_pytest.py
+++ mvpa2/cmdline/cmd_pytest.py
@@ -11,17 +11,17 @@
 This command is mostly useful for testing functionality and results via the
 commandline interface and for asserting arbitrary conditions in scripts.
 
-First, a optional dataset is loaded from one or more source. Afterwards a given
-expression (see --eval) is evaluated. Such an expression can be given as an
-argument on the command line, read from a file, or from STDIN. The return value
-of any given expression is ignored (not evaluated anyhow), only exceptitons are
-treated as errors and cause the command to exit with a non-zero return value.
-To implement tests and assertions is is best to utilize a Python unittest
-framework such as 'nose'.
+First, optional dataset(s) are loaded from one or more source. Afterwards any
+number of given expression (see --eval) are evaluated. Such an expression can be
+given as an argument on the command line, read from a file, or from STDIN. The
+return value of any given expression is ignored (not evaluated anyhow), only
+exceptitons are treated as errors and cause the command to exit with a non-zero
+return value.  To implement tests and assertions is is best to utilize a Python
+unittest framework such as 'nose'.
 
 In the namespace in which all expressions are evaluated the NumPy module is
 available via the alias 'np', and the nose.tools under the alias 'nt' (if
-installed).
+installed). Any loaded datasets are avialable as a list ``dss``.
 
 Examples:
 
@@ -31,7 +31,7 @@ Assert some condition
 
 Check for the presence of a particular sample attribute in a dataset
 
-  $ pymvpa2 pytest -e 'ds.sa.subj3' mydata.hdf5
+  $ pymvpa2 pytest -e 'dss[0].sa.subj3' mydata.hdf5
 
 """
 
@@ -48,7 +48,7 @@ from mvpa2.base import verbose, warning, error
 from mvpa2.datasets import vstack
 if __debug__:
     from mvpa2.base import debug
-from mvpa2.cmdline.helpers import hdf2ds, parser_add_common_opt
+from mvpa2.cmdline.helpers import arg2ds, parser_add_common_opt
 
 
 parser_args = {
@@ -56,24 +56,24 @@ parser_args = {
 }
 
 def setup_parser(parser):
-    parser_add_common_opt(parser, 'multidata', nargs='*')
+    parser_add_common_opt(parser, 'multidata', nargs='*', action='append')
     parser.add_argument('-e', '--eval', type=str, required=True,
-            metavar='EXPR',
+            metavar='EXPR', action='append',
             help="""Python expression, or filename of a Python script,
             or '-' to read expressions from STDIN.""")
 
 def run(args):
     if not args.data is None:
-        dss = hdf2ds(args.data)
-        ds = vstack(dss)
+        dss = [arg2ds(d) for d in args.data]
     try:
         import nose.tools as nt
     except ImportError:
         pass
-    if args.eval == '-':
-        exec sys.stdin
-    elif os.path.isfile(args.eval):
-        execfile(args.eval, globals())
-    else:
-        exec args.eval
+    for expr in args.eval:
+        if expr == '-':
+            exec sys.stdin
+        elif os.path.isfile(expr):
+            execfile(expr, globals())
+        else:
+            exec expr
 
