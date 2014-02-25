cf9c03ba89e2e0842b081fae62e766c47b9e4585
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2014-02-25 18:11:47 +0100

    PY3: change expected type of error depending on python version.

--- mvpa2/tests/test_constraints.py
+++ mvpa2/tests/test_constraints.py
@@ -13,6 +13,7 @@ from mvpa2.testing import *
 import unittest
 
 from mvpa2.base.constraints import *
+import sys
 
 class SimpleConstraintsTests(unittest.TestCase):
 
@@ -96,13 +97,18 @@ class SimpleConstraintsTests(unittest.TestCase):
         c = EnsureRange(min=3, max=7)
         # this should always work
         assert_equal(c(3.0), 3.0)
+
+        # Python 3 raises an TypeError if incompatible types are compared,
+        # whereas Python 2 raises a ValueError
+        type_error = TypeError if sys.version_info[0] >= 3 else ValueError
+
         # this should always fail
         assert_raises(ValueError, lambda: c(2.9999999))
         assert_raises(ValueError, lambda: c(77))
-        assert_raises(ValueError, lambda: c('fail'))
-        assert_raises(ValueError, lambda: c((3,4)))
+        assert_raises(type_error, lambda: c('fail'))
+        assert_raises(type_error, lambda: c((3, 4)))
         # since no type checks are performed
-        assert_raises(ValueError, lambda: c('7'))
+        assert_raises(type_error, lambda: c('7'))
 
         # Range doesn't have to be numeric
         c = EnsureRange(min="e", max="qqq")
