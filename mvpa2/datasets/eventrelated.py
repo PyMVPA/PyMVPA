cd636eabe69e6ecf867bf85011508b210d06e610
Michael Hanke <michael.hanke@gmail.com>
2014-02-27 14:36:02 +0100

    BF: Missed another rename.

--- mvpa2/datasets/eventrelated.py
+++ mvpa2/datasets/eventrelated.py
@@ -186,7 +186,7 @@ def _fit_hrf_event_model(
         glmfit_kwargs=None, regr_attrs=None, eprefix='event', conditions=None):
     if externals.exists('nipy', raise_=True):
         from nipy.modalities.fmri.design_matrix import make_dmtx
-        from mvpa2.mappers.nipy_glm import NiPyGLMMapper
+        from mvpa2.mappers.glm import NiPyGLMMapper
 
     evvars = _events2dict(events)
     add_paradigm_kwargs = {}
