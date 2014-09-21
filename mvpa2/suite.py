2d2e77f755bbb3bd2617c8d35a037af147a8dd33
Michael Hanke <michael.hanke@gmail.com>
2014-09-21 13:41:21 +0200

    RF: Sanitize imports

--- mvpa2/suite.py
+++ mvpa2/suite.py
@@ -119,7 +119,6 @@ from mvpa2.datasets.eep import *
 from mvpa2.datasets.eventrelated import *
 if externals.exists('nibabel') :
     from mvpa2.datasets.mri import *
-    from mvpa2.datasets.sources.openfmri import *
 from mvpa2.datasets.sources import *
 from mvpa2.datasets import niml
 from mvpa2.datasets.niml import from_niml, to_niml
