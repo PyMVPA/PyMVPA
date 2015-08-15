69820cd081ea02d68a194a60daa82f69419201ff
dinga92 <dinga92@gmail.com>
2015-08-15 12:14:17 +0200

    added more informative error msg

--- mvpa2/generators/permutation.py
+++ mvpa2/generators/permutation.py
@@ -231,6 +231,9 @@ class AttributePermutator(Node):
 
     def _permute_chunks(self, limit_idx, in_pattrs, out_pattrs, chunks=None):
         # limit_idx is doing nothing
+        
+        if chunks == None:
+            raise RuntimeError("Missing 'chunk_attr' for strategy='chunk'")
 
         uniques = np.unique(chunks)
 
