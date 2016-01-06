d7f2addec696672f4db67722ca7464e50bbdbab0
Michael Hanke <michael.hanke@gmail.com>
2016-01-06 15:51:06 +0100

    PEP8 nothing else

--- mvpa2/tests/test_mapper.py
+++ mvpa2/tests/test_mapper.py
@@ -10,10 +10,9 @@
 
 import numpy as np
 # for repr
-from numpy import array
 
 from mvpa2.testing.tools import ok_, assert_raises, assert_false, assert_equal, \
-        assert_true, assert_array_equal, nodebug
+    assert_true, assert_array_equal, nodebug
 from mvpa2.testing import sweepargs
 
 from mvpa2.testing.datasets import datasets
@@ -24,7 +23,6 @@ from mvpa2.mappers.slicing import SampleSliceMapper, StripBoundariesSamples
 from mvpa2.support.copy import copy
 from mvpa2.datasets.base import Dataset
 from mvpa2.base.collections import ArrayCollectable
-from mvpa2.datasets.base import dataset_wizard
 from mvpa2.mappers.flatten import ProductFlattenMapper
 
 import itertools
@@ -32,9 +30,11 @@ import operator
 
 from mvpa2.base import externals
 
+
 # arbitrary ndarray subclass for testing
 class myarray(np.ndarray):
     pass
+
 
 def test_flatten():
     samples_shape = (2, 2, 4)
@@ -127,8 +127,8 @@ def test_flatten():
 def test_product_flatten():
     nsamples = 17
     product_name_values = [('chan', ['C1', 'C2']),
-                         ('freq', np.arange(4, 20, 6)),
-                         ('time', np.arange(-200, 800, 200))]
+                           ('freq', np.arange(4, 20, 6)),
+                           ('time', np.arange(-200, 800, 200))]
 
     shape = (nsamples,) + tuple(len(v) for _, v in product_name_values)
 
@@ -163,7 +163,8 @@ def test_product_flatten():
         import tempfile
         import os
 
-        fd, testfn = tempfile.mkstemp('mapper.h5py', 'test_product'); os.close(fd)
+        fd, testfn = tempfile.mkstemp('mapper.h5py', 'test_product')
+        os.close(fd)
         h5save(testfn, flattener)
         flattener = h5load(testfn)
         os.unlink(testfn)
@@ -176,12 +177,10 @@ def test_product_flatten():
 
     mds = flattener(ds)
 
-    prod = lambda x:reduce(operator.mul, x)
+    prod = lambda x: reduce(operator.mul, x)
 
     # ensure the size is ok
     assert_equal(mds.shape, (nsamples,) + (prod(shape[1:]),))
-
-    ndim = len(product_name_values)
 
     idxs = [range(len(v)) for v in values]
     for si in xrange(nsamples):
@@ -204,7 +203,6 @@ def test_product_flatten():
 
     dsr = flattener.reverse(mds)
     assert_equal(dsr.shape, ds.shape)
-
 
     names += ('foo',)
 
@@ -305,6 +303,7 @@ def test_subset_filler():
     data_back_fnan = sm_fnan.reverse(data_forwarded)
     ok_(np.all(np.isnan(data_back_fnan[:, 3:])))
 
+
 @nodebug(['ID_IN_REPR', 'MODULE_IN_REPR'])
 def test_repr():
     # this time give mask only by its target length
@@ -313,6 +312,7 @@ def test_repr():
     # check reproduction
     sm_clone = eval(repr(sm))
     assert_equal(repr(sm_clone), repr(sm))
+
 
 @nodebug(['ID_IN_REPR', 'MODULE_IN_REPR'])
 def test_chainmapper():
@@ -330,7 +330,6 @@ def test_chainmapper():
     samples_shape = (2, 2, 4)
     data_shape = (4,) + samples_shape
     data = np.arange(np.prod(data_shape)).reshape(data_shape)
-    pristinedata = data.copy()
     target = [[ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
               [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
               [32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47],
@@ -384,6 +383,7 @@ def test_chainmapper():
     tail_sfs = ds_subsel.a.mapper[-1]
     assert_equal(repr(tail_sfs), 'StaticFeatureSelection(slicearg=array([14]))')
 
+
 def test_sampleslicemapper():
     # this does nothing but Dataset.__getitem__ which is tested elsewhere -- but
     # at least we run it
@@ -401,6 +401,7 @@ def test_strip_boundary():
     assert_equal(len(sds), len(ds) - 3)
     for i in [19, 20, 21]:
         assert_false(i in sds.samples.sid)
+
 
 def test_transpose():
     from mvpa2.mappers.shape import TransposeMapper
@@ -426,6 +427,7 @@ def test_transpose():
     assert_array_equal(rds.samples, ttds.samples)
     assert_equal(rds.sa, ttds.sa)
     assert_equal(rds.fa, ttds.fa)
+
 
 def test_addaxis():
     from mvpa2.mappers.shape import AddAxisMapper
@@ -461,10 +463,12 @@ def test_addaxis():
     ds4 = AddAxisMapper(pos=4)(ds)
     assert_array_equal(ds4.shape, ds.shape + (1, 1))
 
-@sweepargs(s=('i_am_the_test',
-              range(5),
-              np.arange(12).reshape(4,3),
-              Dataset(np.arange(12).reshape(4,3))))
+
+@sweepargs(
+    s=('i_am_the_test',
+       range(5),
+       np.arange(12).reshape(4, 3),
+       Dataset(np.arange(12).reshape(4, 3))))
 def test_identity_mapper(s):
     idm = IdentityMapper()
     # doesn't matter what you throw at it
