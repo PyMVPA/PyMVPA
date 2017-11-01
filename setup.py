0b3ada9a062704416ea490fce113930c385bd7d4
Yaroslav Halchenko <debian@onerussian.com>
2017-11-01 09:13:35 -0400

    Life is going on

--- setup.py
+++ setup.py
@@ -181,7 +181,7 @@ def setup_package():
     sys.path.insert(0, src_path)
 
     setup(name='pymvpa2',
-          version='2.6.2',
+          version='2.6.2.dev1',
           author='Michael Hanke, Yaroslav Halchenko, Nikolaas N. Oosterhof',
           author_email='pkg-exppsy-pymvpa@lists.alioth.debian.org',
           license='MIT License',
